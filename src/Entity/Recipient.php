2425ab41f36656dcef5cf912c88ce19a9d8581ae
Frank Dekker <fdekker@123inkt.nl>
2022-05-10 22:50:45 +0200

    Add doctrine

---
+++ src/Entity/Recipient.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\GitCommitNotification\Entity;
+
+use DR\GitCommitNotification\Repository\RecipientRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RecipientRepository::class)]
+class Recipient
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $name;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $email;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
---
+++ src/Repository/RecipientRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace DR\GitCommitNotification\Repository;
+
+use DR\GitCommitNotification\Entity\Recipient;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Recipient>
+ *
+ * @method Recipient|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Recipient|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Recipient[]    findAll()
+ * @method Recipient[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class RecipientRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Recipient::class);
+    }
+
+    public function add(Recipient $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Recipient $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return Recipient[] Returns an array of Recipient objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('r')
+//            ->andWhere('r.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('r.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Recipient
+//    {
+//        return $this->createQueryBuilder('r')
+//            ->andWhere('r.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
