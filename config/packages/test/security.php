44eed2eb9f589fd5f5a217c160181e47ef27c52f
Frank Dekker <fdekker@123inkt.nl>
2022-05-26 20:03:52 +0200

    Convert yaml config to php

---
+++ config/packages/test/security.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->extension(
+        'security',
+        [
+            'password_hashers' => [
+                PasswordAuthenticatedUserInterface::class => [
+                    'algorithm' => 'auto',
+                    'cost' => 4,
+                    'time_cost' => 3,
+                    'memory_cost' => 10
+                ]
+            ]
+        ]
+    );
+};
