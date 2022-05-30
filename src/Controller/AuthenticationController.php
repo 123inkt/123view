97ce0ca7e4b714414dec5461368018e98d62f202
Frank Dekker <fdekker@123inkt.nl>
2022-05-30 22:13:16 +0200

    Add azure single sign on

--- src/Controller/AuthenticationController.php
+++ src/Controller/AuthenticationController.php
@@ -3,38 +3,16 @@ declare(strict_types=1);
 
 namespace DR\GitCommitNotification\Controller;
 
-use Doctrine\Persistence\ManagerRegistry;
-use DR\GitCommitNotification\Entity\Recipient;
-use DR\GitCommitNotification\Entity\Repository;
-use DR\GitCommitNotification\Entity\RepositoryProperty;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AuthenticationController extends AbstractController
 {
     #[Route('/', self::class)]
-    public function __invoke(ManagerRegistry $doctrine): Response
+    #[Template('authentication/single-sign-on.html.twig')]
+    public function __invoke(): array
     {
-        $em           = $doctrine->getManager();
-        $recipient    = $doctrine->getRepository(Recipient::class)->find(1);
-        $repositories = $doctrine->getRepository(Repository::class)->findAll();
-
-        $result = [];
-
-        if ($recipient !== null) {
-            $result['recipient'] = ['name' => $recipient->getName(), 'email' => $recipient->getEmail()];
-        }
-
-        foreach ($repositories as $repository) {
-            $result['repository'][] = ['repository' => $repository->getName()];
-
-            $property = (new RepositoryProperty())->setName("gitlab-project-id")->setValue("1");
-            $repository->addRepositoryProperty($property);
-
-        }
-
-        return new JsonResponse($result);
+        return ['page_title' => 'Single Sign On'];
     }
 }
