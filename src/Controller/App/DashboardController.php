239973afd966c9fe4425384725def7aba80f24a4
Frank Dekker <fdekker@123inkt.nl>
2022-05-31 20:42:25 +0200

    Add rule dashboard

---
+++ src/Controller/App/DashboardController.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\GitCommitNotification\Controller\App;
+
+use Doctrine\Persistence\ManagerRegistry;
+use DR\GitCommitNotification\Entity\User;
+use DR\GitCommitNotification\ViewModel\App\DashboardViewModel;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController
+{
+    #[Route('app/', name: self::class)]
+    #[Template('app/dashboard.html.twig')]
+    public function __invoke(ManagerRegistry $doctrine): array
+    {
+        $user = $doctrine->getRepository(User::class)->findOneBy(['email' => 'fdekker@123inkt.nl']);
+        if ($user === null) {
+            throw new NotFoundHttpException();
+        }
+
+        $model = new DashboardViewModel();
+        $model->setRules(iterator_to_array($user->getRules()));
+
+        return ['dashboardModel' => $model];
+    }
+}
---
+++ src/ViewModel/App/DashboardViewModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\GitCommitNotification\ViewModel\App;
+
+use DR\GitCommitNotification\Entity\Rule;
+
+class DashboardViewModel
+{
+    /** @var Rule[] */
+    private array $rules = [];
+
+    /**
+     * @return Rule[]
+     */
+    public function getRules(): array
+    {
+        return $this->rules;
+    }
+
+    /**
+     * @param Rule[] $rules
+     *
+     * @return void
+     */
+    public function setRules(array $rules): void
+    {
+        $this->rules = $rules;
+    }
+}
