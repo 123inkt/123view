7a7cf95eaae49900bd76cf872b27ce81fdfb8ce4
Frank Dekker <fdekker@123inkt.nl>
2022-06-08 23:20:24 +0200

    Delete rule controller

---
+++ src/Controller/App/DeleteRuleController.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\GitCommitNotification\Controller\App;
+
+use Doctrine\Persistence\ManagerRegistry;
+use DR\GitCommitNotification\Entity\Rule;
+use DR\GitCommitNotification\Entity\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class DeleteRuleController extends AbstractController
+{
+    public function __construct(private ManagerRegistry $doctrine, private TranslatorInterface $translator, private User $user)
+    {
+    }
+
+    #[Route('/rules/rule/{id<\d+>}', self::class, methods: ['DELETE'])]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    #[Entity('rule')]
+    public function __invoke(Request $request, Rule $rule): RedirectResponse
+    {
+        if ($rule->getUser() !== $this->user) {
+            throw new AccessDeniedException('Access denied');
+        }
+
+        //$this->doctrine->getManager()->remove($rule);
+        //$this->doctrine->getManager()->flush();
+
+        return $this->redirectToRoute(
+            DashboardController::class,
+            ['message' => $this->translator->trans('Rule {name} successfully removed.', ['name' => $rule->getName()])]
+        );
+    }
+}
