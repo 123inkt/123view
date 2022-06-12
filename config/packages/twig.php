5bed852fd17e02ac70916f949fd84472e3683ad0
Frank Dekker <fdekker@123inkt.nl>
2022-06-12 19:33:36 +0200

    move assets

--- config/packages/twig.php
+++ config/packages/twig.php
@@ -8,6 +8,6 @@ use Symfony\Config\TwigConfig;
 return static function (TwigConfig $twig, ContainerConfigurator $configurator): void {
     $twig
         ->defaultPath('%kernel.project_dir%/templates')
-        ->path('%kernel.project_dir%/assets/styles', 'styles')
+        ->path('%kernel.project_dir%/public/assets/styles', 'styles')
         ->formThemes(['bootstrap_5_layout.html.twig']);
 };
