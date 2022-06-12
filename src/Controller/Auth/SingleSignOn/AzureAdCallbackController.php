6900494b2767312deb572207215eee568c40e58e
Frank Dekker <fdekker@123inkt.nl>
2022-06-12 17:40:28 +0200

    Fix azure authentication issue

--- src/Controller/Auth/SingleSignOn/AzureAdCallbackController.php
+++ src/Controller/Auth/SingleSignOn/AzureAdCallbackController.php
@@ -3,8 +3,7 @@ declare(strict_types=1);
 
 namespace DR\GitCommitNotification\Controller\Auth\SingleSignOn;
 
-use DR\GitCommitNotification\Security\AzureAd\LoginFailure;
-use DR\GitCommitNotification\Security\AzureAd\LoginService;
+use RuntimeException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,18 +11,10 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AzureAdCallbackController
 {
-    public function __construct(private LoginService $loginService)
-    {
-    }
-
     #[Route('/single-sign-on/azure-ad/callback', name: self::class, methods: 'GET')]
     public function __invoke(Request $request): RedirectResponse|JsonResponse
     {
-        $result = $this->loginService->handleLogin($request);
-        if ($result instanceof LoginFailure) {
-            return new JsonResponse(['success' => false, 'message' => $result->getMessage()]);
-        }
-
-        return new RedirectResponse('https://www.google.com');
+        // never called, handled by AzureAdAuthenticator
+        throw new RuntimeException('AzureAdAuthenticator route is not configured');
     }
 }
