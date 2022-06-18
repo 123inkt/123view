a43a34e186cd5919ebd7ba7defdcb7487d353ff3
Frank Dekker <fdekker@123inkt.nl>
2022-06-18 17:05:46 +0200

    Add docker development environment (#10)

---
+++ docker/php-fpm/run/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+if [ "${APP_ENV}" == "dev" ]; then
+    composer install --no-interaction --optimize-autoloader
+else
+    composer install --no-dev --no-interaction --optimize-autoloader --classmap-authoritative
+fi
+
+##
+# warmup cache
+#
+php bin/console cache:clear
+
+# echo 'dumping environment variables to local file'
+# composer dump-env prod # Currently no composer in production container
+
+##
+# run doctrine migrations
+#
+php bin/console doctrine:migrations:migrate --no-interaction
+
+export -p > /tmp/env
+mkdir -p /app/var/log
+chown www-data:www-data /tmp/env
+chown www-data:www-data -R /app/var
+
+# exec is needed to make supervisord pid 1 and able to receive SIGTERM signal
+exec supervisord
