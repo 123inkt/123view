05f56965f39268870b789cdf4088c3c10ffdf1ff
Frank Dekker <fdekker@123inkt.nl>
2022-06-23 22:19:28 +0200

    Refactor MailCommand to Entity classes.

--- src/Command/Repository/ListRepositoriesCommand.php
+++ src/Command/Repository/ListRepositoriesCommand.php
@@ -7,7 +7,6 @@ use Doctrine\Persistence\ManagerRegistry;
 use DR\GitCommitNotification\Entity\Repository;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
