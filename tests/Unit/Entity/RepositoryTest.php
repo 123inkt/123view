8fb4da065f1d958f24efd20fe083ec865f9d4ae1
Frank Dekker <fdekker@123inkt.nl>
2022-06-26 16:54:55 +0200

    Update entity tests

--- tests/Unit/Entity/RepositoryTest.php
+++ tests/Unit/Entity/RepositoryTest.php
@@ -3,7 +3,9 @@ declare(strict_types=1);
 
 namespace DR\GitCommitNotification\Tests\Unit\Entity;
 
+use DigitalRevolution\AccessorPairConstraint\Constraint\ConstraintConfig;
 use DR\GitCommitNotification\Entity\Repository;
+use DR\GitCommitNotification\Entity\RepositoryProperty;
 use DR\GitCommitNotification\Tests\AbstractTest;
 
 /**
@@ -16,7 +18,27 @@ class RepositoryTest extends AbstractTest
      */
     public function testAccessorPairs(): void
     {
+        $config = new ConstraintConfig();
+        $config->setExcludedMethods(['addRepositoryProperty', 'getRepositoryProperties']);
+
         static::assertNull((new Repository())->getId());
-        static::assertAccessorPairs(Repository::class);
+        static::assertAccessorPairs(Repository::class, $config);
+    }
+
+    /**
+     * @covers ::addRepositoryProperty
+     * @covers ::getRepositoryProperties
+     * @covers ::removeRepositoryProperty
+     */
+    public function testRepositoryPropertyAccessors(): void
+    {
+        $property = new RepositoryProperty();
+
+        $repository = new Repository();
+        $repository->addRepositoryProperty($property);
+        static::assertSame([$property], $repository->getRepositoryProperties()->getValues());
+
+        $repository->removeRepositoryProperty($property);
+        static::assertCount(0, $repository->getRepositoryProperties());
     }
 }
