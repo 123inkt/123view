8c2349a654d4f25567b5ea66abd63ea8c95af612
Frank Dekker <fdekker@123inkt.nl>
2022-07-02 21:44:08 +0200

    Add coverage rule configuration

---
+++ tests/Unit/Entity/Config/RuleConfigurationTest.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\GitCommitNotification\Tests\Unit\Entity\Config;
+
+use DateTime;
+use DR\GitCommitNotification\Entity\Config\Rule;
+use DR\GitCommitNotification\Entity\Config\RuleConfiguration;
+use DR\GitCommitNotification\Tests\AbstractTestCase;
+
+/**
+ * @coversDefaultClass \DR\GitCommitNotification\Entity\Config\RuleConfiguration
+ */
+class RuleConfigurationTest extends AbstractTestCase
+{
+    /**
+     * @covers ::__construct
+     */
+    public function testConstruct(): void
+    {
+        $dateA = new DateTime();
+        $dateB = new DateTime();
+        $rule  = new Rule();
+
+        $config = new RuleConfiguration($dateA, $dateB, [], $rule);
+        static::assertSame($dateA, $config->startTime);
+        static::assertSame($dateB, $config->endTime);
+        static::assertSame($rule, $rule);
+    }
+}
