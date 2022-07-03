cdc08a93cbac0686e6c927beed2f13cccf1271a5
Frank Dekker <fdekker@123inkt.nl>
2022-07-03 18:59:31 +0200

    Add coverage RecipientCollectionType

--- tests/Unit/Form/Recipient/RecipientCollectionTypeTest.php
+++ tests/Unit/Form/Recipient/RecipientCollectionTypeTest.php
@@ -1,41 +1,43 @@
 <?php
 declare(strict_types=1);
 
-namespace DR\GitCommitNotification\Tests\Unit\Form\Filter;
+namespace DR\GitCommitNotification\Tests\Unit\Form\Recipient;
 
-use DR\GitCommitNotification\Entity\Config\Filter;
-use DR\GitCommitNotification\Form\Filter\FilterCollectionType;
-use DR\GitCommitNotification\Form\Filter\FilterType;
+use DR\GitCommitNotification\Entity\Config\Recipient;
+use DR\GitCommitNotification\Form\Recipient\RecipientCollectionType;
+use DR\GitCommitNotification\Form\Recipient\RecipientType;
 use DR\GitCommitNotification\Tests\AbstractTestCase;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\OptionsResolver\Debug\OptionsResolverIntrospector;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
- * @coversDefaultClass \DR\GitCommitNotification\Form\Filter\FilterCollectionType
+ * @coversDefaultClass \DR\GitCommitNotification\Form\Recipient\RecipientCollectionType
+ * @covers ::__construct
  */
-class FilterCollectionTypeTest extends AbstractTestCase
+class RecipientCollectionTypeTest extends AbstractTestCase
 {
     /**
- * @covers ::configureOptions
- */
+     * @covers ::configureOptions
+     */
     public function testConfigureOptions(): void
     {
         $resolver     = new OptionsResolver();
         $introspector = new OptionsResolverIntrospector($resolver);
 
-        $type = new FilterCollectionType();
+        $type = new RecipientCollectionType();
         $type->configureOptions($resolver);
 
-        static::assertSame(FilterType::class, $introspector->getDefault('entry_type'));
+        static::assertSame(RecipientType::class, $introspector->getDefault('entry_type'));
         static::assertTrue($introspector->getDefault('allow_add'));
         static::assertTrue($introspector->getDefault('allow_delete'));
         static::assertTrue($introspector->getDefault('prototype'));
+        static::assertFalse($introspector->getDefault('by_reference'));
 
         $deleteEmpty = $introspector->getDefault('delete_empty');
         static::assertIsCallable($deleteEmpty);
-        static::assertTrue($deleteEmpty((new Filter())));
-        static::assertFalse($deleteEmpty((new Filter())->setPattern('pattern')));
+        static::assertTrue($deleteEmpty((new Recipient())));
+        static::assertFalse($deleteEmpty((new Recipient())->setEmail('email')));
 
         static::assertCount(1, $introspector->getDefault('constraints'));
     }
@@ -45,7 +47,7 @@ class FilterCollectionTypeTest extends AbstractTestCase
      */
     public function testGetParent(): void
     {
-        $type = new FilterCollectionType();
+        $type = new RecipientCollectionType();
         static::assertSame(CollectionType::class, $type->getParent());
     }
 }
