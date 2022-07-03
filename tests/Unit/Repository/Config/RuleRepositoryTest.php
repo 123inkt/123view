a50d65cf4380977221bc16e811a29db4acf8e1d0
Frank Dekker <fdekker@123inkt.nl>
2022-07-03 13:43:44 +0200

    Add coverage repositories

---
+++ tests/Unit/Repository/Config/RuleRepositoryTest.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\GitCommitNotification\Tests\Unit\Repository\Config;
+
+use DR\GitCommitNotification\Entity\Config\Rule;
+use DR\GitCommitNotification\Repository\Config\RuleRepository;
+use DR\GitCommitNotification\Tests\AbstractRepositoryTestCase;
+use PHPUnit\Framework\MockObject\MockObject;
+
+/**
+ * @coversDefaultClass \DR\GitCommitNotification\Repository\Config\RuleRepository
+ * @covers ::__construct
+ */
+class RuleRepositoryTest extends AbstractRepositoryTestCase
+{
+    /** @var RuleRepository&MockObject */
+    private RuleRepository $repository;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->repository = $this->getRepository(RuleRepository::class);
+    }
+
+    /**
+     * @covers ::add
+     */
+    public function testAdd(): void
+    {
+        $rule = new Rule();
+
+        $this->expectPersist($rule);
+        $this->expectFlush();
+        $this->repository->add($rule, true);
+    }
+
+    /**
+     * @covers ::remove
+     */
+    public function testRemove(): void
+    {
+        $rule = new Rule();
+
+        $this->expectRemove($rule);
+        $this->expectFlush();
+        $this->repository->remove($rule, true);
+    }
+}
