d6371e7a3fc83fbebf7743491f34726603e0cf64
Frank Dekker <fdekker@123inkt.nl>
2022-07-28 18:02:12 +0200

    Upgrade to symfon 6.1

--- config/packages/security.php
+++ config/packages/security.php
@@ -7,12 +7,10 @@ use DR\GitCommitNotification\Controller\Auth\LogoutController;
 use DR\GitCommitNotification\Entity\Config\User;
 use DR\GitCommitNotification\Security\AzureAd\AzureAdAuthenticator;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Config\SecurityConfig;
 
 return static function (ContainerConfigurator $containerConfigurator, SecurityConfig $security): void {
     $security->enableAuthenticatorManager(true);
-    $security->passwordHasher(PasswordAuthenticatedUserInterface::class)->algorithm('auto');
 
     $security->provider('app_user_provider')
         ->entity()
