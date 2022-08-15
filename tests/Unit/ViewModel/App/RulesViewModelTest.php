479b73e3436c57b69f369eb22fd209e413184d0e
Frank Dekker <fdekker@123inkt.nl>
2022-08-15 21:50:29 +0200

    Fix unit test

--- tests/Unit/ViewModel/App/RulesViewModelTest.php
+++ tests/Unit/ViewModel/App/RulesViewModelTest.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace DR\GitCommitNotification\Tests\Unit\ViewModel\App;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use DR\GitCommitNotification\Tests\AbstractTestCase;
 use DR\GitCommitNotification\ViewModel\App\RulesViewModel;
 
@@ -12,10 +13,13 @@ use DR\GitCommitNotification\ViewModel\App\RulesViewModel;
 class RulesViewModelTest extends AbstractTestCase
 {
     /**
+     * @covers ::__construct
      * @covers ::getRules
      */
     public function testAccessorPairs(): void
     {
-        static::assertAccessorPairs(RulesViewModel::class);
+        $collection = new ArrayCollection();
+        $rules      = new RulesViewModel($collection);
+        static::assertSame($collection, $rules->getRules());
     }
 }
