7a1fe67f29f8340745e172660c716b4c71eb5c73
Frank Dekker <fdekker@123inkt.nl>
2022-09-05 17:31:41 +0200

    Add circuit breaker

--- src/Service/Git/GitRepositoryService.php
+++ src/Service/Git/GitRepositoryService.php
@@ -8,8 +8,10 @@ use CzProject\GitPhp\GitException;
 use CzProject\GitPhp\Helpers;
 use DR\GitCommitNotification\Exception\RepositoryException;
 use DR\GitCommitNotification\Git\GitRepository;
+use DR\GitCommitNotification\Utility\CircuitBreaker;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Filesystem\Filesystem;
+use Throwable;
 
 /**
  * Service to clone or pull the repository from the given url.
@@ -20,12 +22,14 @@ class GitRepositoryService
     private Filesystem      $filesystem;
     private LoggerInterface $log;
     private string          $cacheDirectory;
+    private CircuitBreaker  $circuitBreaker;
 
     public function __construct(LoggerInterface $log, Git $git, Filesystem $filesystem, string $cacheDirectory)
     {
         $this->log            = $log;
         $this->git            = $git;
         $this->filesystem     = $filesystem;
+        $this->circuitBreaker = new CircuitBreaker(5, 5000);
         $this->cacheDirectory = $cacheDirectory . '/git/';
     }
 
@@ -35,7 +39,7 @@ class GitRepositoryService
     public function getRepository(string $repositoryUrl): GitRepository
     {
         try {
-            return $this->tryGetRepository($repositoryUrl);
+            return $this->circuitBreaker->execute(fn() => $this->tryGetRepository($repositoryUrl));
         } catch (GitException $exception) {
             $message = $exception->getMessage() . ': ';
             if ($exception->getRunnerResult() !== null) {
@@ -43,6 +47,8 @@ class GitRepositoryService
             }
 
             throw new RepositoryException($message, $exception->getCode(), $exception);
+        } catch (Throwable $exception) {
+            throw new RepositoryException($exception->getMessage(), $exception->getCode(), $exception);
         }
     }
 
---
+++ src/Utility/CircuitBreaker.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\GitCommitNotification\Utility;
+
+use RuntimeException;
+use Throwable;
+
+class CircuitBreaker
+{
+    /**
+     * @param int $attempts the amount of attempts to retry the action
+     * @param int $wait     the amount of milliseconds to wait between attempts
+     */
+    public function __construct(private int $attempts, private int $wait)
+    {
+    }
+
+    /**
+     * @template T
+     *
+     * @param callable(): T $action
+     *
+     * @return T
+     * @throws Throwable
+     */
+    public function execute(callable $action): mixed
+    {
+        $exception = null;
+        for ($i = 0; $i < $this->attempts; $i++) {
+            try {
+                return $action();
+            } catch (Throwable $e) {
+                usleep($this->wait);
+                $exception = $e;
+            }
+        }
+
+        throw new RuntimeException('CircuitBreaker failed after: ' . $this->attempts, 0, $exception);
+    }
+}
