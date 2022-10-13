79628e4859da57f259a2630e67147b2533857359
Frank Dekker <fdekker@123inkt.nl>
2022-10-13 21:03:15 +0200

    Add projects controller

--- src/Controller/App/Review/ReviewsController.php
+++ src/Controller/App/Review/ReviewsController.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 namespace DR\GitCommitNotification\Controller\App\Review;
 
 use DR\GitCommitNotification\Controller\AbstractController;
+use DR\GitCommitNotification\Entity\Config\Repository;
 use DR\GitCommitNotification\ViewModel\App\Review\ProjectsViewModel;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ReviewsController extends AbstractController
@@ -14,9 +17,10 @@ class ReviewsController extends AbstractController
      * @return array<string, ProjectsViewModel>
      */
     #[Route('app/projects/{id<\d+>}/reviews', name: self::class, methods: 'GET')]
-    //#[Template('app/review/reviews.html.twig')]
+    #[Template('app/review/reviews.html.twig')]
     #[IsGranted('IS_AUTHENTICATED_FULLY')]
-    public function __invoke(): array
+    #[Entity('repository')]
+    public function __invoke(Repository $repository): array
     {
         return [];
     }
