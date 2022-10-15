5518db4f1ea7a02350de4242bac24b7c0d6eec98
Frank Dekker <fdekker@123inkt.nl>
2022-10-15 12:48:06 +0200

    Update add reviewer form type

--- src/Form/Review/AddReviewerFormType.php
+++ src/Form/Review/AddReviewerFormType.php
@@ -3,18 +3,17 @@ declare(strict_types=1);
 
 namespace DR\GitCommitNotification\Form\Review;
 
-use Doctrine\ORM\EntityRepository;
 use DR\GitCommitNotification\Controller\App\Review\ReviewController;
 use DR\GitCommitNotification\Entity\Config\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use DR\GitCommitNotification\Repository\Config\UserRepository;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Component\Security\Core\Security;
 
 class AddReviewerFormType extends AbstractType
 {
-    public function __construct(private UrlGeneratorInterface $urlGenerator, private Security $security)
+    public function __construct(private UrlGeneratorInterface $urlGenerator, private UserRepository $userRepository, private User $user)
     {
     }
 
@@ -23,20 +22,26 @@ class AddReviewerFormType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $currentUser = $this->security->getUser();
-
         $builder->setAction($this->urlGenerator->generate(ReviewController::class, ['id' => 5]));
         $builder->setMethod('POST');
-
-        $builder->add('users', EntityType::class, [
-            'label'             => '',
-            'class'             => User::class,
-            'choice_label'      => 'name',
-            'choice_value'      => static fn(?User $entity) => $entity?->getId() ?? 0,
-            'query_builder'     => static fn(EntityRepository $er) => $er->createQueryBuilder('u')->orderBy('u.name', 'ASC'),
-            'preferred_choices' => [$currentUser],
-            'multiple'          => false,
-            'expanded'          => false,
+        $builder->add('users', ChoiceType::class, [
+            'required'                  => false,
+            'label'                     => false,
+            'choice_translation_domain' => false,
+            'choice_label'              => static fn(?User $user) => (string)$user?->getName(),
+            'choice_value'              => static fn(?User $user) => (string)$user?->getId(),
+            'choices'                   => $this->getUserChoices(),
+            'preferred_choices'         => [$this->user],
+            'multiple'                  => false,
+            'expanded'                  => false,
         ]);
+    }
+
+    /**
+     * @return array<string, int>
+     */
+    private function getUserChoices(): array
+    {
+        return $this->userRepository->findBy([], ['name' => 'ASC']);
     }
 }
