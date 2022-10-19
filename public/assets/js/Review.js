c3a57ca7ae6655637e71c8aa56c551d519883e37
Frank Dekker <fdekker@123inkt.nl>
2022-10-19 19:23:32 +0200

    Add review comment gui

--- public/assets/js/Review.js
+++ public/assets/js/Review.js
@@ -3,6 +3,21 @@ import Controller from './Controller.js';
 export default class Review extends Controller {
 
     connect() {
-        console.log('test');
+        this.listen('click', 'line-add-comment', this.addComment.bind(this));
+    }
+
+    addComment(target) {
+        // get file and line numbers
+        const lineBefore = target.dataset.lineBefore;
+        const lineAfter  = target.dataset.lineAfter;
+        const file       = this.role('revision-file').dataset.file;
+
+        // build the url
+        const location = new URL(window.location.href);
+        location.searchParams.set('filePath', file);
+        location.searchParams.set('addComment', lineBefore + ':' + lineAfter);
+
+        // forward
+        window.location = location.toString();
     }
 }
