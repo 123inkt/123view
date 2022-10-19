e3637db07b4969f6d3552e564cbeafa6b2bc43f3
Frank Dekker <fdekker@123inkt.nl>
2022-10-19 19:24:04 +0200

    Add review comment gui

--- public/assets/js/Review.js
+++ public/assets/js/Review.js
@@ -7,15 +7,10 @@ export default class Review extends Controller {
     }
 
     addComment(target) {
-        // get file and line numbers
-        const lineBefore = target.dataset.lineBefore;
-        const lineAfter  = target.dataset.lineAfter;
-        const file       = this.role('revision-file').dataset.file;
-
         // build the url
         const location = new URL(window.location.href);
-        location.searchParams.set('filePath', file);
-        location.searchParams.set('addComment', lineBefore + ':' + lineAfter);
+        location.searchParams.set('filePath', this.role('revision-file').dataset.file);
+        location.searchParams.set('addComment', target.dataset.lineBefore + ':' + target.dataset.lineAfter);
 
         // forward
         window.location = location.toString();
