4134de8b35f084ea4d8c204097ac18547de5a661
Frank Dekker <fdekker@123inkt.nl>
2022-10-20 23:24:06 +0200

    Add comment entity

---
+++ src/Twig/MarkdownExtension.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\GitCommitNotification\Twig;
+
+use League\CommonMark\MarkdownConverter;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class MarkdownExtension extends AbstractExtension
+{
+    public function __construct(private readonly MarkdownConverter $converter)
+    {
+    }
+
+    /**
+     * @return TwigFilter[]
+     */
+    public function getFilters(): array
+    {
+        return [new TwigFilter('markdown', [$this, 'convert'], ['is_safe' => ['all']])];
+    }
+
+    public function convert(string $string): string
+    {
+        return $this->converter->convert($string)->getContent();
+    }
+}
