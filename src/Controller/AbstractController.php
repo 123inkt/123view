56e0f6f6e6659f900a05dd7e22ec4b1f0cb62930
Frank Dekker <fdekker@123inkt.nl>
2022-10-20 23:07:45 +0200

    Add comment entity

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace DR\GitCommitNotification\Controller;
 
 use DR\GitCommitNotification\Entity\Config\User;
+use League\Uri\Http;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController as SymfonyAbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,7 +19,7 @@ abstract class AbstractController extends SymfonyAbstractController
     /**
      * @param array<string, (int|string|null)> $parameters
      */
-    public function refererRedirect(string $route, array $parameters = []): RedirectResponse
+    public function refererRedirect(string $route, array $parameters = [], array $filter = []): RedirectResponse
     {
         /** @var RequestStack $stack */
         $stack = $this->container->get('request_stack');
@@ -29,6 +30,15 @@ abstract class AbstractController extends SymfonyAbstractController
         $referer = $request->server->get('HTTP_REFERER');
         if (is_string($referer) === false) {
             $referer = null;
+        }
+
+        if ($referer !== null && count($filter) > 0) {
+            $uri = Http::createFromString($referer);
+            parse_str($uri->getQuery(), $queryParams);
+            foreach ($filter as $key) {
+                unset($queryParams[$key]);
+            }
+            $referer = (string)$uri->withQuery(http_build_query($queryParams));
         }
 
         return $this->redirect($referer ?? $this->generateUrl($route, $parameters));
