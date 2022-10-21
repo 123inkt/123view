d0b800e0ed064a423e11a0030a74930bdb69db57
Frank Dekker <fdekker@123inkt.nl>
2022-10-21 19:56:51 +0200

    Add reply to comment

--- public/assets/js/Review.js
+++ public/assets/js/Review.js
@@ -8,23 +8,19 @@ export default class Review extends Controller {
     }
 
     addComment(target) {
-        // build the url
         const location = new URL(window.location.href);
         location.searchParams.set('filePath', this.role('revision-file').dataset.file);
         location.searchParams.set('addComment', target.dataset.line + ':' + target.dataset.lineOffset + ':' + target.dataset.lineAfter);
         location.searchParams.delete('replyComment');
 
-        // forward
         window.location = location.toString();
     }
 
     replyToComment(target) {
-        // build the url
         const location = new URL(window.location.href);
         location.searchParams.delete('addComment');
         location.searchParams.set('replyComment', target.dataset.replyTo);
 
-        // forward
         window.location = location.toString();
     }
 }
