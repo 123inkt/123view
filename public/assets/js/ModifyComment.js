fe17c10c4623256c8f26182046933aa1da44bbc0
Frank Dekker <fdekker@123inkt.nl>
2022-10-22 11:04:47 +0200

    Add ctrl+enter to submit

--- public/assets/js/ModifyComment.js
+++ public/assets/js/ModifyComment.js
@@ -1,10 +1,17 @@
 import Controller from './Controller.js';
 
 export default class Comment extends Controller {
-
     connect() {
        this.role('comment-textarea').focus();
+       this.listen('keyup', 'comment-textarea', this.commentKeyListener.bind(this));
        this.listen('click', 'cancel-comment', this.cancelComment.bind(this));
+    }
+
+    commentKeyListener(target, event) {
+        // ctrl + enter should submit the form
+        if (event.key === 'Enter' && event.ctrlKey) {
+            target.closest('form').submit();
+        }
     }
 
     cancelComment()  {
