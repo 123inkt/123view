b3f41f976b5747f7a8062c0da44fc6bb96ea0c07
Frank Dekker <fdekker@123inkt.nl>
2022-10-28 23:44:21 +0200

    Create attach revision to review handler

--- src/Service/Git/Review/CodeReviewService.php
+++ src/Service/Git/Review/CodeReviewService.php
@@ -3,12 +3,7 @@ declare(strict_types=1);
 
 namespace DR\GitCommitNotification\Service\Git\Review;
 
-use DR\GitCommitNotification\Doctrine\Type\CodeReviewerStateType;
-use DR\GitCommitNotification\Doctrine\Type\CodeReviewStateType;
-use DR\GitCommitNotification\Doctrine\Type\CommentStateType;
-use DR\GitCommitNotification\Entity\Config\User;
 use DR\GitCommitNotification\Entity\Review\CodeReview;
-use DR\GitCommitNotification\Entity\Review\CodeReviewer;
 use DR\GitCommitNotification\Entity\Review\Revision;
 use DR\GitCommitNotification\Repository\Review\RevisionRepository;
 
