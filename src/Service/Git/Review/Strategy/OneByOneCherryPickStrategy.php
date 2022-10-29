34c8d3373ed2d9c17f2a995f1f153c710120fc1c
Frank Dekker <fdekker@123inkt.nl>
2022-10-29 15:07:12 +0200

    Extract GitRepositoryResetManager

--- src/Service/Git/Review/Strategy/OneByOneCherryPickStrategy.php
+++ src/Service/Git/Review/Strategy/OneByOneCherryPickStrategy.php
@@ -5,11 +5,10 @@ namespace DR\GitCommitNotification\Service\Git\Review\Strategy;
 
 use DR\GitCommitNotification\Entity\Config\Repository;
 use DR\GitCommitNotification\Service\Git\Add\GitAddService;
-use DR\GitCommitNotification\Service\Git\Branch\GitBranchService;
 use DR\GitCommitNotification\Service\Git\Checkout\GitCheckoutService;
 use DR\GitCommitNotification\Service\Git\CherryPick\GitCherryPickService;
 use DR\GitCommitNotification\Service\Git\Diff\GitDiffService;
-use DR\GitCommitNotification\Service\Git\Reset\GitResetService;
+use DR\GitCommitNotification\Service\Git\GitRepositoryResetManager;
 use DR\GitCommitNotification\Utility\Arrays;
 
 class OneByOneCherryPickStrategy implements ReviewDiffStrategyInterface
@@ -18,9 +17,8 @@ class OneByOneCherryPickStrategy implements ReviewDiffStrategyInterface
         private readonly GitCheckoutService $checkoutService,
         private readonly GitCherryPickService $cherryPickService,
         private readonly GitDiffService $diffService,
-        private readonly GitResetService $resetService,
-        private readonly GitBranchService $branchService,
-        private readonly GitAddService $addService
+        private readonly GitAddService $addService,
+        private readonly GitRepositoryResetManager $resetManager,
     ) {
     }
 
@@ -32,29 +30,18 @@ class OneByOneCherryPickStrategy implements ReviewDiffStrategyInterface
         // create branch
         $branchName = $this->checkoutService->checkoutRevision(Arrays::first($revisions));
 
-        try {
+        return $this->resetManager->start($repository, $branchName, function () use ($repository, $revisions) {
             foreach ($revisions as $revision) {
-                // cherry pick single revision
+                // cherry-pick single revision
                 $result = $this->cherryPickService->tryCherryPickRevisions([$revision]);
 
-                // cherry pick failed due to merge conflicts. Take theirs:
+                // cherry-pick failed due to merge conflicts. Take theirs:
                 if ($result === false) {
                     $this->addService->add($repository, '.');
                 }
             }
 
-            $files = $this->diffService->getBundledDiffFromRevisions($repository);
-        } finally {
-            // reset the repository again
-            $this->resetService->resetHard($repository);
-
-            // checkout master
-            $this->checkoutService->checkout($repository, 'master');
-
-            // cleanup branch
-            $this->branchService->tryDeleteBranch($repository, $branchName);
-        }
-
-        return $files;
+            return $this->diffService->getBundledDiffFromRevisions($repository);
+        });
     }
 }
