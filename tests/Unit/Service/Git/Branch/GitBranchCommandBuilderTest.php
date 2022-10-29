0939206558d65a692ffdb8e9da7db850495c889f
Frank Dekker <fdekker@123inkt.nl>
2022-10-29 20:28:24 +0200

    Add coverage GitBranchCommandService

---
+++ tests/Unit/Service/Git/Branch/GitBranchCommandBuilderTest.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\GitCommitNotification\Tests\Unit\Service\Git\Branch;
+
+use DR\GitCommitNotification\Service\Git\Branch\GitBranchCommandBuilder;
+use DR\GitCommitNotification\Tests\AbstractTestCase;
+
+/**
+ * @coversDefaultClass \DR\GitCommitNotification\Service\Git\Branch\GitBranchCommandBuilder
+ * @covers ::__construct
+ */
+class GitBranchCommandBuilderTest extends AbstractTestCase
+{
+    private const DEFAULTS = ['git', 'branch'];
+
+    private GitBranchCommandBuilder $builder;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->builder = new GitBranchCommandBuilder('git');
+    }
+
+    /**
+     * @covers ::build
+     */
+    public function testBuildDefaults(): void
+    {
+        static::assertSame(self::DEFAULTS, $this->builder->build());
+    }
+
+    /**
+     * @covers ::delete
+     * @covers ::build
+     */
+    public function testBuildMethods(): void
+    {
+        static::assertSame(['git', 'branch', '-D branchName'], $this->builder->delete('branchName')->build());
+    }
+
+    /**
+     * @covers ::command
+     */
+    public function testCommand(): void
+    {
+        static::assertSame('branch', $this->builder->command());
+    }
+
+    /**
+     * @covers ::__toString
+     */
+    public function testToString(): void
+    {
+        static::assertSame('git branch -D branchName', (string)$this->builder->delete('branchName'));
+    }
+}
