366d97c0cfaba5d485fcc187362800a3722204fe
Frank Dekker <fdekker@123inkt.nl>
2022-10-29 12:23:13 +0200

    Add coverage Comment

---
+++ tests/Unit/Entity/Review/CommentTest.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\GitCommitNotification\Tests\Unit\Entity\Review;
+
+use DigitalRevolution\AccessorPairConstraint\Constraint\ConstraintConfig;
+use Doctrine\Common\Collections\ArrayCollection;
+use DR\GitCommitNotification\Entity\Review\Comment;
+use DR\GitCommitNotification\Entity\Review\LineReference;
+use DR\GitCommitNotification\Tests\AbstractTestCase;
+
+/**
+ * @coversDefaultClass \DR\GitCommitNotification\Entity\Review\Comment
+ * @covers ::__construct
+ */
+class CommentTest extends AbstractTestCase
+{
+    /**
+     * @covers ::<public>
+     */
+    public function testAccessorPairs(): void
+    {
+        $config = (new ConstraintConfig())->setExcludedMethods(['setReplies', 'setLineReference']);
+        static::assertAccessorPairs(Comment::class, $config);
+    }
+
+    /**
+     * @covers ::setLineReference
+     * @covers ::getLineReference
+     */
+    public function testLineReference(): void
+    {
+        $comment = new Comment();
+        static::assertNull($comment->getLineReference());
+
+        $comment->setLineReference(new LineReference('foobar', 1, 2, 3));
+        static::assertEquals(new LineReference('foobar', 1, 2, 3), $comment->getLineReference());
+    }
+
+    /**
+     * @covers ::getReplies
+     * @covers ::setReplies
+     */
+    public function testReplies(): void
+    {
+        $collection = new ArrayCollection();
+
+        $comment = new Comment();
+        static::assertInstanceOf(ArrayCollection::class, $comment->getReplies());
+
+        $comment->setReplies($collection);
+        static::assertSame($collection, $comment->getReplies());
+    }
+}
