2150fcf9095ecccdf952cc557d31981731a47dde
Frank Dekker <fdekker@123inkt.nl>
2022-10-31 19:11:01 +0100

    Add mail notification handler

--- src/MessageHandler/MailNotificationMessageHandler.php
+++ src/MessageHandler/MailNotificationMessageHandler.php
@@ -3,33 +3,91 @@ declare(strict_types=1);
 
 namespace DR\GitCommitNotification\MessageHandler;
 
+use DR\GitCommitNotification\Doctrine\Type\CommentStateType;
+use DR\GitCommitNotification\Entity\Review\NotificationStatus;
 use DR\GitCommitNotification\Message\Comment\CommentAdded;
 use DR\GitCommitNotification\Message\Comment\CommentReplyAdded;
 use DR\GitCommitNotification\Message\Comment\CommentResolved;
-use Psr\Log\LoggerInterface;
+use DR\GitCommitNotification\Repository\Config\UserRepository;
+use DR\GitCommitNotification\Repository\Review\CommentReplyRepository;
+use DR\GitCommitNotification\Repository\Review\CommentRepository;
+use DR\GitCommitNotification\Service\Mail\MailService;
+use DR\GitCommitNotification\Utility\Type;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Component\Messenger\Handler\MessageSubscriberInterface;
+use Throwable;
 
-#[AsMessageHandler(fromTransport: 'async_delay_mail')]
-class MailNotificationMessageHandler implements MessageSubscriberInterface
+#[AsMessageHandler]
+class MailNotificationMessageHandler implements MessageSubscriberInterface, LoggerAwareInterface
 {
-    public function __construct(private readonly LoggerInterface $logger)
-    {
+    use LoggerAwareTrait;
+
+    public function __construct(
+        private readonly MailService $mailService,
+        private readonly CommentRepository $commentRepository,
+        private readonly CommentReplyRepository $replyRepository,
+        private readonly UserRepository $userRepository
+    ) {
     }
 
+    /**
+     * @throws Throwable
+     */
     public function handleCommentAdded(CommentAdded $message): void
     {
-        $this->logger->info('comment added: ' . $message->commentId);
+        $this->logger?->info('MailNotificationMessageHandler: comment added: ' . $message->commentId);
+
+        $comment = $this->commentRepository->find($message->commentId);
+        if ($comment === null) {
+            // comment was removed before we could send it
+            return;
+        }
+
+        // a notification was already send for this comment
+        if ($comment->getNotificationStatus()->hasStatus(NotificationStatus::STATUS_CREATED)) {
+            return;
+        }
+
+        $this->mailService->sendNewCommentMail(Type::notNull($comment->getReview()), $comment);
+
+        // update status and save
+        $comment->getNotificationStatus()->addStatus(NotificationStatus::STATUS_CREATED);
+        $this->commentRepository->save($comment);
     }
 
+    /**
+     * @throws Throwable
+     */
     public function handleCommentReplyAdded(CommentReplyAdded $message): void
     {
-        $this->logger->info('comment reply: ' . $message->commentReplyId);
+        $this->logger?->info('MailNotificationMessageHandler: comment reply: ' . $message->commentReplyId);
     }
 
+    /**
+     * @throws Throwable
+     */
     public function handleCommentResolved(CommentResolved $message): void
     {
-        $this->logger->info('comment resolved: ' . $message->commentId);
+        $this->logger?->info('MailNotificationMessageHandler: comment resolved: ' . $message->commentId);
+
+        $comment = $this->commentRepository->find($message->commentId);
+        $user    = $this->userRepository->find($message->resolveByUserId);
+        if ($comment === null || $user === null || $comment->getState() !== CommentStateType::RESOLVED) {
+            return;
+        }
+
+        // a notification was already send for this comment
+        if ($comment->getNotificationStatus()->hasStatus(NotificationStatus::STATUS_RESOLVED)) {
+            return;
+        }
+
+        $this->mailService->sendCommentResolvedMail(Type::notNull($comment->getReview()), $comment, $user);
+
+        // update status and save
+        $comment->getNotificationStatus()->addStatus(NotificationStatus::STATUS_RESOLVED);
+        $this->commentRepository->save($comment);
     }
 
     /**
@@ -37,8 +95,8 @@ class MailNotificationMessageHandler implements MessageSubscriberInterface
      */
     public static function getHandledMessages(): iterable
     {
-        yield CommentAdded::class => ['method' => 'handleCommentAdded'];
-        yield CommentReplyAdded::class => ['method' => 'handleCommentReplyAdded'];
-        yield CommentResolved::class => ['method' => 'handleCommentResolved'];
+        yield CommentAdded::class => ['method' => 'handleCommentAdded', 'from_transport' => 'async_delay_mail'];
+        yield CommentReplyAdded::class => ['method' => 'handleCommentReplyAdded', 'from_transport' => 'async_delay_mail'];
+        yield CommentResolved::class => ['method' => 'handleCommentResolved', 'from_transport' => 'async_delay_mail'];
     }
 }
