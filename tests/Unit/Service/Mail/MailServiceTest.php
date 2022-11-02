9e3d3c850beee485aca6fe078c233e7339f47c7b
Frank Dekker <fdekker@123inkt.nl>
2022-11-02 08:59:34 +0100

    Fix phpstan issue

--- tests/Unit/Service/Mail/MailServiceTest.php
+++ tests/Unit/Service/Mail/MailServiceTest.php
@@ -10,13 +10,16 @@ use DR\GitCommitNotification\Entity\Config\Recipient;
 use DR\GitCommitNotification\Entity\Config\Rule;
 use DR\GitCommitNotification\Entity\Config\RuleConfiguration;
 use DR\GitCommitNotification\Entity\Config\RuleOptions;
+use DR\GitCommitNotification\Service\Mail\MailRecipientService;
 use DR\GitCommitNotification\Service\Mail\MailService;
 use DR\GitCommitNotification\Service\Mail\MailSubjectFormatter;
 use DR\GitCommitNotification\Tests\AbstractTestCase;
+use DR\GitCommitNotification\ViewModelProvider\Mail\MailCommentViewModelProvider;
 use PHPUnit\Framework\MockObject\MockObject;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
  * @coversDefaultClass \DR\GitCommitNotification\Service\Mail\MailService
@@ -24,17 +27,29 @@ use Symfony\Component\Mailer\MailerInterface;
  */
 class MailServiceTest extends AbstractTestCase
 {
-    private MailerInterface&MockObject      $mailer;
-    private MailSubjectFormatter&MockObject $formatter;
-    private MailService                     $service;
-    private Rule                            $rule;
+    private MailerInterface&MockObject              $mailer;
+    private MailSubjectFormatter&MockObject         $formatter;
+    private TranslatorInterface&MockObject          $translator;
+    private MailRecipientService&MockObject         $recipientService;
+    private MailCommentViewModelProvider&MockObject $viewModelProvider;
+    private MailService                             $service;
+    private Rule                                    $rule;
 
     protected function setUp(): void
     {
         parent::setUp();
-        $this->mailer    = $this->createMock(MailerInterface::class);
-        $this->formatter = $this->createMock(MailSubjectFormatter::class);
-        $this->service   = new MailService($this->log, $this->mailer, $this->formatter);
+        $this->mailer            = $this->createMock(MailerInterface::class);
+        $this->formatter         = $this->createMock(MailSubjectFormatter::class);
+        $this->translator        = $this->createMock(TranslatorInterface::class);
+        $this->recipientService  = $this->createMock(MailRecipientService::class);
+        $this->viewModelProvider = $this->createMock(MailCommentViewModelProvider::class);
+        $this->service           = new MailService(
+            $this->translator,
+            $this->mailer,
+            $this->formatter,
+            $this->recipientService,
+            $this->viewModelProvider
+        );
 
         $recipient = new Recipient();
         $recipient->setEmail('recipient@example.com');
