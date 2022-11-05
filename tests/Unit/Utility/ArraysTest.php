fe95889aef4d1af81271d3affca3cb4b11ed4771
Frank Dekker <fdekker@123inkt.nl>
2022-11-05 11:15:32 +0100

    Add coverage Arrays

--- tests/Unit/Utility/ArraysTest.php
+++ tests/Unit/Utility/ArraysTest.php
@@ -6,6 +6,7 @@ namespace DR\GitCommitNotification\Tests\Unit\Utility;
 use DR\GitCommitNotification\Tests\AbstractTestCase;
 use DR\GitCommitNotification\Utility\Arrays;
 use RuntimeException;
+use stdClass;
 
 /**
  * @coversDefaultClass \DR\GitCommitNotification\Utility\Arrays
@@ -39,5 +40,33 @@ class ArraysTest extends AbstractTestCase
 
         static::assertSame([], Arrays::mapAssoc([], $callback));
         static::assertSame(['foo' => 'bar'], Arrays::mapAssoc([['foo', 'bar']], $callback));
+    }
+
+    /**
+     * @covers ::tryFind
+     */
+    public function testTryFind(): void
+    {
+        $objA  = new stdClass();
+        $objB  = new stdClass();
+        $array = [$objA, $objB];
+
+        static::assertSame($objA, Arrays::tryFind($array, static fn($item) => $item === $objA));
+        static::assertSame($objB, Arrays::tryFind($array, static fn($item) => $item === $objB));
+        static::assertNull(Arrays::tryFind($array, static fn($item) => false));
+    }
+
+    /**
+     * @covers ::remove
+     */
+    public function testRemove(): void
+    {
+        $objA  = new stdClass();
+        $objB  = new stdClass();
+        $array = [$objA, $objB];
+
+        static::assertSame([1 => $objB], Arrays::remove($array, $objA));
+        static::assertSame([0 => $objA], Arrays::remove($array, $objB));
+        static::assertSame($array, Arrays::remove($array, 'foobar'));
     }
 }
