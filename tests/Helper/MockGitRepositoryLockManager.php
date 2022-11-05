3319e3d471482c9f434abe5ab09e0199d870f354
Frank Dekker <fdekker@123inkt.nl>
2022-11-05 10:49:41 +0100

    Add coverage GitRepositoryLockManagerTest

--- tests/Helper/MockGitRepositoryLockManager.php
+++ tests/Helper/MockGitRepositoryLockManager.php
@@ -5,12 +5,13 @@ namespace DR\GitCommitNotification\Tests\Helper;
 
 use DR\GitCommitNotification\Entity\Config\Repository;
 use DR\GitCommitNotification\Service\Git\GitRepositoryLockManager;
+use Symfony\Component\Filesystem\Filesystem;
 
 class MockGitRepositoryLockManager extends GitRepositoryLockManager
 {
     public function __construct()
     {
-        parent::__construct('/tmp/');
+        parent::__construct('/tmp/', new Filesystem());
     }
 
     public function start(Repository $repository, callable $callback): mixed
