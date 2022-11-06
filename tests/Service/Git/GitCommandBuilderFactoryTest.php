3d601a9ae72a91404fd553b3069772806608c7c0
Frank Dekker <fdekker@123inkt.nl>
2022-11-06 22:09:17 +0100

    Add coverage GitCommandBuilderFactory

---
+++ tests/Service/Git/GitCommandBuilderFactoryTest.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\GitCommitNotification\Tests\Service\Git;
+
+use DR\GitCommitNotification\Service\Git\GitCommandBuilderFactory;
+use DR\GitCommitNotification\Tests\AbstractTestCase;
+
+/**
+ * @coversDefaultClass \DR\GitCommitNotification\Service\Git\GitCommandBuilderFactory
+ * @covers ::__construct
+ */
+class GitCommandBuilderFactoryTest extends AbstractTestCase
+{
+    private GitCommandBuilderFactory $factory;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->factory = new GitCommandBuilderFactory('git');
+    }
+
+    /**
+     * @covers ::createCheryPick
+     * @covers ::createCheckout
+     * @covers ::createLog
+     * @covers ::createAdd
+     * @covers ::createBranch
+     * @covers ::createShow
+     * @covers ::createDiff
+     * @covers ::createReset
+     */
+    public function testCreate(): void
+    {
+        $this->factory->createCheryPick();
+        $this->factory->createCheckout();
+        $this->factory->createLog();
+        $this->factory->createAdd();
+        $this->factory->createDiff();
+        $this->factory->createBranch();
+        $this->factory->createShow();
+        $this->factory->createReset();
+        $this->addToAssertionCount(1);
+    }
+}
