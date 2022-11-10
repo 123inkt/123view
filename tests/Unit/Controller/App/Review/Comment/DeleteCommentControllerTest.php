a9328e15493e21db195e340cd01f714d1b3cd1f7
Frank Dekker <fdekker@123inkt.nl>
2022-11-10 22:42:29 +0100

    Add coverage DeleteCommentController

---
+++ tests/Unit/Controller/App/Review/Comment/DeleteCommentControllerTest.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\GitCommitNotification\Tests\Unit\Controller\App\Review\Comment;
+
+use DR\GitCommitNotification\Controller\App\Review\Comment\DeleteCommentController;
+use DR\GitCommitNotification\Controller\App\Review\ReviewController;
+use DR\GitCommitNotification\Entity\Review\CodeReview;
+use DR\GitCommitNotification\Entity\Review\Comment;
+use DR\GitCommitNotification\Repository\Review\CommentRepository;
+use DR\GitCommitNotification\Security\Voter\CommentVoter;
+use DR\GitCommitNotification\Tests\AbstractControllerTestCase;
+use PHPUnit\Framework\MockObject\MockObject;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+/**
+ * @coversDefaultClass \DR\GitCommitNotification\Controller\App\Review\Comment\DeleteCommentController
+ * @covers ::__construct
+ */
+class DeleteCommentControllerTest extends AbstractControllerTestCase
+{
+    private CommentRepository&MockObject $commentRepository;
+
+    public function setUp(): void
+    {
+        $this->commentRepository = $this->createMock(CommentRepository::class);
+        parent::setUp();
+    }
+
+    /**
+     * @covers ::__invoke
+     */
+    public function testInvoke(): void
+    {
+        $comment = new Comment();
+        $comment->setId(123);
+        $review = new CodeReview();
+        $review->setId(456);
+        $comment->setReview($review);
+
+        $this->expectedDenyAccessUnlessGranted(CommentVoter::DELETE, $comment);
+        $this->commentRepository->expects(self::once())->method('remove')->with($comment, true);
+        $this->expectRedirect(ReviewController::class, ['id' => 456]);
+
+        static::assertInstanceOf(RedirectResponse::class, ($this->controller)($comment));
+    }
+
+    public function getController(): AbstractController
+    {
+        return new DeleteCommentController($this->commentRepository);
+    }
+}
