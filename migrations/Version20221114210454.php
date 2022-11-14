8b8470edc4226b79da8db41ad1ad6e77ee08383f
Frank Dekker <fdekker@123inkt.nl>
2022-11-14 22:25:32 +0100

    Add code review description (uneditable for now)

--- migrations/Version20221114210454.php
+++ migrations/Version20221114210454.php
@@ -20,7 +20,7 @@ final class Version20221114210454 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE code_review ADD description VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE code_review ADD description VARCHAR(255) NOT NULL AFTER `title`');
     }
 
     public function down(Schema $schema): void
--- migrations/Version20221114210851.php
+++ migrations/Version20221114210851.php
@@ -20,14 +20,12 @@ final class Version20221114210851 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE code_review CHANGE description description VARCHAR(255) NOT NULL');
-        $this->addSql('ALTER TABLE revision ADD description VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE revision ADD description VARCHAR(255) NOT NULL AFTER `title`');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE code_review CHANGE description description VARCHAR(500) NOT NULL');
         $this->addSql('ALTER TABLE revision DROP description');
     }
 }
