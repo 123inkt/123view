c08dc0cc6240f961b1dc7d342835920da4a3e5dc
Frank Dekker <fdekker@123inkt.nl>
2022-11-16 17:49:46 +0100

    Add coverage ReviewsViewModel

--- tests/Unit/ViewModel/App/Review/ReviewsViewModelTest.php
+++ tests/Unit/ViewModel/App/Review/ReviewsViewModelTest.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 namespace DR\GitCommitNotification\Tests\Unit\ViewModel\App\Review;
 
 use ArrayIterator;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Tools\Pagination\Paginator;
 use DR\GitCommitNotification\Entity\Config\Repository;
 use DR\GitCommitNotification\Entity\Review\CodeReview;
+use DR\GitCommitNotification\Entity\Review\Revision;
 use DR\GitCommitNotification\Tests\AbstractTestCase;
 use DR\GitCommitNotification\ViewModel\App\Review\PaginatorViewModel;
 use DR\GitCommitNotification\ViewModel\App\Review\ReviewsViewModel;
@@ -32,5 +34,28 @@ class ReviewsViewModelTest extends AbstractTestCase
         $viewModel = new ReviewsViewModel($repository, $paginator, $paginatorVm, $searchQuery);
 
         static::assertSame($reviews, $viewModel->getReviews());
+    }
+
+    /**
+     * @covers ::getAuthors
+     */
+    public function testGetAuthors(): void
+    {
+        $reviews    = [new CodeReview()];
+        $repository = new Repository();
+        $paginator  = $this->createMock(Paginator::class);
+        $paginator->method('getIterator')->willReturn(new ArrayIterator($reviews));
+        $paginatorVm = $this->createMock(PaginatorViewModel::class);
+        $viewModel   = new ReviewsViewModel($repository, $paginator, $paginatorVm, '');
+
+        $revisionA = new Revision();
+        $revisionB = new Revision();
+        $revisionC = new Revision();
+
+        $revisionA->setAuthorName('Sherlock');
+        $revisionB->setAuthorName('Sherlock');
+        $revisionC->setAuthorName('Watson');
+
+        static::assertSame(['Sherlock', 'Watson'], array_values($viewModel->getAuthors(new ArrayCollection([$revisionA, $revisionB, $revisionC]))));
     }
 }
