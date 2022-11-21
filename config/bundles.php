35dd3d315738b276801d4f9cd9c669477bf7827d
Frank Dekker <fdekker@123inkt.nl>
2022-11-21 11:30:12 +0100

    Allow custom Locale and have twig date format accordingly

--- config/bundles.php
+++ config/bundles.php
@@ -7,6 +7,7 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class                                => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class                              => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class             => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class                                => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class                             => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class                 => ['all' => true],
     DigitalRevolution\SymfonyRequestValidation\Bundle\RequestValidationBundle::class => ['all' => true],
--- config/packages/translation.php
+++ config/packages/translation.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 use Symfony\Config\FrameworkConfig;
 
 return static function (FrameworkConfig $framework): void {
-    $framework->defaultLocale('en');
+    $framework->defaultLocale('%env(APP_LOCALE)%');
     $framework->translator()
         ->enabled(true)
         ->defaultPath('%kernel.project_dir%/translations')
