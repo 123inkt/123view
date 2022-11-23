84b0b9ddb9abb2afbe397e4069ca58e9cc1f358e
Frank Dekker <fdekker@123inkt.nl>
2022-11-23 19:31:48 +0100

    Add coverage UserSettingRepository

--- src/Repository/User/UserSettingRepository.php
+++ src/Repository/User/UserSettingRepository.php
@@ -38,29 +38,4 @@ class UserSettingRepository extends ServiceEntityRepository
             $this->getEntityManager()->flush();
         }
     }
-
-//    /**
-//     * @return UserSetting[] Returns an array of UserSetting objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('u.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?UserSetting
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
