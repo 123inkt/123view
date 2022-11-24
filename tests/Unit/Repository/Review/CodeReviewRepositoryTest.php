abb42edd9ae47d5b6064a7eb4617c6c62e7c4f6c
Frank Dekker <fdekker@123inkt.nl>
2022-11-24 20:41:34 +0100

    Add coverage CodeReviewRepository

---
+++ tests/Unit/Repository/Review/CodeReviewRepositoryTest.php
@@ -0,0 +1,73 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\GitCommitNotification\Tests\Unit\Repository\Review;
+
+use DR\GitCommitNotification\Repository\Config\RepositoryRepository;
+use DR\GitCommitNotification\Repository\Review\CodeReviewRepository;
+use DR\GitCommitNotification\Tests\AbstractRepositoryTestCase;
+use DR\GitCommitNotification\Tests\DataFixtures\CodeReviewFixtures;
+use DR\GitCommitNotification\Tests\DataFixtures\UserFixtures;
+use DR\GitCommitNotification\Utility\Assert;
+use Exception;
+
+/**
+ * @coversDefaultClass \DR\GitCommitNotification\Repository\Review\CodeReviewRepository
+ * @covers ::__construct
+ */
+class CodeReviewRepositoryTest extends AbstractRepositoryTestCase
+{
+    private CodeReviewRepository $repository;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->repository = static::getService(CodeReviewRepository::class);
+    }
+
+    /**
+     * @covers ::getCreateProjectId
+     * @throws Exception
+     */
+    public function testGetCreateProjectId(): void
+    {
+        $repository = Assert::notNull(static::getService(RepositoryRepository::class)->findOneBy(['name' => 'repository']));
+        $review     = Assert::notNull($this->repository->findOneBy(['title' => 'title']));
+
+        $projectId = $this->repository->getCreateProjectId((int)$repository->getId());
+        static::assertSame((int)$review->getProjectId() + 1, $projectId);
+    }
+
+    /**
+     * @covers ::findOneByCommitHash
+     */
+    public function testFindOneByCommitHash(): void
+    {
+    }
+
+    /**
+     * @covers ::findByUrl
+     */
+    public function testFindByUrl(): void
+    {
+    }
+
+    /**
+     * @covers ::getPaginatorForSearchQuery
+     */
+    public function testGetPaginatorForSearchQuery(): void
+    {
+    }
+
+    /**
+     * @covers ::findOneByReferenceId
+     */
+    public function testFindOneByReferenceId(): void
+    {
+    }
+
+    protected function getFixtures(): array
+    {
+        return [UserFixtures::class, CodeReviewFixtures::class];
+    }
+}
