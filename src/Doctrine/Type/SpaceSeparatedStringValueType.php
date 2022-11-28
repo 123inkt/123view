c055b4d46feecfc6fce5c7b729f6c7a647753d66
Frank Dekker <fdekker@123inkt.nl>
2022-11-28 16:27:08 +0100

    Add user roles attribute

---
+++ src/Doctrine/Type/SpaceSeparatedStringValueType.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\GitCommitNotification\Doctrine\Type;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+
+class SpaceSeparatedStringValueType extends Type
+{
+    public const TYPE = "space_separated_string_type";
+
+    /**
+     * @inheritDoc
+     */
+    public function getSQLDeclaration(array $column, AbstractPlatform $platform): string
+    {
+        return $platform->getStringTypeDeclarationSQL($column);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform): string
+    {
+        return implode(" ", $value);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        return explode(" ", $value);
+    }
+
+    public function getName(): string
+    {
+        return static::TYPE;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function requiresSQLCommentHint(AbstractPlatform $platform): bool
+    {
+        return false;
+    }
+}
