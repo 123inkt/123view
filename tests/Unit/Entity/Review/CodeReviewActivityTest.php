76bd4e782088e344ed6c11b3e4d0eef10e54ca5f
Frank Dekker <fdekker@123inkt.nl>
2022-12-04 15:04:23 +0100

    Add coverage CodeReviewActivity

--- tests/Unit/Entity/Review/CodeReviewActivityTest.php
+++ tests/Unit/Entity/Review/CodeReviewActivityTest.php
@@ -18,4 +18,18 @@ class CodeReviewActivityTest extends AbstractTestCase
     {
         static::assertAccessorPairs(CodeReviewActivity::class);
     }
+
+    /**
+     * @covers ::getDataValue
+     */
+    public function testGetDataValue(): void
+    {
+        $activity = new CodeReviewActivity();
+        $activity->setData(['bool' => true, 'int' => 1, 'float' => 1.1, 'string' => 'string', 'null' => null]);
+        static::assertTrue($activity->getDataValue('bool'));
+        static::assertSame(1, $activity->getDataValue('int'));
+        static::assertSame(1.1, $activity->getDataValue('float'));
+        static::assertSame('string', $activity->getDataValue('string'));
+        static::assertNull($activity->getDataValue('null'));
+    }
 }
