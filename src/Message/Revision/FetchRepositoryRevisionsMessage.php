b944851861bd8e3c34fc0c7155242ca0e350e849
Frank Dekker <fdekker@123inkt.nl>
2022-12-07 20:45:11 +0100

    FetchRepositoryRevisions message should not implement AsyncMessage

--- src/Message/Revision/FetchRepositoryRevisionsMessage.php
+++ src/Message/Revision/FetchRepositoryRevisionsMessage.php
@@ -3,13 +3,11 @@ declare(strict_types=1);
 
 namespace DR\Review\Message\Revision;
 
-use DR\Review\Message\AsyncMessageInterface;
-
 /**
  * Message to notify consumers to fetch new revisions from given repository
  * @codeCoverageIgnore
  */
-class FetchRepositoryRevisionsMessage implements AsyncMessageInterface
+class FetchRepositoryRevisionsMessage
 {
     public function __construct(public readonly int $repositoryId)
     {
