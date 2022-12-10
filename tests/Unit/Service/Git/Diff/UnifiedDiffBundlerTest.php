228fa73a885c01dba0456eaeb59c49657bda1783
Frank Dekker <fdekker@123inkt.nl>
2022-12-10 18:32:32 +0100

    Fix tests

--- tests/Unit/Service/Git/Diff/UnifiedDiffBundlerTest.php
+++ tests/Unit/Service/Git/Diff/UnifiedDiffBundlerTest.php
@@ -3,7 +3,9 @@ declare(strict_types=1);
 
 namespace DR\Review\Tests\Unit\Service\Git\Diff;
 
+use DR\Review\Entity\Git\Diff\DiffBlock;
 use DR\Review\Entity\Git\Diff\DiffChange;
+use DR\Review\Entity\Git\Diff\DiffFile;
 use DR\Review\Entity\Git\Diff\DiffLine;
 use DR\Review\Git\Diff\DiffChangeBundler;
 use DR\Review\Git\Diff\DiffLineDiffer;
@@ -23,6 +25,34 @@ class UnifiedDiffBundlerTest extends AbstractTestCase
     {
         parent::setUp();
         $this->bundler = new UnifiedDiffBundler(new DiffLineComparator(), new DiffChangeBundler(), new DiffLineDiffer());
+    }
+
+    /**
+     * @covers ::bundleFile
+     */
+    public function testBundleFile(): void
+    {
+        $line1 = new DiffLine(DiffLine::STATE_UNCHANGED, [new DiffChange(DiffChange::UNCHANGED, 'line1')]);
+        $line2 = new DiffLine(DiffLine::STATE_REMOVED, [new DiffChange(DiffChange::REMOVED, 'line2')]);
+        $line3 = new DiffLine(DiffLine::STATE_ADDED, [new DiffChange(DiffChange::ADDED, 'line3')]);
+        $line4 = new DiffLine(DiffLine::STATE_UNCHANGED, [new DiffChange(DiffChange::UNCHANGED, 'line4')]);
+
+        $block        = new DiffBlock();
+        $block->lines = [$line1, $line2, $line3, $line4];
+        $file         = new DiffFile();
+        $file->addBlock($block);
+
+        $result = $this->bundler->bundleFile($file);
+        static::assertCount(1, $result->getBlocks());
+        static::assertCount(3, $result->getBlocks()[0]->lines);
+        $lines = $result->getBlocks()[0]->lines;
+
+        $expected = [
+            new DiffChange(DiffChange::UNCHANGED, 'line'),
+            new DiffChange(DiffChange::REMOVED, '2'),
+            new DiffChange(DiffChange::ADDED, '3'),
+        ];
+        static::assertEquals($expected, $lines[1]->changes->toArray());
     }
 
     /**
--- tests/Unit/ViewModel/App/Review/FileDiffViewModelTest.php
+++ tests/Unit/ViewModel/App/Review/FileDiffViewModelTest.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 namespace DR\Review\Tests\Unit\ViewModel\App\Review;
 
 use DigitalRevolution\AccessorPairConstraint\AccessorPairAsserter;
+use DR\Review\Entity\Git\Diff\DiffFile;
 use DR\Review\Tests\AbstractTestCase;
 use DR\Review\ViewModel\App\Review\FileDiffViewModel;
+use DR\Review\ViewModel\App\Review\ReviewDiffModeEnum;
 
 /**
  * @coversDefaultClass \DR\Review\ViewModel\App\Review\FileDiffViewModel
@@ -21,5 +23,14 @@ class FileDiffViewModelTest extends AbstractTestCase
     public function testAccessorPairs(): void
     {
         static::assertAccessorPairs(FileDiffViewModel::class);
+    }
+
+    /**
+     * @covers ::getDiffModes
+     */
+    public function testGetDiffModes(): void
+    {
+        $file = new FileDiffViewModel(new Difffile(), ReviewDiffModeEnum::INLINE);
+        static::assertSame(['unified', 'inline'], $file->getDiffModes());
     }
 }
