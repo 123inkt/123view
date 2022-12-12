00688e8702df0fa59fe3c959e89f76ad33aa73bf
Frank Dekker <fdekker@123inkt.nl>
2022-12-12 16:12:26 +0100

    Add redirect when user was approved

--- tests/Unit/Controller/App/User/UserApprovalPendingControllerTest.php
+++ tests/Unit/Controller/App/User/UserApprovalPendingControllerTest.php
@@ -3,20 +3,48 @@ declare(strict_types=1);
 
 namespace DR\Review\Tests\Unit\Controller\App\User;
 
+use DR\Review\Controller\AbstractController;
+use DR\Review\Controller\App\Review\ProjectsController;
 use DR\Review\Controller\App\User\UserApprovalPendingController;
-use DR\Review\Tests\AbstractTestCase;
+use DR\Review\Security\Role\Roles;
+use DR\Review\Tests\AbstractControllerTestCase;
+use PHPUnit\Framework\MockObject\MockObject;
+use Symfony\Bundle\SecurityBundle\Security;
 
 /**
  * @coversDefaultClass \DR\Review\Controller\App\User\UserApprovalPendingController
  */
-class UserApprovalPendingControllerTest extends AbstractTestCase
+class UserApprovalPendingControllerTest extends AbstractControllerTestCase
 {
+    private Security&MockObject $security;
+
+    public function setUp(): void
+    {
+        $this->security = $this->createMock(Security::class);
+        parent::setUp();
+    }
+
     /**
      * @covers ::__invoke
      */
     public function testInvoke(): void
     {
-        $controller = new UserApprovalPendingController();
-        static::assertSame([], $controller());
+        $this->security->expects(self::once())->method('isGranted')->with(Roles::ROLE_USER)->willReturn(false);
+        static::assertSame([], ($this->controller)());
+    }
+
+    /**
+     * @covers ::__invoke
+     */
+    public function testInvokeUserAlreadyHasAccess(): void
+    {
+        $this->security->expects(self::once())->method('isGranted')->with(Roles::ROLE_USER)->willReturn(true);
+        $this->expectRedirectToRoute(ProjectsController::class)->willReturn('url');
+        ($this->controller)();
+    }
+
+    public function getController(): AbstractController
+    {
+        return new UserApprovalPendingController($this->security);
     }
 }
