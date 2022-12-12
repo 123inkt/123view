f0ece6e89328deb91d4d87ba474895de9f96c72f
Frank Dekker <fdekker@123inkt.nl>
2022-12-12 16:23:03 +0100

    Add redirect when user was approved (#51)

--- src/Controller/App/User/UserApprovalPendingController.php
+++ src/Controller/App/User/UserApprovalPendingController.php
@@ -3,18 +3,31 @@ declare(strict_types=1);
 
 namespace DR\Review\Controller\App\User;
 
+use DR\Review\Controller\AbstractController;
+use DR\Review\Controller\App\Review\ProjectsController;
+use DR\Review\Security\Role\Roles;
 use Symfony\Bridge\Twig\Attribute\Template;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
-class UserApprovalPendingController
+class UserApprovalPendingController extends AbstractController
 {
+    public function __construct(private readonly Security $security)
+    {
+    }
+
     /**
-     * @return string[]
+     * @return string[]|RedirectResponse
      */
     #[Route('/app/user-approval-pending', self::class, methods: ['GET'])]
     #[Template('app/user/user.approval.pending.html.twig')]
-    public function __invoke(): array
+    public function __invoke(): array|RedirectResponse
     {
+        if ($this->security->isGranted(Roles::ROLE_USER)) {
+            return $this->redirectToRoute(ProjectsController::class);
+        }
+
         return [];
     }
 }
