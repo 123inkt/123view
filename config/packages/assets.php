0c1dd244a205896cd436edb6ca30329d6e28b029
Frank Dekker <fdekker@123inkt.nl>
2022-12-13 21:46:09 +0100

    Add cache buster to web assets (#61)

--- config/packages/assets.php
+++ config/packages/assets.php
@@ -4,5 +4,5 @@ declare(strict_types=1);
 use Symfony\Config\FrameworkConfig;
 
 return static function (FrameworkConfig $framework): void {
-    $framework->assets()->enabled(true);
+    $framework->assets()->enabled(true)->version('%env(APP_ASSET_VERSION)%');
 };
