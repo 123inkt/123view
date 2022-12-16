b2923e989cb5b82f1a9c61d337dcc1ec34d2a21a
Frank Dekker <fdekker@123inkt.nl>
2022-12-16 17:52:24 +0100

    Fetch revisions from more in the past

--- tests/Unit/Service/Parser/Fetch/GitFetchParserTest.php
+++ tests/Unit/Service/Parser/Fetch/GitFetchParserTest.php
@@ -3,12 +3,13 @@ declare(strict_types=1);
 
 namespace DR\Review\Tests\Unit\Service\Parser\Fetch;
 
+use DR\Review\Entity\Git\Fetch\BranchCreation;
+use DR\Review\Entity\Git\Fetch\BranchUpdate;
 use DR\Review\Service\Parser\Fetch\GitFetchParser;
 use DR\Review\Tests\AbstractTestCase;
 
 /**
  * @coversDefaultClass \DR\Review\Service\Parser\Fetch\GitFetchParser
- * @covers ::__construct
  */
 class GitFetchParserTest extends AbstractTestCase
 {
@@ -18,6 +19,13 @@ class GitFetchParserTest extends AbstractTestCase
     public function testParse(): void
     {
         $parser = new GitFetchParser();
-        $parser->parse(file_get_contents(__DIR__ . '/log.txt'));
+        $result = $parser->parse(file_get_contents(__DIR__ . '/log.txt'));
+
+        $expected = [
+            new BranchCreation('NewBranch', 'origin/NewBranch'),
+            new BranchUpdate('9a07ab4ebc', '060fab08bd', 'master', 'origin/master')
+        ];
+
+        static::assertEquals($expected, $result);
     }
 }
