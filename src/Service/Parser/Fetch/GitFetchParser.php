5ce2eb4fc698b946b1a2260101a09ee891d135d7
Frank Dekker <fdekker@123inkt.nl>
2022-12-16 17:33:36 +0100

    Fetch revisions from more in the past

--- src/Service/Parser/Fetch/GitFetchParser.php
+++ src/Service/Parser/Fetch/GitFetchParser.php
@@ -3,19 +3,26 @@ declare(strict_types=1);
 
 namespace DR\Review\Service\Parser\Fetch;
 
+use DR\Review\Entity\Git\Fetch\BranchCreation;
 use DR\Review\Entity\Git\Fetch\BranchUpdate;
 
 class GitFetchParser
 {
     /**
-     * @return BranchUpdate[]
+     * @return array<BranchCreation|BranchUpdate>
      */
     public function parse(string $fetchLog): array
     {
-        //    0058886bd1..f56b867839  local_branch_name -> origin/remove_branch_name
-        $count = preg_match_all('/^\s*(\w+)\.{2,3}(\w+)\s+(\S+)\s+->\s+(\S+)$/m', $fetchLog, $matches);
-
         $result = [];
+
+        // * [new branch]            NewBranch              -> origin/NewBranch
+        $count = preg_match_all('/^\s*\*?\s*\[new branch]\s+(\S+)\s+->\s+(\S+)\s*$/m', $fetchLog, $matches);
+        for ($i = 0; $i < $count; $i++) {
+            $result[] = new BranchCreation($matches[1][$i], $matches[2][$i]);
+        }
+
+        //    0058886bd1..f56b867839  local_branch_name -> origin/remote_branch_name
+        $count = preg_match_all('/^\s*(\w+)\.{2,3}(\w+)\s+(\S+)\s+->\s+(\S+)\s*$/m', $fetchLog, $matches);
         for ($i = 0; $i < $count; $i++) {
             $result[] = new BranchUpdate($matches[1][$i], $matches[2][$i], $matches[3][$i], $matches[4][$i]);
         }
