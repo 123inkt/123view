061c4d624ccf08bf4f2a87bbce29ed523fad88df
Frank Dekker <fdekker@123inkt.nl>
2022-12-18 19:14:33 +0100

    Add coverage GitLogService

--- tests/Unit/Service/Git/Log/GitLogServiceTest.php
+++ tests/Unit/Service/Git/Log/GitLogServiceTest.php
@@ -111,9 +111,35 @@ class GitLogServiceTest extends AbstractTestCase
         $logBuilder->expects(self::once())->method('since')->with($since)->willReturnSelf();
 
         $gitRepository->expects(static::once())->method('execute')->with($logBuilder)->willReturn('output');
-        $this->repositoryService->expects(static::once())->method('getRepository')->with('https://example.com', true)->willReturn($gitRepository);
+        $this->repositoryService->expects(static::once())->method('getRepository')->with('https://example.com')->willReturn($gitRepository);
         $this->logParser->expects(static::once())->method('parse')->with($repository, 'output', $limit)->willReturn($commits);
 
         $this->logFactory->getCommitsSince($repository, $since, $limit);
+    }
+
+    /**
+     * @covers ::getCommitsFromRange
+     * @throws Exception
+     */
+    public function testGetCommitsFromRange(): void
+    {
+        $commits    = [$this->createMock(Commit::class), $this->createMock(Commit::class)];
+        $repository = new Repository();
+        $repository->setUrl('https://example.com');
+
+        $logBuilder    = $this->createMock(GitLogCommandBuilder::class);
+        $gitRepository = $this->createMock(GitRepository::class);
+
+        $this->commandBuilderFactory->expects(self::once())->method('createLog')->willReturn($logBuilder);
+        $this->patternFactory->expects(self::once())->method('createPattern')->willReturn('pattern');
+        $logBuilder->expects(self::once())->method('noMerges')->willReturnSelf();
+        $logBuilder->expects(self::once())->method('hashRange')->with('foo', 'bar')->willReturnSelf();
+        $logBuilder->expects(self::once())->method('format')->with('pattern')->willReturnSelf();
+
+        $gitRepository->expects(static::once())->method('execute')->with($logBuilder)->willReturn('output');
+        $this->repositoryService->expects(static::once())->method('getRepository')->with('https://example.com')->willReturn($gitRepository);
+        $this->logParser->expects(static::once())->method('parse')->with($repository, 'output')->willReturn($commits);
+
+        static::assertSame($commits, $this->logFactory->getCommitsFromRange($repository, 'foo', 'bar'));
     }
 }
