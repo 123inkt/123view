d8b5da0b7b32cba9bf9ad4e158503cec051e0b1f
Frank Dekker <fdekker@123inkt.nl>
2022-12-19 20:34:41 +0100

    Change webhook activity to "text" field (#66)

---
+++ migrations/Version20221219182118.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221219182118 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE webhook_activity CHANGE request request TEXT NOT NULL, CHANGE response response TEXT NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE webhook_activity CHANGE request request VARCHAR(1000) NOT NULL, CHANGE response response VARCHAR(5000) NOT NULL');
+    }
+}
