337f6ad7a9b38b76b6600466304fad9c7e5a89c0
Frank Dekker <fdekker@123inkt.nl>
2022-12-24 23:57:53 +0100

    Rewrite Comment controller

---
+++ assets/lib/MentionsDropdown.ts
@@ -0,0 +1,72 @@
+import type User from './User';
+
+export default class MentionsDropdown {
+    private dropdown: HTMLElement;
+    private visible: boolean                    = false;
+    private users: User[] = [];
+    private selected: number                    = 0;
+
+    constructor(dropdown: HTMLElement) {
+        this.dropdown = dropdown;
+    }
+
+    public setUsers(users: User[]): void {
+        this.users    = users;
+        this.selected = 0;
+        this.update();
+    }
+
+    public show(): void {
+        this.visible                = true;
+        this.dropdown.style.display = 'block';
+    }
+
+    hide() {
+        this.visible                = false;
+        this.dropdown.style.display = '';
+        this.dropdown.innerHTML     = '';
+    }
+
+    public isVisible(): boolean {
+        return this.visible;
+    }
+
+    public selectNext(): void {
+        this.selected = Math.min(this.selected + 1, this.users.length - 1);
+        this.update();
+    }
+
+    public selectPrev(): void {
+        this.selected = Math.max(this.selected - 1, 0);
+        this.update();
+    }
+
+    public getSelectedUser(element?: HTMLElement): User | undefined {
+        if (element === undefined) {
+            return this.users[this.selected];
+        }
+
+        const userId = parseInt(element.dataset.userId ?? '0');
+        for (const user of this.users) {
+            if (user.id === userId) {
+                return user;
+            }
+        }
+        return undefined;
+    }
+
+    public addEventListener(eventName: string, callback: (event: Event) => void): void {
+        this.dropdown.addEventListener(eventName, callback);
+    }
+
+    private update(): void {
+        let html = '';
+        this.users.forEach((user, index) => {
+            const selected = this.selected === index;
+            html += `
+                <li><span class="dropdown-item ${selected ? 'active' : ''}" data-user-id="${user.id}">${user.name}</span></li>
+            `;
+        });
+        this.dropdown.innerHTML = html;
+    }
+}
