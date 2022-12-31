0f3ecce896e9e0cd5bc284cc06eb19f2a77f8e34
Frank Dekker <fdekker@123inkt.nl>
2022-12-31 10:21:20 +0100

    Change form submitter to be placed on the element that is expected to change.

--- assets/ts/controllers/form_submitter_controller.ts
+++ assets/ts/controllers/form_submitter_controller.ts
@@ -1,7 +1,20 @@
 import {Controller} from '@hotwired/stimulus';
 
-export default class extends Controller<HTMLFormElement> {
+export default class extends Controller<HTMLElement> {
     public connect(): void {
-        this.element.addEventListener('change', () => this.element.submit());
+        switch(this.element.tagName.toLowerCase()) {
+            case 'select':
+                this.element.addEventListener('change', this.submit.bind(this));
+                break;
+            case 'a':
+                this.element.addEventListener('click', this.submit.bind(this));
+                break;
+            default:
+                throw new Error('Unsupported element for FormSubmitter: ' + this.element.tagName);
+        }
+    }
+
+    private submit(event: Event): void {
+        (<HTMLElement>event.target).closest<HTMLFormElement>('form')?.submit();
     }
 }
