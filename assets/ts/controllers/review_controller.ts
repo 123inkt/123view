46ea09e2bcf1ec9f824f3925c232f7530919e20b
Frank Dekker <fdekker@123inkt.nl>
2023-01-01 16:28:54 +0100

    Cleanup

--- assets/ts/controllers/review_controller.ts
+++ assets/ts/controllers/review_controller.ts
@@ -28,45 +28,4 @@ export default class extends Controller {
             })
             .catch(Function.empty);
     }
-
-    // public editComment(event: Event): void {
-    //     const target   = this.getTarget(event);
-    //     const location = new URL(window.location.href);
-    //     location.searchParams.set('filePath', this.getFilePath());
-    //     location.searchParams.set('action', 'edit-comment:' + target.dataset.commentId);
-    //     (window as Window).location = location.toString();
-    // }
-    //
-    // public deleteComment(event: Event): void {
-    //     const target = this.getTarget(event);
-    //     if (confirm(target.dataset.confirmMessage)) {
-    //         target.closest('form')?.submit();
-    //     }
-    // }
-    //
-    // public replyToComment(event: Event): void {
-    //     const target   = this.getTarget(event);
-    //     const location = new URL(window.location.href);
-    //     location.searchParams.set('action', 'add-reply:' + target.dataset.replyTo);
-    //     (window as Window).location = location.toString();
-    // }
-
-    // public editReply(event: Event): void {
-    //     const target   = this.getTarget(event);
-    //     const location = new URL(window.location.href);
-    //     location.searchParams.set('filePath', this.getFilePath());
-    //     location.searchParams.set('action', 'edit-reply:' + target.dataset.replyId);
-    //     (window as Window).location = location.toString();
-    // }
-
-    public deleteReply(event: Event): void {
-        const target = this.getTarget(event);
-        if (confirm(target.dataset.confirmMessage)) {
-            target.closest('form')?.submit();
-        }
-    }
-
-    private getTarget(event: Event): HTMLElement {
-        return <HTMLElement>((<HTMLElement>event.target).closest('[data-action]'));
-    }
 }
