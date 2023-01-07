81d2b3d7ab5c8a5d3e7bac4851500cf07d6357b1
Frank Dekker <fdekker@123inkt.nl>
2023-01-07 17:50:08 +0100

    Add ui

---
+++ assets/ts/controllers/component/icon_checkbox_controller.ts
@@ -0,0 +1,12 @@
+import {Controller} from '@hotwired/stimulus';
+import Events from '../../lib/Events';
+
+export default class extends Controller {
+    public static targets = ['checkbox'];
+    private declare checkboxTarget: HTMLInputElement;
+
+    public onClick(event: Event): void {
+        Events.stop(event);
+        this.checkboxTarget.checked = this.checkboxTarget.checked === false;
+    }
+}
