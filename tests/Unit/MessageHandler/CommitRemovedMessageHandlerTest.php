292b6f87ce6801947e7f9c0a91388dcbd05ffab5
Frank Dekker <fdekker@123inkt.nl>
2023-01-11 20:38:31 +0100

    Cleanup visibilities for a revision

--- tests/Unit/MessageHandler/CommitRemovedMessageHandlerTest.php
+++ tests/Unit/MessageHandler/CommitRemovedMessageHandlerTest.php
@@ -7,11 +7,13 @@ use DR\Review\Doctrine\Type\CodeReviewStateType;
 use DR\Review\Entity\Repository\Repository;
 use DR\Review\Entity\Review\CodeReview;
 use DR\Review\Entity\Revision\Revision;
+use DR\Review\Entity\Revision\RevisionVisibility;
 use DR\Review\Message\Revision\CommitRemovedMessage;
 use DR\Review\MessageHandler\CommitRemovedMessageHandler;
 use DR\Review\Repository\Config\RepositoryRepository;
 use DR\Review\Repository\Review\CodeReviewRepository;
 use DR\Review\Repository\Revision\RevisionRepository;
+use DR\Review\Repository\Revision\RevisionVisibilityRepository;
 use DR\Review\Service\Webhook\ReviewEventService;
 use DR\Review\Tests\AbstractTestCase;
 use PHPUnit\Framework\MockObject\MockObject;
@@ -23,22 +25,25 @@ use Throwable;
  */
 class CommitRemovedMessageHandlerTest extends AbstractTestCase
 {
-    private RepositoryRepository&MockObject $repositoryRepository;
-    private RevisionRepository&MockObject   $revisionRepository;
-    private CodeReviewRepository&MockObject $reviewRepository;
-    private ReviewEventService&MockObject   $eventService;
-    private CommitRemovedMessageHandler     $messageHandler;
+    private RepositoryRepository&MockObject         $repositoryRepository;
+    private RevisionRepository&MockObject           $revisionRepository;
+    private RevisionVisibilityRepository&MockObject $visibilityRepository;
+    private CodeReviewRepository&MockObject         $reviewRepository;
+    private ReviewEventService&MockObject           $eventService;
+    private CommitRemovedMessageHandler             $messageHandler;
 
     public function setUp(): void
     {
         parent::setUp();
         $this->repositoryRepository = $this->createMock(RepositoryRepository::class);
         $this->revisionRepository   = $this->createMock(RevisionRepository::class);
+        $this->visibilityRepository = $this->createMock(RevisionVisibilityRepository::class);
         $this->reviewRepository     = $this->createMock(CodeReviewRepository::class);
         $this->eventService         = $this->createMock(ReviewEventService::class);
         $this->messageHandler       = new CommitRemovedMessageHandler(
             $this->repositoryRepository,
             $this->revisionRepository,
+            $this->visibilityRepository,
             $this->reviewRepository,
             $this->eventService
         );
@@ -70,7 +75,8 @@ class CommitRemovedMessageHandlerTest extends AbstractTestCase
         $repository->setId(123);
         $review = new CodeReview();
         $review->setId(789);
-        $revision = new Revision();
+        $visibility = new RevisionVisibility();
+        $revision   = new Revision();
         $revision->setId(456);
         $revision->setTitle('title');
         $revision->setReview($review);
@@ -83,6 +89,8 @@ class CommitRemovedMessageHandlerTest extends AbstractTestCase
             ->willReturn($revision);
         $this->reviewRepository->expects(self::once())->method('save')->with($review, true);
         $this->eventService->expects(self::once())->method('revisionRemovedFromReview')->with($review, $revision, CodeReviewStateType::OPEN);
+        $this->visibilityRepository->expects(self::once())->method('findBy')->willReturn([$visibility]);
+        $this->visibilityRepository->expects(self::once())->method('removeAll')->with([$visibility]);
         $this->revisionRepository->expects(self::once())->method('remove')->with($revision, true);
 
         ($this->messageHandler)(new CommitRemovedMessage(123, 'hash'));
@@ -109,6 +117,8 @@ class CommitRemovedMessageHandlerTest extends AbstractTestCase
             ->willReturn($revision);
         $this->reviewRepository->expects(self::never())->method('save');
         $this->eventService->expects(self::never())->method('revisionRemovedFromReview');
+        $this->visibilityRepository->expects(self::once())->method('findBy')->willReturn([]);
+        $this->visibilityRepository->expects(self::once())->method('removeAll')->with([]);
         $this->revisionRepository->expects(self::once())->method('remove')->with($revision, true);
 
         ($this->messageHandler)(new CommitRemovedMessage(123, 'hash'));
