363ee6d7f08837b75b3bf3a68d61ee309545dd6d
Frank Dekker <fdekker@123inkt.nl>
2023-01-13 20:50:26 +0100

    Improve git diff performance

--- tests/Unit/MessageHandler/DiffFileCacheMessageHandlerTest.php
+++ tests/Unit/MessageHandler/DiffFileCacheMessageHandlerTest.php
@@ -80,7 +80,7 @@ class DiffFileCacheMessageHandlerTest extends AbstractTestCase
         $file->filePathAfter = 'file-path-after';
 
         $this->reviewRepository->expects(self::once())->method('find')->with(123)->willReturn($review);
-        $this->diffService->expects(self::exactly(2))->method('getDiffFiles')->with($repository, [$revision])->willReturn([$file]);
+        $this->diffService->expects(self::once())->method('getDiffFiles')->with($repository, [$revision])->willReturn([$file]);
 
         $this->messageHandler->handleEvent(new ReviewCreated(123, 456));
     }
@@ -104,7 +104,7 @@ class DiffFileCacheMessageHandlerTest extends AbstractTestCase
         $file->filePathAfter = 'file-path-after';
 
         $this->reviewRepository->expects(self::once())->method('find')->with(123)->willReturn($review);
-        $this->diffService->expects(self::exactly(2))->method('getDiffFiles')->with($repository, [$revision])->willReturn([$file]);
+        $this->diffService->expects(self::once())->method('getDiffFiles')->with($repository, [$revision])->willReturn([$file]);
 
         $this->messageHandler->handleEvent(new ReviewCreated(123, 456));
     }
@@ -128,7 +128,7 @@ class DiffFileCacheMessageHandlerTest extends AbstractTestCase
         $file->filePathBefore = 'file-path-before';
 
         $this->reviewRepository->expects(self::once())->method('find')->with(123)->willReturn($review);
-        $this->diffService->expects(self::exactly(2))->method('getDiffFiles')->with($repository, [$revision])->willReturn([$file]);
+        $this->diffService->expects(self::once())->method('getDiffFiles')->with($repository, [$revision])->willReturn([$file]);
 
         $this->messageHandler->handleEvent(new ReviewCreated(123, 456));
     }
