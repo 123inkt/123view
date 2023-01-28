f1ab5ecacf00d17d2a06f5c304349ab0bd5bf193
Frank Dekker <fdekker@123inkt.nl>
2023-01-28 21:10:58 +0100

    Add newsfeed to projects page

--- src/ViewModelProvider/ProjectsViewModelProvider.php
+++ src/ViewModelProvider/ProjectsViewModelProvider.php
@@ -6,17 +6,12 @@ namespace DR\Review\ViewModelProvider;
 use Doctrine\DBAL\Exception;
 use DR\Review\Entity\User\User;
 use DR\Review\Message\Comment\CommentAdded;
-use DR\Review\Message\Comment\CommentReplyAdded;
 use DR\Review\Message\Comment\CommentResolved;
 use DR\Review\Message\Review\ReviewAccepted;
 use DR\Review\Message\Review\ReviewRejected;
 use DR\Review\Repository\Config\RepositoryRepository;
-use DR\Review\Repository\Review\CodeReviewActivityRepository;
 use DR\Review\Repository\Revision\RevisionRepository;
-use DR\Review\Service\CodeReview\Activity\CodeReviewActivityFormatter;
 use DR\Review\ViewModel\App\Review\ProjectsViewModel;
-use DR\Review\ViewModel\App\Review\Timeline\TimelineEntryViewModel;
-use DR\Review\ViewModel\App\Review\Timeline\TimelineViewModel;
 
 class ProjectsViewModelProvider
 {
@@ -24,15 +19,13 @@ class ProjectsViewModelProvider
         ReviewAccepted::NAME,
         ReviewRejected::NAME,
         CommentAdded::NAME,
-        CommentReplyAdded::NAME,
         CommentResolved::NAME,
     ];
 
     public function __construct(
         private readonly RepositoryRepository $repositoryRepository,
         private readonly RevisionRepository $revisionRepository,
-        private readonly CodeReviewActivityRepository $activityRepository,
-        private readonly CodeReviewActivityFormatter $activityFormatter,
+        private readonly ReviewTimelineViewModelProvider $timelineViewModelProvider,
         private readonly User $user,
     ) {
     }
@@ -42,19 +35,9 @@ class ProjectsViewModelProvider
      */
     public function getProjectsViewModel(): ProjectsViewModel
     {
-        $repositories  = $this->repositoryRepository->findBy(['active' => 1], ['displayName' => 'ASC']);
-        $revisionCount = $this->revisionRepository->getRepositoryRevisionCount();
-
-        $activities      = $this->activityRepository->findForUser($this->user->getId(), self::FEED_EVENTS);
-        $timelineEntries = [];
-        foreach ($activities as $activity) {
-            $message = $this->activityFormatter->format($activity);
-            if ($message === null) {
-                continue;
-            }
-            $timelineEntries[] = new TimelineEntryViewModel([$activity], $message, null);
-        }
-        $timelineViewModel = new TimelineViewModel($timelineEntries);
+        $repositories      = $this->repositoryRepository->findBy(['active' => 1], ['displayName' => 'ASC']);
+        $revisionCount     = $this->revisionRepository->getRepositoryRevisionCount();
+        $timelineViewModel = $this->timelineViewModelProvider->getTimelineViewModelForUser($this->user, self::FEED_EVENTS);
 
         return new ProjectsViewModel($repositories, $revisionCount, $timelineViewModel);
     }
--- src/ViewModelProvider/ReviewTimelineViewModelProvider.php
+++ src/ViewModelProvider/ReviewTimelineViewModelProvider.php
@@ -8,6 +8,7 @@ use DR\Review\Entity\User\User;
 use DR\Review\Message\Comment\CommentAdded;
 use DR\Review\Message\Comment\CommentReplyAdded;
 use DR\Review\Repository\Review\CodeReviewActivityRepository;
+use DR\Review\Repository\Review\CommentRepository;
 use DR\Review\Service\CodeReview\Activity\CodeReviewActivityFormatter;
 use DR\Review\ViewModel\App\Review\Timeline\TimelineEntryViewModel;
 use DR\Review\ViewModel\App\Review\Timeline\TimelineViewModel;
@@ -17,6 +18,7 @@ class ReviewTimelineViewModelProvider
     public function __construct(
         private readonly CodeReviewActivityRepository $activityRepository,
         private readonly CodeReviewActivityFormatter $activityFormatter,
+        private readonly CommentRepository $commentRepository,
         private readonly User $user
     ) {
     }
@@ -37,6 +39,33 @@ class ReviewTimelineViewModelProvider
             $comment = null;
             if ($activity->getEventName() === CommentAdded::NAME) {
                 $comment = $comments->get((int)$activity->getDataValue('commentId'));
+            }
+            $timelineEntries[] = new TimelineEntryViewModel([$activity], $message, $comment);
+        }
+
+        return new TimelineViewModel($timelineEntries);
+    }
+
+    /**
+     * @param string[] $events
+     */
+    public function getTimelineViewModelForUser(User $user, array $events): TimelineViewModel
+    {
+        $activities      = $this->activityRepository->findForUser($user->getId(), $events);
+        $timelineEntries = [];
+
+        // create TimelineEntryViewModel entries
+        foreach ($activities as $activity) {
+            $message = $this->activityFormatter->format($activity, $user);
+            if ($message === null) {
+                continue;
+            }
+            $comment = null;
+            if ($activity->getEventName() === CommentAdded::NAME) {
+                $comment = $this->commentRepository->find((int)$activity->getDataValue('commentId'));
+                if ($comment === null) {
+                    continue;
+                }
             }
             $timelineEntries[] = new TimelineEntryViewModel([$activity], $message, $comment);
         }
