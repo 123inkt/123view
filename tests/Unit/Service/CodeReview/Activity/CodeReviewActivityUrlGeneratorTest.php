b98b4c951bb4d9c296ca0966e5c911739d0b6b1a
Frank Dekker <fdekker@123inkt.nl>
2023-01-30 21:13:23 +0100

    Add custom generated url for activity

---
+++ tests/Unit/Service/CodeReview/Activity/CodeReviewActivityUrlGeneratorTest.php
@@ -0,0 +1,105 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\Review\Tests\Unit\Service\CodeReview\Activity;
+
+use DR\Review\Controller\App\Review\ReviewController;
+use DR\Review\Entity\Review\CodeReview;
+use DR\Review\Entity\Review\CodeReviewActivity;
+use DR\Review\Entity\Review\Comment;
+use DR\Review\Entity\Review\CommentReply;
+use DR\Review\Message\Comment\CommentAdded;
+use DR\Review\Message\Comment\CommentReplyAdded;
+use DR\Review\Message\Review\ReviewAccepted;
+use DR\Review\Repository\Review\CommentReplyRepository;
+use DR\Review\Service\CodeReview\Activity\CodeReviewActivityUrlGenerator;
+use DR\Review\Tests\AbstractTestCase;
+use PHPUnit\Framework\MockObject\MockObject;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+/**
+ * @coversDefaultClass \DR\Review\Service\CodeReview\Activity\CodeReviewActivityUrlGenerator
+ * @covers ::__construct
+ */
+class CodeReviewActivityUrlGeneratorTest extends AbstractTestCase
+{
+    private UrlGeneratorInterface&MockObject  $urlGenerator;
+    private CommentReplyRepository&MockObject $replyRepository;
+    private CodeReviewActivityUrlGenerator    $activityUrlGenerator;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->urlGenerator         = $this->createMock(UrlGeneratorInterface::class);
+        $this->replyRepository      = $this->createMock(CommentReplyRepository::class);
+        $this->activityUrlGenerator = new CodeReviewActivityUrlGenerator($this->urlGenerator, $this->replyRepository);
+    }
+
+    /**
+     * @covers ::generate
+     */
+    public function testGenerateDefaultActivity(): void
+    {
+        $review = new CodeReview();
+        $review->SetId(123);
+
+        $activity = new CodeReviewActivity();
+        $activity->setReview($review);
+        $activity->setEventName(ReviewAccepted::NAME);
+
+        $this->urlGenerator->expects(self::once())
+            ->method('generate')
+            ->with(ReviewController::class, ['review' => $review])
+            ->willReturn('url');
+
+        static::assertSame('url', $this->activityUrlGenerator->generate($activity));
+    }
+
+    /**
+     * @covers ::generate
+     */
+    public function testGenerateCommentActivity(): void
+    {
+        $review = new CodeReview();
+        $review->SetId(123);
+
+        $activity = new CodeReviewActivity();
+        $activity->setReview($review);
+        $activity->setData(['commentId' => 456, 'file' => 'filePath']);
+        $activity->setEventName(CommentAdded::NAME);
+
+        $this->urlGenerator->expects(self::once())
+            ->method('generate')
+            ->with(ReviewController::class, ['review' => $review, 'filePath' => 'filePath'])
+            ->willReturn('url');
+
+        static::assertSame('url#focus:comment:456', $this->activityUrlGenerator->generate($activity));
+    }
+
+    /**
+     * @covers ::generate
+     */
+    public function testGenerateReplyActivity(): void
+    {
+        $comment = new Comment();
+        $comment->setFilePath('filePath');
+        $reply = new CommentReply();
+        $reply->setComment($comment);
+
+        $review = new CodeReview();
+        $review->setId(123);
+
+        $activity = new CodeReviewActivity();
+        $activity->setReview($review);
+        $activity->setData(['commentId' => 456]);
+        $activity->setEventName(CommentReplyAdded::NAME);
+
+        $this->urlGenerator->expects(self::once())
+            ->method('generate')
+            ->with(ReviewController::class, ['review' => $review, 'filePath' => 'filePath'])
+            ->willReturn('url');
+        $this->replyRepository->expects(self::once())->method('find')->with(456)->willReturn($reply);
+
+        static::assertSame('url#focus:reply:456', $this->activityUrlGenerator->generate($activity));
+    }
+}
