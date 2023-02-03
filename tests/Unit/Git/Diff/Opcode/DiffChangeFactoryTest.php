8133ad787cb9cfb5e7b55948ecc183a602ed7582
Frank Dekker <fdekker@123inkt.nl>
2023-02-03 21:18:09 +0100

    Add coverage

---
+++ tests/Unit/Git/Diff/Opcode/DiffChangeFactoryTest.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\Review\Tests\Unit\Git\Diff\Opcode;
+
+use DR\Review\Entity\Git\Diff\DiffChange;
+use DR\Review\Git\Diff\Opcode\DiffChangeFactory;
+use DR\Review\Tests\AbstractTestCase;
+
+/**
+ * @coversDefaultClass \DR\Review\Git\Diff\Opcode\DiffChangeFactory
+ * @covers ::__construct
+ */
+class DiffChangeFactoryTest extends AbstractTestCase
+{
+    private DiffChangeFactory $factory;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->factory = new DiffChangeFactory();
+    }
+
+    /**
+     * @covers ::transform
+     */
+    public function testTransform(): void
+    {
+        $lineBefore = 'my first greatest line';
+        // $lineAfter  = 'my very first line';
+        $opcodes = "c3i5:very c6d9c4";
+
+        $expected = [
+            new DiffChange(DiffChange::UNCHANGED, 'my '),
+            new DiffChange(DiffChange::ADDED, 'very '),
+            new DiffChange(DiffChange::UNCHANGED, 'first '),
+            new DiffChange(DiffChange::REMOVED, 'greatest '),
+            new DiffChange(DiffChange::UNCHANGED, 'line'),
+        ];
+
+        static::assertEquals($expected, $this->factory->createFromOpcodes($lineBefore, $opcodes));
+    }
+}
