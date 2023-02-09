1f8c3f2b2f44cadfed6e25dfcfc0aeeb44b41f1e
Frank Dekker <fdekker@123inkt.nl>
2023-02-09 20:43:56 +0100

    Add coverage

--- tests/Unit/Git/Diff/Opcode/DiffChangeOptimizerTest.php
+++ tests/Unit/Git/Diff/Opcode/DiffChangeOptimizerTest.php
@@ -42,6 +42,26 @@ class DiffChangeOptimizerTest extends AbstractTestCase
     /**
      * @covers ::optimize
      * @covers ::extractCommonPreSuffix
+     */
+    public function testOptimizeWithUnchanged(): void
+    {
+        $changeA = new DiffChange(DiffChange::UNCHANGED, 'first ');
+        $changeB = new DiffChange(DiffChange::REMOVED, 'start aaa end');
+        $changeC = new DiffChange(DiffChange::ADDED, 'start bbb end');
+        $changeD = new DiffChange(DiffChange::UNCHANGED, ' last');
+
+        $collection = $this->optimizer->optimize([$changeA, $changeB, $changeC, $changeD]);
+        static::assertCount(4, $collection);
+
+        static::assertSame('first start ', $collection->get(0)->code);
+        static::assertSame('aaa', $collection->get(1)->code);
+        static::assertSame('bbb', $collection->get(2)->code);
+        static::assertSame(' end last', $collection->get(3)->code);
+    }
+
+    /**
+     * @covers ::optimize
+     * @covers ::extractCommonPreSuffix
      * @covers ::extractCommonInfix
      */
     public function testOptimizeShouldSkipInfixExtraction(): void
