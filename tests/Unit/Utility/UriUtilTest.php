63c6f091073cf35ba3b8f0737f524eb001346cbe
Frank Dekker <fdekker@123inkt.nl>
2023-02-10 12:26:21 +0100

    Add coverage UriUtil

---
+++ tests/Unit/Utility/UriUtilTest.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\Review\Tests\Unit\Utility;
+
+use DR\Review\Tests\AbstractTestCase;
+use DR\Review\Utility\UriUtil;
+use League\Uri\Uri;
+
+/**
+ * @coversDefaultClass \DR\Review\Utility\UriUtil
+ * @covers ::__construct
+ */
+class UriUtilTest extends AbstractTestCase
+{
+    /**
+     * @covers ::credentials
+     */
+    public function testCredentials(): void
+    {
+        static::assertSame([null, null], UriUtil::credentials(null));
+    }
+
+    /**
+     * @covers ::credentials
+     */
+    public function testCredentialsUriWithoutCredentials(): void
+    {
+        static::assertSame([null, null], UriUtil::credentials(Uri::createFromString('https://example.com')));
+    }
+
+    /**
+     * @covers ::credentials
+     */
+    public function testCredentialsUriWithUsername(): void
+    {
+        static::assertSame(['shërlock', null], UriUtil::credentials(Uri::createFromString('https://sh%C3%ABrlock@example.com')));
+    }
+
+    /**
+     * @covers ::credentials
+     */
+    public function testCredentialsUriWithCredentials(): void
+    {
+        static::assertSame(['sherlock', 'passw*rd'], UriUtil::credentials(Uri::createFromString('https://sherlock:passw%2Ard@example.com')));
+    }
+}
