36bbf07e24da833d472a8ce64e291be53c606131
Frank Dekker <fdekker@123inkt.nl>
2023-02-10 22:01:29 +0100

    Add coverage

---
+++ tests/Unit/Form/Repository/RepositoryUrlTypeTest.php
@@ -0,0 +1,66 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\Review\Tests\Unit\Form\Repository;
+
+use DR\Review\Entity\Repository\Repository;
+use DR\Review\Form\Repository\RepositoryUrlType;
+use DR\Review\Tests\AbstractTestCase;
+use DR\Review\Transformer\RepositoryUrlTransformer;
+use League\Uri\Uri;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\Debug\OptionsResolverIntrospector;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * @coversDefaultClass \DR\Review\Form\Repository\RepositoryUrlType
+ * @covers ::__construct
+ */
+class RepositoryUrlTypeTest extends AbstractTestCase
+{
+    /**
+     * @covers ::buildForm
+     */
+    public function testBuildForm(): void
+    {
+        $builder = $this->createMock(FormBuilderInterface::class);
+
+        $builder->expects(self::exactly(3))
+            ->method('add')
+            ->withConsecutive(
+                ['url', UrlType::class],
+                ['username', TextType::class],
+                ['password', PasswordType::class],
+            )->willReturnSelf();
+        $builder->expects(self::once())->method('addModelTransformer')->with(static::isInstanceOf(RepositoryUrlTransformer::class));
+
+        $type = new RepositoryUrlType();
+        $type->buildForm($builder, []);
+    }
+
+    /**
+     * @covers ::configureOptions
+     */
+    public function testConfigureOptions(): void
+    {
+        $resolver     = new OptionsResolver();
+        $introspector = new OptionsResolverIntrospector($resolver);
+
+        $type = new RepositoryUrlType();
+        $type->configureOptions($resolver);
+
+        $callback = $introspector->getDefault('setter');
+        static::assertIsCallable($callback);
+
+        // test callback
+        $repository = new Repository();
+        $repository->setUrl(Uri::createFromString('https://sherlock:holmes@example.com'));
+        $uri = Uri::createFromString('https://watson@example.com');
+        $callback($repository, $uri);
+
+        static::assertSame('https://watson:holmes@example.com', (string)$repository->getUrl());
+    }
+}
