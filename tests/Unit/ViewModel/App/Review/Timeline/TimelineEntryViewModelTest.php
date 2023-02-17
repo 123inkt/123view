0e8d741166c9f59346df6c5f479c425560aba555
Frank Dekker <fdekker@123inkt.nl>
2023-02-17 22:43:49 +0100

    Add keybinding navigation

--- tests/Unit/ViewModel/App/Review/Timeline/TimelineEntryViewModelTest.php
+++ tests/Unit/ViewModel/App/Review/Timeline/TimelineEntryViewModelTest.php
@@ -3,6 +3,9 @@ declare(strict_types=1);
 
 namespace DR\Review\Tests\Unit\ViewModel\App\Review\Timeline;
 
+use DR\Review\Entity\Review\CodeReviewActivity;
+use DR\Review\Entity\Review\Comment;
+use DR\Review\Entity\Revision\Revision;
 use DR\Review\Tests\AbstractTestCase;
 use DR\Review\ViewModel\App\Review\Timeline\TimelineEntryViewModel;
 
@@ -17,6 +20,13 @@ class TimelineEntryViewModelTest extends AbstractTestCase
      */
     public function testAccessorPairs(): void
     {
-        static::assertAccessorPairs(TimelineEntryViewModel::class);
+        $activity = new CodeReviewActivity();
+        $comment  = new Comment();
+        $revision = new Revision();
+
+        $model = new TimelineEntryViewModel([$activity], 'message', 'url');
+
+        static::assertSame($comment, $model->setComment($comment)->getComment());
+        static::assertSame($revision, $model->setRevision($revision)->getRevision());
     }
 }
