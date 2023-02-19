b9101eafe50e084a02a0f03924fa4598cabe3579
Frank Dekker <fdekker@123inkt.nl>
2023-02-19 10:20:44 +0100

    Add user access token

--- src/Repository/User/UserAccessTokenRepository.php
+++ src/Repository/User/UserAccessTokenRepository.php
@@ -1,10 +1,11 @@
 <?php
+declare(strict_types=1);
 
 namespace DR\Review\Repository\User;
 
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
-use DR\Review\Entity\User\AccessToken;
+use DR\Review\Doctrine\EntityRepository\ServiceEntityRepository;
+use DR\Review\Entity\User\UserAccessToken as AccessToken;
 
 /**
  * @extends ServiceEntityRepository<AccessToken>
@@ -15,51 +16,11 @@ use DR\Review\Entity\User\AccessToken;
  */
 class UserAccessTokenRepository extends ServiceEntityRepository
 {
+    /**
+     * @codeCoverageIgnore
+     */
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, AccessToken::class);
     }
-
-    public function save(AccessToken $entity, bool $flush = false): void
-    {
-        $this->getEntityManager()->persist($entity);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-    }
-
-    public function remove(AccessToken $entity, bool $flush = false): void
-    {
-        $this->getEntityManager()->remove($entity);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-    }
-
-//    /**
-//     * @return UserAccessToken[] Returns an array of UserAccessToken objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('a')
-//            ->andWhere('a.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('a.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?UserAccessToken
-//    {
-//        return $this->createQueryBuilder('a')
-//            ->andWhere('a.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
