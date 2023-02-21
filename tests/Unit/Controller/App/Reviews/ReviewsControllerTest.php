429aef7896bf8d391a7cf4b41a8a5366a3c57b73
Frank Dekker <fdekker@123inkt.nl>
2023-02-21 20:43:56 +0100

    Add coverage

--- tests/Unit/Controller/App/Reviews/ReviewsControllerTest.php
+++ tests/Unit/Controller/App/Reviews/ReviewsControllerTest.php
@@ -3,13 +3,10 @@ declare(strict_types=1);
 
 namespace DR\Review\Tests\Unit\Controller\App\Reviews;
 
-use Doctrine\ORM\Tools\Pagination\Paginator;
 use DR\Review\Controller\AbstractController;
 use DR\Review\Controller\App\Reviews\ReviewsController;
 use DR\Review\Entity\Repository\Repository;
-use DR\Review\Entity\User\User;
 use DR\Review\Model\Page\Breadcrumb;
-use DR\Review\Repository\Review\CodeReviewQueryBuilder;
 use DR\Review\Request\Reviews\SearchReviewsRequest;
 use DR\Review\Service\Page\BreadcrumbFactory;
 use DR\Review\Tests\AbstractControllerTestCase;
@@ -23,12 +20,12 @@ use PHPUnit\Framework\MockObject\MockObject;
  */
 class ReviewsControllerTest extends AbstractControllerTestCase
 {
-    private ReviewsViewModelProvider&MockObject $reviewRepository;
+    private ReviewsViewModelProvider&MockObject $viewModelProvider;
     private BreadcrumbFactory&MockObject        $breadcrumbFactory;
 
     public function setUp(): void
     {
-        $this->reviewRepository  = $this->createMock(ReviewsViewModelProvider::class);
+        $this->viewModelProvider = $this->createMock(ReviewsViewModelProvider::class);
         $this->breadcrumbFactory = $this->createMock(BreadcrumbFactory::class);
         parent::setUp();
     }
@@ -38,37 +35,28 @@ class ReviewsControllerTest extends AbstractControllerTestCase
      */
     public function testInvoke(): void
     {
-        $user       = new User();
         $repository = new Repository();
         $repository->setId(123);
         $repository->setDisplayName('repository');
-        $paginator  = $this->createMock(Paginator::class);
+        $viewModel  = $this->createMock(ReviewsViewModel::class);
         $breadcrumb = new Breadcrumb('label', 'url');
+        $request    = $this->createMock(SearchReviewsRequest::class);
 
-        $this->expectGetUser($user);
-        $this->reviewRepository->expects(self::once())
-            ->method('getPaginatorForSearchQuery')
-            ->with($user, 123, 5, 'search', CodeReviewQueryBuilder::ORDER_CREATE_TIMESTAMP)
-            ->willReturn($paginator);
+        $this->viewModelProvider
+            ->expects(self::once())
+            ->method('getReviewsViewModel')
+            ->with($request, $repository)
+            ->willReturn($viewModel);
         $this->breadcrumbFactory->expects(self::once())->method('createForReviews')->with($repository)->willReturn([$breadcrumb]);
-
-        $request = $this->createMock(SearchReviewsRequest::class);
-        $request->method('getPage')->willReturn(5);
-        $request->method('getOrderBy')->willReturn(CodeReviewQueryBuilder::ORDER_CREATE_TIMESTAMP);
-        $request->method('getSearchQuery')->willReturn('search');
 
         $result = ($this->controller)($request, $repository);
         static::assertSame('Repository', $result['page_title']);
         static::assertSame([$breadcrumb], $result['breadcrumbs']);
-
-        /** @var ReviewsViewModel $viewModel */
-        $viewModel = $result['reviewsModel'];
-        static::assertSame($repository, $viewModel->repository);
-        static::assertSame('search', $viewModel->searchQuery);
+        static::assertSame($viewModel, $result['reviewsModel']);
     }
 
     public function getController(): AbstractController
     {
-        return new ReviewsController($this->reviewRepository, $this->breadcrumbFactory);
+        return new ReviewsController($this->viewModelProvider, $this->breadcrumbFactory);
     }
 }
