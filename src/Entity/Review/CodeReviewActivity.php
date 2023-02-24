6cd075e7c0b86d5a075ff955a3b2443016610576
Frank Dekker <fdekker@123inkt.nl>
2023-02-24 09:16:55 +0100

    Add range filter for dates

--- src/Entity/Review/CodeReviewActivity.php
+++ src/Entity/Review/CodeReviewActivity.php
@@ -3,8 +3,8 @@ declare(strict_types=1);
 
 namespace DR\Review\Entity\Review;
 
-use ApiPlatform\Doctrine\Orm\Filter\DateFilter;
 use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Doctrine\Orm\Filter\RangeFilter;
 use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
@@ -34,7 +34,7 @@ use DR\Review\Security\Role\Roles;
         'eventName'     => 'exact',
     ]
 )]
-#[ApiFilter(DateFilter::class, properties: ['createTimestamp'])]
+#[ApiFilter(RangeFilter::class, properties: ['createTimestamp'])]
 #[ApiFilter(
     OrderFilter::class,
     properties: [
