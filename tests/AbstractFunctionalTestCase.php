f8106cb0023bfa10d28a5c7b4fa1d0a6f48f55ab
Frank Dekker <fdekker@123inkt.nl>
2023-02-25 15:34:20 +0100

    Revert "Add trait"
    This reverts commit a4a6f22335e3a45b3c890d753f3750e3235f1683.

--- tests/AbstractFunctionalTestCase.php
+++ tests/AbstractFunctionalTestCase.php
@@ -4,12 +4,21 @@ declare(strict_types=1);
 
 namespace DR\Review\Tests;
 
+use Doctrine\DBAL\Connection;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use DR\Review\Utility\Assert;
 use Exception;
+use Liip\TestFixturesBundle\Services\DatabaseToolCollection;
+use Liip\TestFixturesBundle\Services\DatabaseTools\AbstractDatabaseTool;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 abstract class AbstractFunctionalTestCase extends WebTestCase
 {
-    use TestFixturesTrait;
+    protected ?AbstractDatabaseTool   $databaseTool;
+    protected ?EntityManagerInterface $entityManager;
+    protected KernelBrowser           $client;
 
     /**
      * @see https://latteandcode.medium.com/symfony-improving-your-tests-with-doctrinefixturesbundle-1a37b704ac05
@@ -18,7 +27,17 @@ abstract class AbstractFunctionalTestCase extends WebTestCase
     protected function setUp(): void
     {
         parent::setUp();
-        $this->setupFixtures();
+        $this->client        = static::createClient(['environment' => 'test', 'debug' => 'false']);
+        $this->databaseTool  = Assert::instanceOf(DatabaseToolCollection::class, static::getContainer()->get(DatabaseToolCollection::class))->get();
+        $doctrine            = Assert::instanceOf(ManagerRegistry::class, static::getContainer()->get('doctrine'));
+        $this->entityManager = Assert::instanceOf(EntityManagerInterface::class, $doctrine->getManager());
+
+        Assert::instanceOf(Connection::class, $doctrine->getConnection())->beginTransaction();
+
+        $fixtures = $this->getFixtures();
+        if (count($fixtures) > 0) {
+            $this->databaseTool->loadFixtures($fixtures);
+        }
     }
 
     /**
@@ -26,7 +45,31 @@ abstract class AbstractFunctionalTestCase extends WebTestCase
      */
     protected function tearDown(): void
     {
+        // this call will shutdown the kernel and close any open connections. Ensuring the rollback of any transactions.
         parent::tearDown();
-        $this->teardownFixtures();
+        $this->databaseTool = null;
+        $this->entityManager?->close();
+        $this->entityManager = null;
     }
+
+    /**
+     * @template T of object
+     *
+     * @param class-string<T> $serviceId
+     *
+     * @return T
+     * @throws Exception
+     */
+    protected static function getService(string $serviceId, ?string $alias = null): object
+    {
+        /** @var T $service */
+        $service = self::getContainer()->get($alias ?? $serviceId);
+
+        return $service;
+    }
+
+    /**
+     * @return class-string[]
+     */
+    abstract protected function getFixtures(): array;
 }
