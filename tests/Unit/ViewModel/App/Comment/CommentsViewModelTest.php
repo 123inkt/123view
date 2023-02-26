6ab87a680afeaf50145c95f62b973307c7e93506
Frank Dekker <fdekker@123inkt.nl>
2023-02-26 11:00:55 +0100

    Add coverage

--- tests/Unit/ViewModel/App/Comment/CommentsViewModelTest.php
+++ tests/Unit/ViewModel/App/Comment/CommentsViewModelTest.php
@@ -3,8 +3,10 @@ declare(strict_types=1);
 
 namespace DR\Review\Tests\Unit\ViewModel\App\Comment;
 
+use DR\Review\Doctrine\Type\CommentStateType;
 use DR\Review\Entity\Git\Diff\DiffLine;
 use DR\Review\Entity\Review\Comment;
+use DR\Review\Entity\Review\CommentVisibility;
 use DR\Review\Tests\AbstractTestCase;
 use DR\Review\ViewModel\App\Comment\CommentsViewModel;
 
@@ -20,8 +22,47 @@ class CommentsViewModelTest extends AbstractTestCase
     public function testGetDetachedComments(): void
     {
         $comment   = new Comment();
-        $viewModel = new CommentsViewModel([], [$comment]);
+        $viewModel = new CommentsViewModel([], [$comment], CommentVisibility::ALL);
         static::assertSame([$comment], $viewModel->detachedComments);
+    }
+
+    /**
+     * @covers ::isCommentVisible
+     */
+    public function testIsCommentVisibleAll(): void
+    {
+        $commentA = (new Comment())->setState(CommentStateType::OPEN);
+        $commentB = (new Comment())->setState(CommentStateType::RESOLVED);
+
+        $viewModel = new CommentsViewModel([], [], CommentVisibility::ALL);
+        static::assertTrue($viewModel->isCommentVisible($commentA));
+        static::assertTrue($viewModel->isCommentVisible($commentB));
+    }
+
+    /**
+     * @covers ::isCommentVisible
+     */
+    public function testIsCommentVisibleUnresolvedOnly(): void
+    {
+        $commentA = (new Comment())->setState(CommentStateType::OPEN);
+        $commentB = (new Comment())->setState(CommentStateType::RESOLVED);
+
+        $viewModel = new CommentsViewModel([], [], CommentVisibility::UNRESOLVED);
+        static::assertTrue($viewModel->isCommentVisible($commentA));
+        static::assertFalse($viewModel->isCommentVisible($commentB));
+    }
+
+    /**
+     * @covers ::isCommentVisible
+     */
+    public function testIsCommentVisibleNone(): void
+    {
+        $commentA = (new Comment())->setState(CommentStateType::OPEN);
+        $commentB = (new Comment())->setState(CommentStateType::RESOLVED);
+
+        $viewModel = new CommentsViewModel([], [], CommentVisibility::NONE);
+        static::assertFalse($viewModel->isCommentVisible($commentA));
+        static::assertFalse($viewModel->isCommentVisible($commentB));
     }
 
     /**
@@ -35,7 +76,7 @@ class CommentsViewModelTest extends AbstractTestCase
         $comment  = new Comment();
         $comments = [spl_object_hash($lineA) => [$comment]];
 
-        $viewModel = new CommentsViewModel($comments, []);
+        $viewModel = new CommentsViewModel($comments, [], CommentVisibility::ALL);
         static::assertSame([$comment], $viewModel->getComments($lineA));
         static::assertSame([], $viewModel->getComments($lineB));
     }
