6fc239d769fe2493fe234d58743a407e48efea39
Frank Dekker <fdekker@123inkt.nl>
2023-02-26 16:35:06 +0100

    Add coverage

---
+++ tests/Unit/Controller/App/Review/Comment/GetCommentCountControllerTest.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\Review\Tests\Unit\Controller\App\Review\Comment;
+
+use DR\Review\Controller\AbstractController;
+use DR\Review\Controller\App\Review\Comment\GetCommentCountController;
+use DR\Review\Doctrine\Type\CommentStateType;
+use DR\Review\Entity\Review\CodeReview;
+use DR\Review\Entity\Review\Comment;
+use DR\Review\Tests\AbstractControllerTestCase;
+use Nette\Utils\Json;
+use Nette\Utils\JsonException;
+use PHPUnit\Framework\Attributes\CoversClass;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+#[CoversClass(GetCommentCountController::class)]
+class GetCommentCountControllerTest extends AbstractControllerTestCase
+{
+    /**
+     * @throws JsonException
+     */
+    public function testInvoke(): void
+    {
+        $commentA = (new Comment())->setState(CommentStateType::OPEN);
+        $commentB = (new Comment())->setState(CommentStateType::RESOLVED);
+        $review   = new CodeReview();
+        $review->getComments()->add($commentA);
+        $review->getComments()->add($commentB);
+
+        /** @var JsonResponse $response */
+        $response = ($this->controller)($review);
+        static::assertSame(['total' => 2, 'open' => 1, 'resolved' => 1], Json::decode($response->getContent(), true));
+    }
+
+    public function getController(): AbstractController
+    {
+        return new GetCommentCountController();
+    }
+}
