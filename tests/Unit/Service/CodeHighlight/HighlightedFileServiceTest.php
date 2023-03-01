43226ab0b2bd1ac234f29dd9549455bda869e934
Frank Dekker <fdekker@123inkt.nl>
2023-03-01 21:17:26 +0100

    Add coverage

--- tests/Unit/Service/CodeHighlight/HighlightedFileServiceTest.php
+++ tests/Unit/Service/CodeHighlight/HighlightedFileServiceTest.php
@@ -7,16 +7,14 @@ use DR\Review\Entity\Git\Diff\DiffBlock;
 use DR\Review\Entity\Git\Diff\DiffChange;
 use DR\Review\Entity\Git\Diff\DiffFile;
 use DR\Review\Entity\Git\Diff\DiffLine;
-use DR\Review\Model\Review\Highlight\HighlightedFile;
 use DR\Review\Service\CodeHighlight\FilenameToLanguageTranslator;
 use DR\Review\Service\CodeHighlight\HighlightedFileService;
 use DR\Review\Service\CodeHighlight\HighlightHtmlLineSplitter;
 use DR\Review\Tests\AbstractTestCase;
-use DR\Review\Utility\Assert;
 use Exception;
-use Highlight\Highlighter;
 use PHPUnit\Framework\MockObject\MockObject;
-use stdClass;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
 
 /**
  * @coversDefaultClass \DR\Review\Service\CodeHighlight\HighlightedFileService
@@ -25,17 +23,17 @@ use stdClass;
 class HighlightedFileServiceTest extends AbstractTestCase
 {
     private FilenameToLanguageTranslator&MockObject $translator;
-    private Highlighter&MockObject                  $highlighter;
+    private HttpClientInterface&MockObject          $httpClient;
     private HighlightHtmlLineSplitter&MockObject    $splitter;
     private HighlightedFileService                  $service;
 
     public function setUp(): void
     {
         parent::setUp();
-        $this->translator  = $this->createMock(FilenameToLanguageTranslator::class);
-        $this->highlighter = $this->createMock(Highlighter::class);
-        $this->splitter    = $this->createMock(HighlightHtmlLineSplitter::class);
-        $this->service     = new HighlightedFileService($this->translator, $this->highlighter, $this->splitter);
+        $this->translator = $this->createMock(FilenameToLanguageTranslator::class);
+        $this->httpClient = $this->createMock(HttpClientInterface::class);
+        $this->splitter   = $this->createMock(HighlightHtmlLineSplitter::class);
+        $this->service    = new HighlightedFileService($this->translator, $this->httpClient, $this->splitter);
     }
 
     /**
@@ -49,7 +47,7 @@ class HighlightedFileServiceTest extends AbstractTestCase
 
         $this->translator->expects(self::once())->method('translate')->with('')->willReturn(null);
 
-        $this->highlighter->expects(self::never())->method('highlight');
+        $this->httpClient->expects(self::never())->method('request');
         $this->splitter->expects(self::never())->method('split');
 
         static::assertNull($this->service->fromDiffFile($file));
@@ -68,16 +66,17 @@ class HighlightedFileServiceTest extends AbstractTestCase
         $file->filePathAfter = '/path/to/file.xml';
         $file->addBlock($block);
 
-        $result        = new stdClass();
-        $result->value = 'highlighted-data';
+        $response = $this->createMock(ResponseInterface::class);
+        $response->expects(self::once())->method('getContent')->willReturn('highlighted-data');
 
         $this->translator->expects(self::once())->method('translate')->with('/path/to/file.xml')->willReturn('xml');
-
-        $this->highlighter->expects(self::once())->method('highlight')->with('xml', 'file-data')->willReturn($result);
+        $this->httpClient->expects(self::once())->method('request')
+            ->with('POST', '', ['query' => ['language' => 'xml'], 'body' => 'file-data'])
+            ->willReturn($response);
         $this->splitter->expects(self::once())->method('split')->with('highlighted-data')->willReturn(['highlighted', 'data']);
 
-        $actual   = $this->service->fromDiffFile($file);
-        $expected = new HighlightedFile(Assert::isString($file->filePathAfter), ['highlighted', 'data']);
-        static::assertEquals($expected, $actual);
+        $actual = $this->service->fromDiffFile($file);
+        static::assertSame($file->filePathAfter, $actual->filePath);
+        static::assertSame(['highlighted', 'data'], ($actual->closure)());
     }
 }
