db9287f1ae13f53c9586e448a6180e2f3eff4510
Frank Dekker <fdekker@123inkt.nl>
2023-03-21 16:19:43 +0100

    Make web profile controllers not stateless (#205)

--- config/routes/dev/web_profiler.php
+++ config/routes/dev/web_profiler.php
@@ -5,9 +5,7 @@ use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return static function (RoutingConfigurator $routes): void {
     $routes->import('@WebProfilerBundle/Resources/config/routing/wdt.xml')
-        ->prefix('/_wdt')
-        ->stateless(true);
+        ->prefix('/_wdt');
     $routes->import('@WebProfilerBundle/Resources/config/routing/profiler.xml')
-        ->prefix('/_profiler')
-        ->stateless(true);
+        ->prefix('/_profiler');
 };
