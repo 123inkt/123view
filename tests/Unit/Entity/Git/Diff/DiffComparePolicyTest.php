b52b03f78b57b8627280c1026ae8b608e0df25b0
Frank Dekker <fdekker@123inkt.nl>
2023-03-26 21:02:46 +0200

    Add coverage

---
+++ tests/Unit/Entity/Git/Diff/DiffComparePolicyTest.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\Review\Tests\Unit\Entity\Git\Diff;
+
+use DR\JBDiff\ComparisonPolicy;
+use DR\Review\Entity\Git\Diff\DiffComparePolicy;
+use DR\Review\Tests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(DiffComparePolicy::class)]
+class DiffComparePolicyTest extends AbstractTestCase
+{
+    public function testValues(): void
+    {
+        static::assertSame(['all', 'trim', 'ignore'], DiffComparePolicy::values());
+    }
+
+    public function testToComparisonPolicy(): void
+    {
+        static::assertSame(ComparisonPolicy::DEFAULT, DiffComparePolicy::ALL->toComparisonPolicy());
+        static::assertSame(ComparisonPolicy::TRIM_WHITESPACES, DiffComparePolicy::TRIM->toComparisonPolicy());
+        static::assertSame(ComparisonPolicy::IGNORE_WHITESPACES, DiffComparePolicy::IGNORE->toComparisonPolicy());
+    }
+}
