c20d2c7b4a799ec7b9132ea722acc2f6176e1920
Frank Dekker <fdekker@123inkt.nl>
2023-04-06 10:47:23 +0200

    Fix auto detect dark mode

--- assets/app.light.ts
+++ assets/app.light.ts
@@ -1,4 +1,2 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-utilities.css';
 import 'highlight.js/scss/intellij-light.scss';
 import './styles/theme.light.scss';
