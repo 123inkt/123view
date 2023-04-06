59987d318e5980d9b6868154403288e41578cfe7
Frank Dekker <fdekker@123inkt.nl>
2023-04-06 10:50:47 +0200

    Fix auto detect dark mode (#224)

--- assets/app.dark.ts
+++ assets/app.dark.ts
@@ -1,4 +1,2 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-utilities.css';
 import 'highlight.js/scss/github-dark.scss';
 import './styles/theme.dark.scss';
--- assets/app.ts
+++ assets/app.ts
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-utilities.css';
 import 'bootstrap-icons/font/bootstrap-icons.scss';
 
 // import css
