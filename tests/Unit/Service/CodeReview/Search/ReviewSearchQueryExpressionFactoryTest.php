bd11a5f9bde1d59690b460615d2f8cb9ce48eb6a
Frank Dekker <fdekker@123inkt.nl>
2023-04-09 19:16:26 +0200

    Add coverage

--- tests/Unit/Service/CodeReview/Search/ReviewSearchQueryExpressionFactoryTest.php
+++ tests/Unit/Service/CodeReview/Search/ReviewSearchQueryExpressionFactoryTest.php
@@ -57,19 +57,75 @@ class ReviewSearchQueryExpressionFactoryTest extends AbstractTestCase
         static::assertSame(['state1' => 'open'], $collection->toArray());
     }
 
-    public function testCreateReviewStateExpression(): void
+    public function testCreateReviewAuthorExpressionShouldNotMatch(): void
     {
+        static::assertNull($this->expressionFactory->createReviewAuthorExpression(new MatchWord('query'), new ArrayCollection()));
+        static::assertNull($this->expressionFactory->createReviewAuthorExpression(new MatchFilter('foo', 'bar'), new ArrayCollection()));
     }
 
-    public function testCreateReviewAuthorExpression(): void
+    public function testCreateReviewAuthorExpressionWithMe(): void
     {
+        $this->user->setEmail('email');
+        $collection = new ArrayCollection();
+
+        $expression = $this->expressionFactory->createReviewAuthorExpression(new MatchFilter('author', 'me'), $collection);
+
+        static::assertEquals(new Expr\Comparison('rv.authorEmail', '=', ':authorEmail1'), $expression);
+        static::assertSame(['authorEmail1' => 'email'], $collection->toArray());
+    }
+
+    public function testCreateReviewAuthorExpressionWithUser(): void
+    {
+        $collection = new ArrayCollection();
+
+        $expression = $this->expressionFactory->createReviewAuthorExpression(new MatchFilter('author', 'sherlock'), $collection);
+
+        static::assertEquals(
+            new Expr\Orx(
+                [
+                    new Expr\Comparison('rv.authorEmail', 'LIKE', ':authorEmail1'),
+                    new Expr\Comparison('rv.authorName', 'LIKE', ':authorEmail1')
+                ]
+            ),
+            $expression
+        );
+        static::assertSame(['authorEmail1' => '%sherlock%'], $collection->toArray());
+    }
+
+    public function testCreateReviewReviewerExpressionShouldNotMatch(): void
+    {
+        static::assertNull($this->expressionFactory->createReviewReviewerExpression(new MatchWord('query'), new ArrayCollection()));
+        static::assertNull($this->expressionFactory->createReviewReviewerExpression(new MatchFilter('foo', 'bar'), new ArrayCollection()));
+    }
+
+    public function testCreateReviewReviewerExpressionWithMe(): void
+    {
+        $collection = new ArrayCollection();
+        $expression = $this->expressionFactory->createReviewReviewerExpression(new MatchFilter('reviewer', 'me'), $collection);
+
+        $this->user->setEmail('email');
+        static::assertEquals(new Expr\Comparison('u.email', '=', ':reviewerEmail1'), $expression);
+        static::assertSame(['reviewerEmail1' => 'email'], $collection->toArray());
+    }
+
+    public function testCreateReviewReviewerExpressionWithUser(): void
+    {
+        $collection = new ArrayCollection();
+        $expression = $this->expressionFactory->createReviewReviewerExpression(new MatchFilter('reviewer', 'sherlock'), $collection);
+
+        static::assertEquals(
+            new Expr\Orx(
+                [
+                    new Expr\Comparison('u.email', 'LIKE', ':reviewerEmail1'),
+                    new Expr\Comparison('u.name', 'LIKE', ':reviewerEmail1')
+                ]
+            ),
+            $expression
+        );
+        static::assertSame(['reviewerEmail1' => '%sherlock%'], $collection->toArray());
     }
 
     public function testCreateSearchExpression(): void
-    {
-    }
-
-    public function testCreateReviewReviewerExpression(): void
     {
     }
 }
