b079813f6d2ed2821a106eeb944b0205c44ba801
Frank Dekker <fdekker@123inkt.nl>
2023-04-28 20:32:45 +0200

    Add openapi docs for test coverage endpoint

--- src/Controller/Api/UploadTestCoverage.php
+++ src/Controller/Api/UploadTestCoverage.php
@@ -3,17 +3,17 @@ declare(strict_types=1);
 
 namespace DR\Review\Controller\Api;
 
-use DR\Review\Entity\Repository\Repository;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UploadTestCoverage
 {
-    #[Route('/api/test-coverage', name: self::class, methods: 'POST')]
-    public function __invoke(Repository $repository): JsonResponse
+    #[Route('/api/test-coverage', name: self::class, methods: ['GET', 'POST'])]
+    public function __invoke(): JsonResponse
     {
         return new JsonResponse(
             [
+                'repository' => 'test'
 
             ]
         );
