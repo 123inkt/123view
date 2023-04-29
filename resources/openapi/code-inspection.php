31e151e5ada71ef87a11c0a7bcb050fcca839b33
Frank Dekker <fdekker@123inkt.nl>
2023-04-29 09:57:02 +0200

    Add openapi docs

--- resources/openapi/code-inspection.php
+++ resources/openapi/code-inspection.php
@@ -2,16 +2,34 @@
 
 declare(strict_types=1);
 
+use ApiPlatform\OpenApi\Model\MediaType;
 use ApiPlatform\OpenApi\Model\Operation;
 use ApiPlatform\OpenApi\Model\Parameter;
 use ApiPlatform\OpenApi\Model\PathItem;
+use ApiPlatform\OpenApi\Model\RequestBody;
+use ApiPlatform\OpenApi\Model\Response;
 use ApiPlatform\OpenApi\OpenApi;
 
 // phpcs:ignorefile
 return static function (OpenApi $openApi) {
+    $content = new ArrayObject();
+    $content->offsetSet(
+        "application/json",
+        new MediaType(new ArrayObject(['type' => 'string']), "{}")
+    );
+    $content->offsetSet(
+        "application/xml",
+        new MediaType(new ArrayObject(['type' => 'string']), "<?xml version='1.0' encoding='UTF-8'?>\n><coverage></coverage>")
+    );
+
     $operation = new Operation(
         operationId: 'uploadCodeInspection',
         tags       : ['Report'],
+        responses  : [
+                         204 => new Response('When the request has been successfully processed'),
+                         400 => new Response('On any invalid arguments'),
+                         404 => new Response('When the repository with the given `name` cant be found'),
+                     ],
         summary    : "Upload code inspection report",
         description: "Upload code inspection report in varies formats",
         parameters : [
@@ -45,7 +63,12 @@ return static function (OpenApi $openApi) {
                                      ]
                          )
 
-                     ]
+                     ],
+        requestBody: new RequestBody(
+                         'The `xml` or `json` of the code inspection specified by the `format`.',
+                         $content,
+                         true
+                     )
     );
 
     $openApi->getPaths()->addPath('/api/report/code-inspection/{repository}', new PathItem(post: $operation));
