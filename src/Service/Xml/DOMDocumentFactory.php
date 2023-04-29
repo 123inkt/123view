b6c00acdfd808d3b354f31d6b2bde3629e56a189
Frank Dekker <fdekker@123inkt.nl>
2023-04-29 17:02:07 +0200

    Add coverage

--- src/Service/Xml/DOMDocumentFactory.php
+++ src/Service/Xml/DOMDocumentFactory.php
@@ -15,7 +15,10 @@ class DOMDocumentFactory
     public function createFromString(string $data): DOMDocument
     {
         $document = new DOMDocument();
-        if ($document->loadXML($data) === false) {
+
+        // avoid libxml triggering the error handler
+        libxml_use_internal_errors(true);
+        if (@$document->loadXML($data) === false) {
             $error = libxml_get_last_error();
             $error === false ? throw new ParseException('Unable to read xml. Invalid format') : throw new XMLException($error);
         }
