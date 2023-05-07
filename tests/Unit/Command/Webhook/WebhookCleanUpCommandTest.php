8c27621940f39fb5ee71b60d1fff4d1681e0e3eb
Frank Dekker <fdekker@123inkt.nl>
2023-05-07 16:42:20 +0200

    Add coverage

---
+++ tests/Unit/Command/Webhook/WebhookCleanUpCommandTest.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\Review\Tests\Unit\Command\Webhook;
+
+use DR\Review\Command\Webhook\WebhookCleanUpCommand;
+use DR\Review\Repository\Webhook\WebhookActivityRepository;
+use DR\Review\Tests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\MockObject\MockObject;
+use Symfony\Component\Console\Tester\CommandTester;
+
+#[CoversClass(WebhookCleanUpCommand::class)]
+class WebhookCleanUpCommandTest extends AbstractTestCase
+{
+    private WebhookActivityRepository&MockObject $activityRepository;
+    private WebhookCleanUpCommand                $command;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->activityRepository = $this->createMock(WebhookActivityRepository::class);
+        $this->command            = new WebhookCleanUpCommand($this->activityRepository);
+    }
+
+    public function testExecute(): void
+    {
+        $this->activityRepository->expects(self::once())->method('cleanUp')->willReturn(5);
+
+        $tester = new CommandTester($this->command);
+        $tester->execute([]);
+
+        static::assertSame(0, $tester->getStatusCode());
+        static::assertSame("Removed 5 webhook activities", trim($tester->getDisplay()));
+    }
+}
