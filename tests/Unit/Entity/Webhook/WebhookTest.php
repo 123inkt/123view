c4fa2590b30e7a9dbc80654b45dca2c6ff782976
Frank Dekker <fdekker@123inkt.nl>
2023-05-07 16:49:39 +0200

    Add coverage

--- tests/Unit/Entity/Webhook/WebhookTest.php
+++ tests/Unit/Entity/Webhook/WebhookTest.php
@@ -7,41 +7,17 @@ use DigitalRevolution\AccessorPairConstraint\Constraint\ConstraintConfig;
 use Doctrine\Common\Collections\ArrayCollection;
 use DR\Review\Entity\Webhook\Webhook;
 use DR\Review\Tests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
 
-/**
- * @coversDefaultClass \DR\Review\Entity\Webhook\Webhook
- * @covers ::__construct
- */
+#[CoversClass(Webhook::class)]
 class WebhookTest extends AbstractTestCase
 {
-    /**
-     * @covers ::setId
-     * @covers ::getId
-     * @covers ::isEnabled
-     * @covers ::setEnabled
-     * @covers ::getUrl
-     * @covers ::setUrl
-     * @covers ::getRetries
-     * @covers ::setRetries
-     * @covers ::isVerifySsl
-     * @covers ::setVerifySsl
-     * @covers ::getHeaders
-     * @covers ::setHeaders
-     * @covers ::getRepository
-     * @covers ::setRepository
-     * @covers ::getActivities
-     * @covers ::setActivities
-     */
     public function testAccessorPairs(): void
     {
-        $config = (new ConstraintConfig())->setExcludedMethods(['getActivities']);
+        $config = (new ConstraintConfig())->setExcludedMethods(['getActivities', 'getRepositories']);
         static::assertAccessorPairs(Webhook::class, $config);
     }
 
-    /**
-     * @covers ::getActivities
-     * @covers ::setActivities
-     */
     public function testReviewers(): void
     {
         $collection = new ArrayCollection();
