a9c43ae8d672bf13264f48d8e1980706bbd14760
Frank Dekker <fdekker@123inkt.nl>
2023-05-07 17:03:19 +0200

    Add coverage

---
+++ tests/Unit/Repository/Webhook/WebhookRepositoryTest.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\Review\Tests\Unit\Repository\Webhook;
+
+use DR\Review\Repository\Config\RepositoryRepository;
+use DR\Review\Repository\Webhook\WebhookRepository;
+use DR\Review\Tests\AbstractRepositoryTestCase;
+use DR\Review\Tests\DataFixtures\RepositoryFixtures;
+use DR\Review\Tests\DataFixtures\WebhookFixtures;
+use DR\Review\Utility\Assert;
+use Exception;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(WebhookRepository::class)]
+class WebhookRepositoryTest extends AbstractRepositoryTestCase
+{
+    /**
+     * @throws Exception
+     */
+    public function testFindByRepositoryId(): void
+    {
+        $repository        = Assert::notNull(self::getService(RepositoryRepository::class)->findOneBy(['name' => 'repository']));
+        $webhookRepository = self::getService(WebhookRepository::class);
+
+        static::assertCount(0, $webhookRepository->findByRepositoryId(-1));
+        static::assertCount(1, $webhookRepository->findByRepositoryId($repository->getId()));
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testFindByRepositoryIdWithEnabled(): void
+    {
+        $repository        = Assert::notNull(self::getService(RepositoryRepository::class)->findOneBy(['name' => 'repository']));
+        $webhookRepository = self::getService(WebhookRepository::class);
+
+        static::assertCount(1, $webhookRepository->findByRepositoryId($repository->getId(), true));
+        static::assertCount(0, $webhookRepository->findByRepositoryId($repository->getId(), false));
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getFixtures(): array
+    {
+        return [WebhookFixtures::class, RepositoryFixtures::class];
+    }
+}
