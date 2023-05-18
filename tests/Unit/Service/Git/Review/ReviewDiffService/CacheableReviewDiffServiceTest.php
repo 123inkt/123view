796757ab99cb4212b85e59110b3b64a4547cf5e0
Frank Dekker <fdekker@123inkt.nl>
2023-05-18 17:15:58 +0200

    Add coverage

--- tests/Unit/Service/Git/Review/ReviewDiffService/CacheableReviewDiffServiceTest.php
+++ tests/Unit/Service/Git/Review/ReviewDiffService/CacheableReviewDiffServiceTest.php
@@ -11,14 +11,12 @@ use DR\Review\Service\Git\Review\FileDiffOptions;
 use DR\Review\Service\Git\Review\ReviewDiffService\CacheableReviewDiffService;
 use DR\Review\Service\Git\Review\ReviewDiffService\ReviewDiffServiceInterface;
 use DR\Review\Tests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\MockObject\MockObject;
 use Symfony\Contracts\Cache\CacheInterface;
 use Throwable;
 
-/**
- * @coversDefaultClass \DR\Review\Service\Git\Review\ReviewDiffService\CacheableReviewDiffService
- * @covers ::__construct
- */
+#[CoversClass(CacheableReviewDiffService::class)]
 class CacheableReviewDiffServiceTest extends AbstractTestCase
 {
     private CacheInterface&MockObject             $cache;
@@ -34,7 +32,6 @@ class CacheableReviewDiffServiceTest extends AbstractTestCase
     }
 
     /**
-     * @covers ::getDiffForRevisions
      * @throws Throwable
      */
     public function testGetDiffFilesWithoutRevisions(): void
@@ -45,7 +42,6 @@ class CacheableReviewDiffServiceTest extends AbstractTestCase
     }
 
     /**
-     * @covers ::getDiffForRevisions
      * @throws Throwable
      */
     public function testGetDiffFilesWithRevisions(): void
@@ -59,10 +55,33 @@ class CacheableReviewDiffServiceTest extends AbstractTestCase
 
         $this->cache->expects(self::once())
             ->method('get')
-            ->with('diff-files-123-hash-fdo-20-trim')
+            ->with('diff-files-revision-123-hash-fdo-20-trim')
             ->willReturnCallback(static fn($repository, $callback) => $callback());
-        $this->diffService->expects(self::once())->method('getDiffForRevisions')->with($repository, [$revision])->willReturn([$diffFile]);
+        $this->diffService->expects(self::once())->method('getDiffForRevisions')->with($repository, [$revision], $options)->willReturn([$diffFile]);
 
         static::assertSame([$diffFile], $this->service->getDiffForRevisions($repository, [$revision], $options));
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function testGetDiffForBranch(): void
+    {
+        $repository = new Repository();
+        $repository->setId(123);
+        $revision = new Revision();
+        $revision->setCommitHash('hash');
+        $diffFile = new DiffFile();
+        $options  = new FileDiffOptions(20, DiffComparePolicy::TRIM);
+
+        $this->cache->expects(self::once())
+            ->method('get')
+            ->with('4040ca4757de48a78fe1faea962462df96f5ce9f9c3d270cb2d53d3b4bcccafb')
+            ->willReturnCallback(static fn($repository, $callback) => $callback());
+        $this->diffService->expects(self::once())->method('getDiffForBranch')
+            ->with($repository, [$revision], 'branch', $options)
+            ->willReturn([$diffFile]);
+
+        static::assertSame([$diffFile], $this->service->getDiffForBranch($repository, [$revision], 'branch', $options));
     }
 }
