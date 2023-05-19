18285ea2e6a9371df35d78c736f1cd9e8203b546
Frank Dekker <fdekker@123inkt.nl>
2023-05-19 10:29:43 +0200

    Show existing review in branch list

--- src/ViewModelProvider/ProjectBranchesViewModelProvider.php
+++ src/ViewModelProvider/ProjectBranchesViewModelProvider.php
@@ -3,15 +3,20 @@ declare(strict_types=1);
 
 namespace DR\Review\ViewModelProvider;
 
+use DR\Review\Doctrine\Type\CodeReviewType;
 use DR\Review\Entity\Repository\Repository;
 use DR\Review\Exception\RepositoryException;
+use DR\Review\Repository\Review\CodeReviewRepository;
 use DR\Review\Service\Git\Branch\GitBranchService;
+use DR\Review\Utility\Arrays;
 use DR\Review\ViewModel\App\Project\ProjectBranchesViewModel;
 
 class ProjectBranchesViewModelProvider
 {
-    public function __construct(private readonly GitBranchService $branchService)
-    {
+    public function __construct(
+        private readonly GitBranchService $branchService,
+        private readonly CodeReviewRepository $reviewRepository,
+    ) {
     }
 
     /**
@@ -22,6 +27,9 @@ class ProjectBranchesViewModelProvider
         $branches       = $this->branchService->getRemoteBranches($repository);
         $mergedBranches = $this->branchService->getRemoteBranches($repository, true);
 
-        return new ProjectBranchesViewModel($repository, $branches, $mergedBranches);
+        $branchReviews = $this->reviewRepository->findBy(['repository' => $repository, 'type' => CodeReviewType::BRANCH, 'referenceId' => $branches]);
+        $branchReviews = Arrays::reindex($branchReviews, static fn($review) => (string)$review->getReferenceId());
+
+        return new ProjectBranchesViewModel($repository, $branches, $mergedBranches, $branchReviews);
     }
 }
