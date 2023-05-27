95ce59e922d2cb93dd748a62069aa47db98b61ab
Frank Dekker <fdekker@123inkt.nl>
2023-05-27 18:21:17 +0200

    Add coverage

---
+++ tests/Unit/Request/Report/UploadCodeCoverageRequestTest.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\Review\Tests\Unit\Request\Report;
+
+use DigitalRevolution\SymfonyRequestValidation\ValidationRules;
+use DigitalRevolution\SymfonyValidationShorthand\Rule\InvalidRuleException;
+use DR\Review\Request\Report\UploadCodeCoverageRequest;
+use DR\Review\Tests\Unit\Request\AbstractRequestTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+/**
+ * @extends AbstractRequestTestCase<UploadCodeCoverageRequest>
+ */
+#[CoversClass(UploadCodeCoverageRequest::class)]
+class UploadCodeCoverageRequestTest extends AbstractRequestTestCase
+{
+    public function testGetBasePath(): void
+    {
+        $this->request->query->set('basePath', 'basePath');
+        static::assertSame('basePath', $this->validatedRequest->getBasePath());
+    }
+
+    public function testGetFormat(): void
+    {
+        $this->request->query->set('format', 'gitlab');
+        static::assertSame('gitlab', $this->validatedRequest->getFormat());
+    }
+
+    public function testGetContent(): void
+    {
+        static::assertSame('content', $this->validatedRequest->getData());
+    }
+
+    /**
+     * @throws InvalidRuleException
+     */
+    public function testGetValidationRules(): void
+    {
+        $expected = new ValidationRules(
+            [
+                'query' => [
+                    'basePath' => 'string',
+                    'format'   => 'string|in:cobertura'
+                ]
+            ]
+        );
+        $this->expectGetValidationRules($expected);
+
+        $this->validatedRequest->validate();
+    }
+
+    protected static function getClassToTest(): string
+    {
+        return UploadCodeCoverageRequest::class;
+    }
+}
