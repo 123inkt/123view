9b2c9e058c4e6bd97608a88c81d92c3f23f52f23
Frank Dekker <fdekker@123inkt.nl>
2023-06-30 19:03:09 +0200

    Add coverage LineReferenceMatcher

--- tests/Unit/Service/CodeReview/LineReferenceMatcherTest.php
+++ tests/Unit/Service/CodeReview/LineReferenceMatcherTest.php
@@ -14,8 +14,54 @@ use PHPUnit\Framework\Attributes\CoversClass;
 #[CoversClass(LineReferenceMatcher::class)]
 class LineReferenceMatcherTest extends AbstractTestCase
 {
-    public function testExactMatch(): void
+    private LineReferenceMatcher $matcher;
+
+    protected function setUp(): void
     {
+        parent::setUp();
+        $this->matcher = new LineReferenceMatcher();
+    }
+
+    public function testExactMatchAddedLine(): void
+    {
+        $line                  = new DiffLine(DiffLine::STATE_ADDED, []);
+        $line->lineNumberAfter = 123;
+
+        $reference = new LineReference('/file/path', 10, 20, 123);
+
+        static::assertSame($line, $this->matcher->exactMatch([$line], $reference));
+    }
+
+    public function testExactMatchRemovedLine(): void
+    {
+        $line                   = new DiffLine(DiffLine::STATE_CHANGED, []);
+        $line->lineNumberBefore = 123;
+
+        $reference = new LineReference('/file/path', 123, 0, 0);
+
+        static::assertSame($line, $this->matcher->exactMatch([$line], $reference));
+    }
+
+    public function testExactMatchModifiedLine(): void
+    {
+        $line                   = new DiffLine(DiffLine::STATE_CHANGED, []);
+        $line->lineNumberBefore = 123;
+        $line->lineNumberAfter  = 456;
+
+        $reference = new LineReference('/file/path', 123, 0, 456);
+
+        static::assertSame($line, $this->matcher->exactMatch([$line], $reference));
+    }
+
+    public function testExactMatchNoMatch(): void
+    {
+        $line                   = new DiffLine(DiffLine::STATE_CHANGED, []);
+        $line->lineNumberBefore = 123;
+        $line->lineNumberAfter  = 456;
+
+        $reference = new LineReference('/file/path', 123, 456, 789);
+
+        static::assertNull($this->matcher->exactMatch([$line], $reference));
     }
 
     public function testBestEffortMatch(): void
