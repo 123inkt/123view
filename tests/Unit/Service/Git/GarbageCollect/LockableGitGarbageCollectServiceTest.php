c37bf3bce7fd4213b1049e35103074929ffe69c8
Frank Dekker <fdekker@123inkt.nl>
2023-07-19 20:44:18 +0200

    Add coverage LockableGitGarbageCollectService

---
+++ tests/Unit/Service/Git/GarbageCollect/LockableGitGarbageCollectServiceTest.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\Review\Tests\Unit\Service\Git\GarbageCollect;
+
+use DR\Review\Entity\Repository\Repository;
+use DR\Review\Service\Git\GarbageCollect\GitGarbageCollectService;
+use DR\Review\Service\Git\GarbageCollect\LockableGitGarbageCollectService;
+use DR\Review\Service\Git\GitRepositoryLockManager;
+use DR\Review\Tests\AbstractTestCase;
+use Exception;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\MockObject\MockObject;
+
+#[CoversClass(LockableGitGarbageCollectService::class)]
+class LockableGitGarbageCollectServiceTest extends AbstractTestCase
+{
+    private GitRepositoryLockManager&MockObject $lockManager;
+    private GitGarbageCollectService&MockObject $garbageCollectService;
+    private LockableGitGarbageCollectService    $service;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->lockManager           = $this->createMock(GitRepositoryLockManager::class);
+        $this->garbageCollectService = $this->createMock(GitGarbageCollectService::class);
+        $this->service               = new LockableGitGarbageCollectService($this->lockManager, $this->garbageCollectService);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testGarbageCollect(): void
+    {
+        $repository = new Repository();
+
+        $this->lockManager->expects(self::once())
+            ->method('start')
+            ->with($repository)
+            ->willReturnCallback(static fn($repository, $callback) => $callback());
+        $this->garbageCollectService->expects(self::once())->method('garbageCollect')->with($repository, 'date');
+
+        $this->service->garbageCollect($repository, 'date');
+    }
+}
