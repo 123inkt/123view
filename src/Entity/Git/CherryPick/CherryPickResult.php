88e97cccf938776747a85a81f0428e33355fdfd9
Frank Dekker <fdekker@123inkt.nl>
2023-07-29 17:34:10 +0200

    Add coverage

--- src/Entity/Git/CherryPick/CherryPickResult.php
+++ src/Entity/Git/CherryPick/CherryPickResult.php
@@ -6,6 +6,8 @@ namespace DR\Review\Entity\Git\CherryPick;
 class CherryPickResult
 {
     /**
+     * @codeCoverageIgnore
+     *
      * @param string[] $conflicts
      */
     public function __construct(public readonly bool $completed, public readonly array $conflicts = [])
