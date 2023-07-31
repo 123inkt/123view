40b8e5efdd7c68ad250d43da49abfb15efc57065
Frank Dekker <fdekker@123inkt.nl>
2023-07-31 21:56:45 +0200

    Split off build directory. Convert request id config from yaml to php.

--- public/index.php
+++ public/index.php
@@ -6,5 +6,5 @@ use DR\Review\Kernel;
 require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], false);
+    return new Kernel($context['APP_ENV'], (bool)$context['APP_DEBUG']);
 };
