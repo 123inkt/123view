7e19a39e69c78423bb35269a7e34e307bfcfb8d0
Frank Dekker <fdekker@123inkt.nl>
2023-08-01 17:54:06 +0200

    Ensure git cherry pick parser handler file renames

--- tests/Unit/Service/Git/CherryPick/GitCherryPickParserTest.php
+++ tests/Unit/Service/Git/CherryPick/GitCherryPickParserTest.php
@@ -27,6 +27,17 @@ class GitCherryPickParserTest extends AbstractTestCase
         static::assertSame([], $result->conflicts);
     }
 
+    public function testParseWithRename(): void
+    {
+        $data = "some random text\n";
+        $data .= "  CONFLICT (rename/delete): src/Tests/Unit/AbstractTest.php renamed to src/Tests/Unit/AbstractTestCase.php in c61a64dc1de.";
+        $data .= "some random text\n";
+
+        $result = $this->parser->parse($data);
+        static::assertFalse($result->completed);
+        static::assertSame(['src/Tests/Unit/AbstractTestCase.php'], $result->conflicts);
+    }
+
     public function testParseWithConflicts(): void
     {
         $data = "some random text\n";
