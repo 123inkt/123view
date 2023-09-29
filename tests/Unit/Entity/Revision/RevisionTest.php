0a92113497f93162cfe1b289b1da9263f354affd
Frank Dekker <fdekker@123inkt.nl>
2023-09-29 19:59:47 +0200

    Add coverage

--- tests/Unit/Entity/Revision/RevisionTest.php
+++ tests/Unit/Entity/Revision/RevisionTest.php
@@ -5,32 +5,11 @@ namespace DR\Review\Tests\Unit\Entity\Revision;
 
 use DR\Review\Entity\Revision\Revision;
 use DR\Review\Tests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
 
-/**
- * @coversDefaultClass \DR\Review\Entity\Revision\Revision
- */
+#[CoversClass(Revision::class)]
 class RevisionTest extends AbstractTestCase
 {
-    /**
-     * @covers ::setId
-     * @covers ::getId
-     * @covers ::getCommitHash
-     * @covers ::setCommitHash
-     * @covers ::getTitle
-     * @covers ::setTitle
-     * @covers ::getDescription
-     * @covers ::setDescription
-     * @covers ::getAuthorEmail
-     * @covers ::setAuthorEmail
-     * @covers ::getAuthorName
-     * @covers ::setAuthorName
-     * @covers ::getCreateTimestamp
-     * @covers ::setCreateTimestamp
-     * @covers ::getRepository
-     * @covers ::setRepository
-     * @covers ::getReview
-     * @covers ::setReview
-     */
     public function testAccessorPairs(): void
     {
         static::assertNull((new Revision())->getId());
