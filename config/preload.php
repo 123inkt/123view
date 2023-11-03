7ada9d8718602836ff61e084aaedbd525a1e487d
Frank Dekker <fdekker@123inkt.nl>
2023-11-03 23:53:34 +0100

    Fix preload path (#469)

--- config/preload.php
+++ config/preload.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types=1);
 
-if (file_exists(dirname(__DIR__) . '/var/cache/prod/DR_Review_KernelProdContainer.preload.php')) {
-    require dirname(__DIR__) . '/var/cache/prod/DR_Review_KernelProdContainer.preload.php';
+if (file_exists(dirname(__DIR__) . '/var/build/prod/DR_Review_KernelProdContainer.preload.php')) {
+    require dirname(__DIR__) . '/var/build/prod/DR_Review_KernelProdContainer.preload.php';
 }
