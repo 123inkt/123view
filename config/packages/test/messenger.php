8dbe799bce6ecf520b02ba7d2378d013f3c8db0a
Frank Dekker <fdekker@123inkt.nl>
2023-11-05 14:43:15 +0100

    Add coverage

--- config/packages/test/messenger.php
+++ config/packages/test/messenger.php
@@ -5,7 +5,6 @@ use Symfony\Config\FrameworkConfig;
 
 return static function (FrameworkConfig $framework): void {
     $messenger = $framework->messenger();
-    $messenger->enabled(false);
     $messenger->transport('async_messages')->dsn('sync://');
     $messenger->transport('async_revisions')->dsn('sync://');
     $messenger->transport('async_delay_mail')->dsn('sync://');
