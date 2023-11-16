ff896162074bfae0679efd8965fe042b241517e8
Frank Dekker <fdekker@123inkt.nl>
2023-11-16 21:37:19 +0100

    Add coverage

--- tests/Unit/Service/Mail/CommitMailServiceTest.php
+++ tests/Unit/Service/Mail/CommitMailServiceTest.php
@@ -6,38 +6,41 @@ namespace DR\Review\Tests\Unit\Service\Mail;
 use DateInterval;
 use DatePeriod;
 use DateTime;
+use DR\Review\Doctrine\Type\MailThemeType;
 use DR\Review\Entity\Notification\Recipient;
 use DR\Review\Entity\Notification\Rule;
 use DR\Review\Entity\Notification\RuleConfiguration;
+use DR\Review\Entity\Notification\RuleNotification;
 use DR\Review\Entity\Notification\RuleOptions;
 use DR\Review\Service\Mail\CommitMailService;
 use DR\Review\Service\Mail\MailSubjectFormatter;
 use DR\Review\Tests\AbstractTestCase;
+use DR\Review\ViewModel\Mail\CommitsViewModel;
+use DR\Review\ViewModelProvider\Mail\CommitsViewModelProvider;
+use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\MockObject\MockObject;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
 
-/**
- * @coversDefaultClass \DR\Review\Service\Mail\CommitMailService
- * @covers ::__construct
- */
+#[CoversClass(CommitMailService::class)]
 class CommitMailServiceTest extends AbstractTestCase
 {
-    private MailerInterface&MockObject      $mailer;
-    private MailSubjectFormatter&MockObject $formatter;
-    private CommitMailService               $service;
+    private MailerInterface&MockObject          $mailer;
+    private MailSubjectFormatter&MockObject     $formatter;
+    private CommitsViewModelProvider&MockObject $viewModelProvider;
+    private CommitMailService                   $service;
 
     protected function setUp(): void
     {
         parent::setUp();
-        $this->mailer    = $this->createMock(MailerInterface::class);
-        $this->formatter = $this->createMock(MailSubjectFormatter::class);
-        $this->service   = new CommitMailService('foobar', $this->mailer, $this->formatter);
+        $this->mailer            = $this->createMock(MailerInterface::class);
+        $this->formatter         = $this->createMock(MailSubjectFormatter::class);
+        $this->viewModelProvider = $this->createMock(CommitsViewModelProvider::class);
+        $this->service           = new CommitMailService('foobar', $this->mailer, $this->formatter, $this->viewModelProvider);
     }
 
     /**
-     * @covers ::sendCommitsMail
      * @throws TransportExceptionInterface
      */
     public function testSendCommitsMail(): void
@@ -50,12 +53,17 @@ class CommitMailServiceTest extends AbstractTestCase
         $rule->addRecipient($recipient);
         $rule->setRuleOptions((new RuleOptions())->setSubject('subject'));
 
+        $notification = new RuleNotification();
+
         // prep data
         $commits = [$this->createCommit(), $this->createCommit()];
         $config  = new RuleConfiguration(new DatePeriod(new DateTime(), new DateInterval('PT1H'), new DateTime()), $rule);
 
+        $viewModel = new CommitsViewModel([], MailThemeType::UPSOURCE);
+
         // assert mailer send argument
         $this->formatter->expects(self::once())->method('format')->with('subject', $rule, $commits)->willReturn('replaced-subject');
+        $this->viewModelProvider->expects(self::once())->method('getCommitsViewModel')->with($commits, $rule, $notification)->willReturn($viewModel);
         $this->mailer->expects(static::once())
             ->method('send')
             ->with(
@@ -68,6 +76,6 @@ class CommitMailServiceTest extends AbstractTestCase
                 )
             );
 
-        $this->service->sendCommitsMail($config, $commits);
+        $this->service->sendCommitsMail($config, $commits, $notification);
     }
 }
