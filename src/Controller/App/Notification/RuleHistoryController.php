a848cf8203ab5fdb1f2e3f294e55982c384c83e9
Frank Dekker <fdekker@123inkt.nl>
2023-11-16 18:22:22 +0100

    Open rule notification in the same tab (#502)

--- src/Controller/App/Notification/RuleHistoryController.php
+++ src/Controller/App/Notification/RuleHistoryController.php
@@ -6,10 +6,9 @@ namespace DR\Review\Controller\App\Notification;
 use Doctrine\DBAL\Exception;
 use DR\Review\Controller\AbstractController;
 use DR\Review\Security\Role\Roles;
-use DR\Review\ViewModel\App\Notification\RuleNotificationViewModel;
 use DR\Review\ViewModelProvider\RuleNotificationViewModelProvider;
-use Symfony\Bridge\Twig\Attribute\Template;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
@@ -20,17 +19,22 @@ class RuleHistoryController extends AbstractController
     }
 
     /**
-     * @return array<string, RuleNotificationViewModel>
      * @throws Exception
      */
     #[Route('app/rule-history', name: self::class, methods: 'GET')]
-    #[Template('app/notification/rule_history.html.twig')]
     #[IsGranted(Roles::ROLE_USER)]
-    public function __invoke(Request $request): array
+    public function __invoke(Request $request): Response
     {
         $ruleId = $request->query->getInt('ruleId');
         $unread = $request->query->get('filter') === 'unread';
 
-        return ['notificationViewModel' => $this->viewModelProvider->getNotificationsViewModel($ruleId <= 0 ? null : $ruleId, $unread)];
+        $params   = ['notificationViewModel' => $this->viewModelProvider->getNotificationsViewModel($ruleId <= 0 ? null : $ruleId, $unread)];
+        $response = $this->render('app/notification/rule_history.html.twig', $params);
+
+        // set no cache headers
+        $response->headers->set('cache-control', 'no-cache, no-store, must-revalidate');
+        $response->headers->set('expires', '0');
+
+        return $response;
     }
 }
