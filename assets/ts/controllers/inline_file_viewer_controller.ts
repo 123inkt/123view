e18ca24551407558e8148a32822c99f8c9ed93f9
Frank Dekker <fdekker@123inkt.nl>
2023-11-26 11:45:32 +0100

    Add support for viewing markdown file

---
+++ assets/ts/controllers/inline_file_viewer_controller.ts
@@ -0,0 +1,23 @@
+import {Controller} from '@hotwired/stimulus';
+import axios from 'axios';
+import Function from '../lib/Function';
+
+export default class extends Controller<HTMLElement> {
+    public static values = {file: String};
+    private readonly declare fileValue: string;
+
+    public connect(): void {
+        console.log('connected1');
+    }
+
+    public viewFile(event: Event): void {
+        event.preventDefault();
+        event.stopPropagation();
+
+        this.element.innerHTML = '';
+        this.element.classList.add('comment__markdown');
+        axios.get(this.fileValue)
+            .then(response => this.element.innerHTML = response.data)
+            .catch(Function.empty);
+    }
+}
