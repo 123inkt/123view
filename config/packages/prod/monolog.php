b88ec83627966240f55b12f4ef1f7974ad5b4504
Frank Dekker <fdekker@123inkt.nl>
2023-12-09 12:03:04 +0100

    Improve monolog channels (#533)

--- config/packages/prod/monolog.php
+++ config/packages/prod/monolog.php
@@ -4,19 +4,29 @@ declare(strict_types=1);
 use Symfony\Config\MonologConfig;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\env;
 
+/**
+ * Setup summary:
+ * - Level: INFO  - /logs/prod.log - !event, !console, !deprecation
+ * - Level: ERROR - /logs/prod.error.log - !event, !console, !deprecation  (fingers crossed)
+ * - Level: DEBUG - /logs/deprecations.log - deprecation
+ * - Level: ERROR - mailer - !console
+ * - Level: DEBUG - stderr - !event, !deprecation, !console
+ * - Level: DEBUG - console - !event, !deprecation
+ */
 return static function (MonologConfig $monolog) {
     $monolog->handler('info')
         ->type('rotating_file')
         ->path('%kernel.logs_dir%/%kernel.environment%.log')
         ->level('info')
         ->formatter('monolog.formatter.line')
+        ->channels()->elements(["!event", "!console", "!deprecation"])
         ->maxFiles(10);
 
     $monolog->handler('error_fingers_crossed')
         ->type('fingers_crossed')
         ->actionLevel('error')
         ->handler('error')
-        ->channels()->elements(["!event"]);
+        ->channels()->elements(["!event", "!console", "!deprecation"]);
     $monolog->handler('error')
         ->type('rotating_file')
         ->path('%kernel.logs_dir%/%kernel.environment%.error.log')
@@ -24,12 +34,21 @@ return static function (MonologConfig $monolog) {
         ->formatter('monolog.formatter.line')
         ->maxFiles(10);
 
+    $monolog->handler('deprecations')
+        ->type('rotating_file')
+        ->path('%kernel.logs_dir%/deprecations.%kernel.environment%.log')
+        ->level('debug')
+        ->formatter('monolog.formatter.line')
+        ->maxFiles(1)
+        ->channels()->elements(["deprecation"]);
+
     if ((string)env('ERROR_MAIL') !== '') {
         // error mailer
         $monolog->handler('main')
             ->type('fingers_crossed')
             ->level('error')
-            ->handler('deduplicated');
+            ->handler('deduplicated')
+            ->channels()->elements(['!console']);
         $monolog->handler('deduplicated')
             ->type('deduplication')
             ->handler('symfony_mailer');
@@ -45,8 +64,16 @@ return static function (MonologConfig $monolog) {
         $monolog->handler('main')->excludedHttpCode()->code(403)->code(404);
     }
 
+    $monolog->handler('docker')
+        ->type('stream')
+        ->level('info')
+        ->path('php://stderr')
+        ->formatter('monolog.formatter.line')
+        ->channels()->elements(["!event", "!deprecation", "!console"]);
+
     $monolog->handler('console')
         ->type('console')
-        ->level('info')
-        ->formatter('monolog.formatter.line');
+        ->level('debug')
+        ->formatter('monolog.formatter.line')
+        ->channels()->elements(["!event", "!deprecation"]);
 };
