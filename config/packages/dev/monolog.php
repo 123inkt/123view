c0e3a56ea533922a320d8db16c2de1201bfe1e9f
Frank Dekker <fdekker@123inkt.nl>
2023-12-09 11:58:20 +0100

    Improve monolog channels

--- config/packages/dev/monolog.php
+++ config/packages/dev/monolog.php
@@ -3,6 +3,17 @@ declare(strict_types=1);
 
 use Symfony\Config\MonologConfig;
 
+/**
+ * Setup summary:
+ * - Level: INFO  - /logs/dev.log - !event, !console, !deprecation
+ * - Level: ERROR - /logs/dev.error.log - !event
+ * - Level: DEBUG - /logs/doctrine.log - doctrine
+ * - Level: DEBUG - /logs/git.log - git
+ * - Level: DEBUG - /logs/app.log - app
+ * - Level: DEBUG - /logs/deprecations.log - deprecation
+ * - Level: DEBUG - stderr - !event, !deprecation, !console
+ * - Level: DEBUG - console - !event, !deprecation
+ */
 return static function (MonologConfig $monolog) {
     $monolog->handler('main')
         ->type('rotating_file')
@@ -10,7 +21,7 @@ return static function (MonologConfig $monolog) {
         ->level('info')
         ->maxFiles(1)
         ->formatter('monolog.formatter.line')
-        ->channels()->elements(["!event"]);
+        ->channels()->elements(["!event", "!console", "!deprecation"]);
 
     $monolog->handler('error')
         ->type('stream')
@@ -57,10 +68,11 @@ return static function (MonologConfig $monolog) {
         ->level('debug')
         ->path('php://stderr')
         ->formatter('monolog.formatter.line')
-        ->channels()->elements(["!event"]);
+        ->channels()->elements(["!event", "!deprecation", "!console"]);
 
     $monolog->handler('console')
         ->type('console')
         ->level('debug')
-        ->formatter('monolog.formatter.line');
+        ->formatter('monolog.formatter.line')
+        ->channels()->elements(["!event", "!deprecation"]);
 };
