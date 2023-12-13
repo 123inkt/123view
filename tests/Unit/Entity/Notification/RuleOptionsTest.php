2eb9c30f01c79e452560f5de89da5b6128ca256d
Frank Dekker <fdekker@123inkt.nl>
2023-12-13 21:32:19 +0100

    Update tests

--- tests/Unit/Entity/Notification/RuleOptionsTest.php
+++ tests/Unit/Entity/Notification/RuleOptionsTest.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace DR\Review\Tests\Unit\Entity\Notification;
 
+use DR\Review\Doctrine\Type\NotificationSendType;
 use DR\Review\Entity\Notification\RuleOptions;
 use DR\Review\Tests\AbstractTestCase;
 use PHPUnit\Framework\Attributes\CoversClass;
@@ -14,5 +15,20 @@ class RuleOptionsTest extends AbstractTestCase
     {
         static::assertNull((new RuleOptions())->getId());
         static::assertAccessorPairs(RuleOptions::class);
+    }
+
+    public function testSendType(): void
+    {
+        $options = new RuleOptions();
+        static::assertTrue($options->hasSendType(NotificationSendType::MAIL));
+        static::assertTrue($options->hasSendType(NotificationSendType::BROWSER));
+
+        $options->setSendType(NotificationSendType::MAIL);
+        static::assertTrue($options->hasSendType(NotificationSendType::MAIL));
+        static::assertFalse($options->hasSendType(NotificationSendType::BROWSER));
+
+        $options->setSendType(NotificationSendType::BROWSER);
+        static::assertFalse($options->hasSendType(NotificationSendType::MAIL));
+        static::assertTrue($options->hasSendType(NotificationSendType::BROWSER));
     }
 }
