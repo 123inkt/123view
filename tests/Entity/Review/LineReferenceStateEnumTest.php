7050210e80807f55ae0e3b5d47d4a4f0b79b7d6a
Frank Dekker <fdekker@123inkt.nl>
2023-12-15 14:39:21 +0100

    Update coverage

---
+++ tests/Entity/Review/LineReferenceStateEnumTest.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\Review\Tests\Entity\Review;
+
+use DR\Review\Entity\Review\LineReferenceStateEnum;
+use DR\Review\Tests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(LineReferenceStateEnum::class)]
+class LineReferenceStateEnumTest extends AbstractTestCase
+{
+    public function testValues(): void
+    {
+        static::assertSame(['M', 'U', 'A', 'D', '?'], LineReferenceStateEnum::values());
+    }
+}
