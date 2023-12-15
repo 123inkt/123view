51b0cdd7ea475627748aea43379424b4ec748d06
Frank Dekker <fdekker@123inkt.nl>
2023-12-15 14:41:36 +0100

    Update coverage

--- tests/Unit/Entity/Review/LineReferenceStateEnumTest.php
+++ tests/Unit/Entity/Review/LineReferenceStateEnumTest.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace DR\Review\Tests\Entity\Review;
+namespace DR\Review\Tests\Unit\Entity\Review;
 
 use DR\Review\Entity\Review\LineReferenceStateEnum;
 use DR\Review\Tests\AbstractTestCase;
