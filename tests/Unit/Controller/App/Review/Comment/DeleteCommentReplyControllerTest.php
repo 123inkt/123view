d582d2db7ca26f1e9b72238ae60e07f184c0a225
Frank Dekker <fdekker@123inkt.nl>
2023-12-17 14:43:56 +0100

    Add coverage

--- tests/Unit/Controller/App/Review/Comment/DeleteCommentReplyControllerTest.php
+++ tests/Unit/Controller/App/Review/Comment/DeleteCommentReplyControllerTest.php
@@ -8,23 +8,33 @@ use DR\Review\Controller\App\Review\Comment\DeleteCommentReplyController;
 use DR\Review\Entity\Review\CodeReview;
 use DR\Review\Entity\Review\Comment;
 use DR\Review\Entity\Review\CommentReply;
+use DR\Review\Entity\User\User;
+use DR\Review\Message\Comment\CommentReplyRemoved;
 use DR\Review\Repository\Review\CommentReplyRepository;
 use DR\Review\Security\Voter\CommentReplyVoter;
+use DR\Review\Service\CodeReview\Comment\CommentEventMessageFactory;
 use DR\Review\Tests\AbstractControllerTestCase;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\MockObject\MockObject;
+use stdClass;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 #[CoversClass(DeleteCommentReplyController::class)]
 class DeleteCommentReplyControllerTest extends AbstractControllerTestCase
 {
-    private CommentReplyRepository&MockObject $commentRepository;
+    private CommentReplyRepository&MockObject     $commentRepository;
+    private CommentEventMessageFactory&MockObject $messageFactory;
+    private MessageBusInterface&MockObject        $bus;
 
     public function setUp(): void
     {
         $this->commentRepository = $this->createMock(CommentReplyRepository::class);
+        $this->messageFactory    = $this->createMock(CommentEventMessageFactory::class);
+        $this->bus               = $this->createMock(MessageBusInterface::class);
         parent::setUp();
     }
 
@@ -36,6 +46,7 @@ class DeleteCommentReplyControllerTest extends AbstractControllerTestCase
 
     public function testInvoke(): void
     {
+        $user    = new User();
         $comment = new Comment();
         $comment->setId(123);
         $review = new CodeReview();
@@ -45,8 +56,13 @@ class DeleteCommentReplyControllerTest extends AbstractControllerTestCase
         $reply = new CommentReply();
         $reply->setComment($comment);
 
+        $event = new CommentReplyRemoved(1, 2, 3, 4, 5, 'message', null);
+
+        $this->expectGetUser($user);
         $this->expectDenyAccessUnlessGranted(CommentReplyVoter::DELETE, $reply);
+        $this->messageFactory->expects(self::once())->method('createReplyRemove')->with($reply, $user)->willReturn($event);
         $this->commentRepository->expects(self::once())->method('remove')->with($reply, true);
+        $this->bus->expects(self::once())->method('dispatch')->with($event)->willReturn(new Envelope(new stdClass()));
 
         $response = ($this->controller)($reply);
         static::assertInstanceOf(JsonResponse::class, $response);
@@ -55,6 +71,6 @@ class DeleteCommentReplyControllerTest extends AbstractControllerTestCase
 
     public function getController(): AbstractController
     {
-        return new DeleteCommentReplyController($this->commentRepository);
+        return new DeleteCommentReplyController($this->commentRepository, $this->messageFactory, $this->bus);
     }
 }
