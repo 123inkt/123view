3d4300ea7396196471948c7c7a1ec22ded38ac17
Frank Dekker <fdekker@123inkt.nl>
2023-12-21 23:37:48 +0100

    Fix test

--- tests/Unit/Service/Api/Gitlab/GitlabCommentFormatterTest.php
+++ tests/Unit/Service/Api/Gitlab/GitlabCommentFormatterTest.php
@@ -22,7 +22,7 @@ class GitlabCommentFormatterTest extends AbstractTestCase
     {
         parent::setUp();
         $this->urlGenerator = $this->createMock(UrlGeneratorInterface::class);
-        $this->formatter    = new GitlabCommentFormatter($this->urlGenerator);
+        $this->formatter    = new GitlabCommentFormatter('https://example.com', $this->urlGenerator);
     }
 
     public function testFormat(): void
@@ -34,9 +34,9 @@ class GitlabCommentFormatterTest extends AbstractTestCase
 
         $this->urlGenerator->expects(self::once())
             ->method('generate')
-            ->with(ReviewController::class, ['review' => $review], UrlGeneratorInterface::ABSOLUTE_URL)
-            ->willReturn('https://example.com/path');
+            ->with(ReviewController::class, ['review' => $review])
+            ->willReturn('/path/to/review');
 
-        static::assertSame("foo\n<br>bar\n<br>\n<br>*https://example.com/path*", $this->formatter->format($comment));
+        static::assertSame("foo\n<br>bar\n<br>\n<br>*123view: https://example.com/path/to/review*", $this->formatter->format($comment));
     }
 }
