91bafed7629c8691f1d1a4deac6d2f8c4c87d79e
Frank Dekker <fdekker@123inkt.nl>
2023-12-22 23:10:33 +0100

    Format url in gitlab comment sync (#559)

--- tests/Unit/Service/Api/Gitlab/GitlabCommentFormatterTest.php
+++ tests/Unit/Service/Api/Gitlab/GitlabCommentFormatterTest.php
@@ -6,6 +6,7 @@ namespace DR\Review\Tests\Unit\Service\Api\Gitlab;
 use DR\Review\Controller\App\Review\ReviewController;
 use DR\Review\Entity\Review\CodeReview;
 use DR\Review\Entity\Review\Comment;
+use DR\Review\Entity\Review\LineReference;
 use DR\Review\Service\Api\Gitlab\GitlabCommentFormatter;
 use DR\Review\Tests\AbstractTestCase;
 use PHPUnit\Framework\Attributes\CoversClass;
@@ -27,16 +28,22 @@ class GitlabCommentFormatterTest extends AbstractTestCase
 
     public function testFormat(): void
     {
-        $review  = new CodeReview();
+        $review = new CodeReview();
+        $review->setProjectId(123);
         $comment = new Comment();
+        $comment->setId(456);
+        $comment->setLineReference(LineReference::fromString('old/path:new/path:1:2:3:commitSha:A'));
         $comment->setMessage("foo\nbar");
         $comment->setReview($review);
 
         $this->urlGenerator->expects(self::once())
             ->method('generate')
-            ->with(ReviewController::class, ['review' => $review])
+            ->with(ReviewController::class, ['review' => $review, 'filePath' => 'new/path'])
             ->willReturn('/path/to/review');
 
-        static::assertSame("foo\n<br>bar\n<br>\n<br>*123view: https://example.com/path/to/review*", $this->formatter->format($comment));
+        static::assertSame(
+            "foo<br>\nbar<br>\n<br>\n*[123view: CR-123](https://example.com/path/to/review#focus:comment:456)*",
+            $this->formatter->format($comment)
+        );
     }
 }
