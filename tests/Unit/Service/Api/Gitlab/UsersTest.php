b9acdbbac2b3152615aee9910e83aac50cb6ff45
Frank Dekker <fdekker@123inkt.nl>
2024-01-25 22:57:40 +0100

    Add coverage

---
+++ tests/Unit/Service/Api/Gitlab/UsersTest.php
@@ -0,0 +1,69 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\Review\Tests\Unit\Service\Api\Gitlab;
+
+use DR\Review\Model\Api\Gitlab\User;
+use DR\Review\Service\Api\Gitlab\Users;
+use DR\Review\Tests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\MockObject\MockObject;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+use Throwable;
+
+#[CoversClass(Users::class)]
+class UsersTest extends AbstractTestCase
+{
+    private HttpClientInterface&MockObject $httpClient;
+    private SerializerInterface&MockObject $serializer;
+    private Users                          $users;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $logger           = $this->createMock(LoggerInterface::class);
+        $this->httpClient = $this->createMock(HttpClientInterface::class);
+        $this->serializer = $this->createMock(SerializerInterface::class);
+        $this->users      = new Users($logger, $this->httpClient, $this->serializer);
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function testGetUserSuccess(): void
+    {
+        $response = $this->createMock(ResponseInterface::class);
+        $response->expects(self::once())->method('getStatusCode')->willReturn(Response::HTTP_OK);
+        $response->expects(self::once())->method('getContent')->willReturn('{"foo":"bar"}');
+
+        $user = new User();
+
+        $this->httpClient->expects(self::once())->method('request')->with('GET', 'users/123')->willReturn($response);
+        $this->serializer->expects(self::once())
+            ->method('deserialize')
+            ->with('{"foo":"bar"}', User::class, JsonEncoder::FORMAT, [AbstractNormalizer::ALLOW_EXTRA_ATTRIBUTES => true])
+            ->willReturn($user);
+
+        static::assertSame($user, $this->users->getUser(123));
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function testGetUserFailure(): void
+    {
+        $response = $this->createMock(ResponseInterface::class);
+        $response->expects(self::once())->method('getStatusCode')->willReturn(Response::HTTP_BAD_REQUEST);
+
+        $this->httpClient->expects(self::once())->method('request')->with('GET', 'users/123')->willReturn($response);
+        $this->serializer->expects(self::never())->method('deserialize');
+
+        static::assertNull($this->users->getUser(123));
+    }
+}
