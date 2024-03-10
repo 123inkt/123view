bc827a01dcdc8689aff748d8c4b34826a643bace
Frank Dekker <fdekker@123inkt.nl>
2024-03-10 10:33:35 +0100

    Add custom health checks

---
+++ src/Service/Health/DoctrineDbal.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\Review\Service\Health;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\Persistence\ManagerRegistry;
+use DR\Utils\Assert;
+use Laminas\Diagnostics\Check\AbstractCheck;
+use Laminas\Diagnostics\Result\ResultInterface;
+use Laminas\Diagnostics\Result\Success;
+use Override;
+
+class DoctrineDbal extends AbstractCheck
+{
+    public function __construct(private readonly ManagerRegistry $registry)
+    {
+    }
+
+    #[Override]
+    public function check(): ResultInterface
+    {
+        $connection = $this->registry->getConnection();
+        $query      = Assert::isInstanceOf($connection->getDatabasePlatform(), AbstractPlatform::class)->getDummySelectSQL();
+
+        $connection->fetchOne($query);
+
+        return new Success();
+    }
+}
