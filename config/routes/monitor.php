8ebce2899e4d53f254ee3984bad0ff5b0d52549d
Frank Dekker <fdekker@123inkt.nl>
2024-03-10 09:56:23 +0100

    Configure liip monitor bundle

--- config/routes/monitor.php
+++ config/routes/monitor.php
@@ -2,10 +2,13 @@
 
 declare(strict_types=1);
 
+use Liip\MonitorBundle\Controller\HealthCheckController;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
-return static function (RoutingConfigurator $routingConfigurator): void {
-    $routingConfigurator
-        ->import("@LiipMonitorBundle/Resources/config/routing.xml")
-        ->prefix('/health');
+return static function (RoutingConfigurator $routes): void {
+    /* @see vendor/liip/monitor-bundle/Resources/config/routing.xml */
+    $routes
+        ->add('liip_monitor_run_all_checks', '/health')
+        ->methods(['GET'])
+        ->controller([HealthCheckController::class, 'runAllChecksAction']);
 };
