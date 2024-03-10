fa37d3971f3fcbe997921043dd7d50cdc1f80d7d
Frank Dekker <fdekker@123inkt.nl>
2024-03-10 09:20:59 +0100

    Update config

--- config/routes/log_viewer.php
+++ config/routes/log_viewer.php
@@ -5,7 +5,5 @@ declare(strict_types=1);
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return static function (RoutingConfigurator $routingConfigurator): void {
-    $routingConfigurator
-        ->import('@FrameworkBundle/Resources/config/routing/webhook.xml')
-        ->prefix('/webhook');
+    $routingConfigurator->import('.', 'fd_symfony_log_viewer')->prefix('/log-viewer');
 };
