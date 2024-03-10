6edc98b44f795628c493283b69dde722864d5dab
Frank Dekker <fdekker@123inkt.nl>
2024-03-10 11:38:49 +0100

    Fix fixtures

--- config/services_test.php
+++ config/services_test.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
 
-    $services->load('DR\\Review\\Tests\\DataFixtures\\', '../../../tests/DataFixtures/*')->tag('doctrine.fixture.orm');
+    $services->load('DR\\Review\\Tests\\DataFixtures\\', '../tests/DataFixtures/*')->tag('doctrine.fixture.orm');
 
     // make these services public to allow mocking in E2E/Integration test
     $services->get(RuleRepository::class)->public();
