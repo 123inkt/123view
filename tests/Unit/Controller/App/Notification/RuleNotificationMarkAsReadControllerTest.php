2c7399ce4c6727d946c7017e9a256c0b2627222d
Frank Dekker <fdekker@123inkt.nl>
2024-04-20 14:21:55 +0200

    Add coverage

---
+++ tests/Unit/Controller/App/Notification/RuleNotificationMarkAsReadControllerTest.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\Review\Tests\Unit\Controller\App\Notification;
+
+use DR\Review\Controller\AbstractController;
+use DR\Review\Controller\App\Notification\RuleHistoryController;
+use DR\Review\Controller\App\Notification\RuleNotificationMarkAsReadController;
+use DR\Review\Entity\Notification\Rule;
+use DR\Review\Repository\Config\RuleNotificationRepository;
+use DR\Review\Tests\AbstractControllerTestCase;
+use Override;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\MockObject\MockObject;
+
+#[CoversClass(RuleNotificationMarkAsReadController::class)]
+class RuleNotificationMarkAsReadControllerTest extends AbstractControllerTestCase
+{
+    private RuleNotificationRepository&MockObject $notificationRepository;
+
+    #[Override]
+    protected function setUp(): void
+    {
+        $this->notificationRepository = $this->createMock(RuleNotificationRepository::class);
+        parent::setUp();
+    }
+
+    public function testInvoke(): void
+    {
+        $rule = new Rule();
+
+        $this->notificationRepository->expects(self::once())->method('markAsRead')->with($rule);
+        $this->expectRefererRedirect(RuleHistoryController::class);
+
+        ($this->controller)($rule);
+    }
+
+    #[Override]
+    public function getController(): AbstractController
+    {
+        return new RuleNotificationMarkAsReadController($this->notificationRepository);
+    }
+}
