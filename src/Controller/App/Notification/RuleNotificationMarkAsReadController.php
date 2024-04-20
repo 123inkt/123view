6a071f37296f87399aca185cd28a32fe4f2ac95e
Frank Dekker <fdekker@123inkt.nl>
2024-04-20 14:19:10 +0200

    Add mark all as read button to notification rule

---
+++ src/Controller/App/Notification/RuleNotificationMarkAsReadController.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\Review\Controller\App\Notification;
+
+use DR\Review\Controller\AbstractController;
+use DR\Review\Entity\Notification\Rule;
+use DR\Review\Repository\Config\RuleNotificationRepository;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RuleNotificationMarkAsReadController extends AbstractController
+{
+    public function __construct(private readonly RuleNotificationRepository $notificationRepository)
+    {
+    }
+
+    #[Route('/app/rules/{id}/notification/mark-as-read', self::class, methods: 'GET')]
+    public function __invoke(#[MapEntity] Rule $rule): Response
+    {
+        $this->notificationRepository->markAsRead($rule);
+
+        return $this->refererRedirect(RuleHistoryController::class);
+    }
+}
