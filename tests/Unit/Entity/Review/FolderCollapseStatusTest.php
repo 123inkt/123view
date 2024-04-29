377d5e4739f1ffa9cdedd90d7520df84b919672c
Frank Dekker <fdekker@123inkt.nl>
2024-04-29 22:00:41 +0200

    Add coverage

---
+++ tests/Unit/Entity/Review/FolderCollapseStatusTest.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\Review\Tests\Unit\Entity\Review;
+
+use DR\Review\Entity\Review\FileSeenStatus;
+use DR\Review\Entity\Review\FolderCollapseStatus;
+use DR\Review\Tests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(FolderCollapseStatus::class)]
+class FolderCollapseStatusTest extends AbstractTestCase
+{
+    public function testAccessorPairs(): void
+    {
+        static::assertAccessorPairs(FileSeenStatus::class);
+    }
+}
