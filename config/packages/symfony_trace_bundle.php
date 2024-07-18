ab69404ef5f11967dc55f73bfebb36430180c55c
Frank Dekker <fdekker@123inkt.nl>
2024-07-18 19:51:25 +0200

    Migrate symfony trace bundle config

--- config/packages/symfony_trace_bundle.php
+++ config/packages/symfony_trace_bundle.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 use Symfony\Config\SymfonyTraceConfig;
 
 return static function (SymfonyTraceConfig $config): void {
-    $config->trustRequestHeader(false);
-    $config->sendResponseHeader(true);
+    $config->request()->trustHeader(false);
+    $config->response()->sendHeader(true);
     $config->enableMonolog(true);
     $config->console()->enabled(true);
     $config->enableMessenger(true);
