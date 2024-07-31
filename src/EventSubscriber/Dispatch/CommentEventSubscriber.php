22f895535359a1dfcbafc7ef3ab7de4b1f8d3d36
Frank Dekker <fdekker@123inkt.nl>
2024-07-31 20:58:22 +0200

    Ensure comments are resolved by the correct user

--- src/EventSubscriber/Dispatch/CommentEventSubscriber.php
+++ src/EventSubscriber/Dispatch/CommentEventSubscriber.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\Event\PreUpdateEventArgs;
 use Doctrine\ORM\Events;
 use DR\Review\Doctrine\Type\CommentStateType;
 use DR\Review\Entity\Review\Comment;
+use DR\Review\Entity\User\User;
 use DR\Review\Message\Comment\CommentAdded;
 use DR\Review\Message\Comment\CommentRemoved;
 use DR\Review\Message\Comment\CommentResolved;
@@ -15,6 +16,7 @@ use DR\Review\Message\Comment\CommentUnresolved;
 use DR\Review\Message\Comment\CommentUpdated;
 use DR\Review\Service\CodeReview\Comment\CommentEventMessageFactory;
 use DR\Utils\Assert;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Console\ConsoleEvents;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -35,13 +37,16 @@ class CommentEventSubscriber implements ResetInterface
     /** @var array<int, mixed[]> */
     private array $updated = [];
 
-    public function __construct(private readonly MessageBusInterface $bus, private readonly CommentEventMessageFactory $messageFactory)
-    {
+    public function __construct(
+        private readonly Security $security,
+        private readonly MessageBusInterface $bus,
+        private readonly CommentEventMessageFactory $messageFactory
+    ) {
     }
 
     public function commentAdded(Comment $comment): void
     {
-        $this->events[] = $this->messageFactory->createAdded($comment, $comment->getUser());
+        $this->events[] = $this->messageFactory->createAdded($comment, $this->getUser($comment));
     }
 
     public function preCommentUpdated(Comment $comment, PreUpdateEventArgs $event): void
@@ -53,13 +58,14 @@ class CommentEventSubscriber implements ResetInterface
 
     public function commentUpdated(Comment $comment): void
     {
+        $user      = $this->getUser($comment);
         $changeSet = $this->updated[$comment->getId()] ?? null;
         if ($changeSet === null) {
             return;
         }
 
         if (array_key_exists('message', $changeSet)) {
-            $this->events[] = $this->messageFactory->createUpdated($comment, $comment->getUser(), Assert::string($changeSet['message'][0]));
+            $this->events[] = $this->messageFactory->createUpdated($comment, $user, Assert::string($changeSet['message'][0]));
         }
 
         if (array_key_exists('state', $changeSet) === false) {
@@ -67,19 +73,20 @@ class CommentEventSubscriber implements ResetInterface
         }
 
         if ($comment->getState() === CommentStateType::RESOLVED) {
-            $this->events[] = $this->messageFactory->createResolved($comment, $comment->getUser());
+            $this->events[] = $this->messageFactory->createResolved($comment, $user);
         } else {
-            $this->events[] = $this->messageFactory->createUnresolved($comment, $comment->getUser());
+            $this->events[] = $this->messageFactory->createUnresolved($comment, $user);
         }
     }
 
     public function commentRemoved(Comment $comment): void
     {
-        if ($comment->getUser()->hasId() === false) {
+        $user = $this->getUser($comment);
+        if ($user->hasId() === false) {
             return;
         }
 
-        $this->events[] = $this->messageFactory->createRemoved($comment, $comment->getUser());
+        $this->events[] = $this->messageFactory->createRemoved($comment, $user);
     }
 
     /**
@@ -100,5 +107,12 @@ class CommentEventSubscriber implements ResetInterface
         foreach ($events as $event) {
             $this->bus->dispatch($event);
         }
+    }
+
+    private function getUser(Comment $comment): User
+    {
+        $user = $this->security->getUser();
+
+        return $user instanceof User ? $user : $comment->getUser();
     }
 }
