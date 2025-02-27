eed1a34695d7c84a82c935256829139dbe1f0f94
Frank Dekker <fdekker@123inkt.nl>
2025-02-27 13:18:52 +0100

    Make iframe 0x0 width

--- assets/ts/controllers/ide_button_controller.ts
+++ assets/ts/controllers/ide_button_controller.ts
@@ -4,10 +4,13 @@ export default class extends Controller {
     public onClick(event: Event): void {
         event.preventDefault();
 
-        const iframe   = document.createElement('iframe');
+        const iframe = document.createElement('iframe');
         document.body.appendChild(iframe);
-        iframe.onload  = () => iframe.remove();
-        iframe.onerror = () => iframe.remove();
-        iframe.src = (this.element as HTMLAnchorElement).href;
+        iframe.onload        = () => iframe.remove();
+        iframe.onerror       = () => iframe.remove();
+        iframe.style.display = 'block';
+        iframe.style.width   = '0px';
+        iframe.style.height  = '0px';
+        iframe.src           = (this.element as HTMLAnchorElement).href;
     }
 }
