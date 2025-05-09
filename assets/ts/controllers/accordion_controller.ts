fb147db5171985f847e4e35446d7671306324897
Frank Dekker <fdekker@123inkt.nl>
2025-05-09 11:06:02 +0200

    Add bootstrap accordion

---
+++ assets/ts/controllers/accordion_controller.ts
@@ -0,0 +1,21 @@
+import {Controller} from '@hotwired/stimulus';
+import Elements from '../lib/Elements';
+
+export default class extends Controller<HTMLElement> {
+    public toggle(event: Event): void {
+        event.preventDefault();
+
+        const target    = event.currentTarget as HTMLElement;
+        const accordion = Elements.closestRole(target, 'accordion-item');
+        const collapse  = accordion.querySelector('[data-role~=accordion-collapse]') as HTMLElement;
+
+        if (target.classList.contains('collapsed')) {
+            target.classList.remove('collapsed');
+            collapse.classList.add('show');
+        } else {
+            target.classList.add('collapsed');
+            collapse.classList.remove('show');
+        }
+        console.log(event);
+    }
+}
