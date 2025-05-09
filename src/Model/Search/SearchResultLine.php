0049281ca92eeb4b6070ec36cd314567a056d5a5
Frank Dekker <fdekker@123inkt.nl>
2025-05-09 15:27:22 +0200

    Add coverage

--- src/Model/Search/SearchResultLine.php
+++ src/Model/Search/SearchResultLine.php
@@ -5,6 +5,9 @@ namespace DR\Review\Model\Search;
 
 readonly class SearchResultLine
 {
+    /**
+     * @codeCoverageIgnore Simple DTO
+     */
     public function __construct(public string $line, public int $lineNumber, public SearchResultLineTypeEnum $type)
     {
     }
