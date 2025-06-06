13ba8bb8882feff9c13e510a7b019cd4750ede8a
Frank Dekker <fdekker@123inkt.nl>
2025-06-06 17:15:47 +0200

    Update config

--- config/packages/symfony_trace_bundle.php
+++ config/packages/symfony_trace_bundle.php
@@ -7,9 +7,9 @@ use Symfony\Config\SymfonyTraceConfig;
 return static function (SymfonyTraceConfig $config): void {
     $config->request()->trustHeader(true);
     $config->response()->sendHeader(true);
-    $config->enableMonolog(true);
+    $config->monolog()->enabled(true);
     $config->console()->enabled(true);
-    $config->enableMessenger(true);
-    $config->enableTwig(true);
+    $config->messenger()->enabled(true);
+    $config->twig()->enabled(true);
     $config->httpClient()->enabled(false);
 };
