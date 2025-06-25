e74614276d4aa68def46c16045e5700b66b0df79
Frank Dekker <fdekker@123inkt.nl>
2025-06-25 11:34:31 +0200

    Add absolute url for images in markdown  (#1117)

--- tests/Unit/Service/CodeReview/Comment/CommonMarkdownConverterTest.php
+++ tests/Unit/Service/CodeReview/Comment/CommonMarkdownConverterTest.php
@@ -7,15 +7,24 @@ use DR\Review\Service\CodeReview\Comment\CommonMarkdownConverter;
 use DR\Review\Tests\AbstractTestCase;
 use League\CommonMark\Extension\CommonMark\Node\Block\FencedCode;
 use PHPUnit\Framework\Attributes\CoversClass;
+use Psr\EventDispatcher\EventDispatcherInterface;
 use Spatie\CommonMarkHighlighter\FencedCodeRenderer;
 
 #[CoversClass(CommonMarkdownConverter::class)]
 class CommonMarkdownConverterTest extends AbstractTestCase
 {
+    private CommonMarkdownConverter $converter;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $eventDispatcher = $this->createMock(EventDispatcherInterface::class);
+        $this->converter = new CommonMarkdownConverter($eventDispatcher);
+    }
+
     public function testConstruct(): void
     {
-        $converter   = new CommonMarkdownConverter();
-        $environment = $converter->getEnvironment();
+        $environment = $this->converter->getEnvironment();
 
         $extension = $environment->getExtensions();
         static::assertCount(3, $extension);
