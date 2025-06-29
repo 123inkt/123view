806b7f8d68d813465f5a102ce6233e0a776ad216
Frank Dekker <fdekker@123inkt.nl>
2025-06-29 18:57:08 +0200

    Add router

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {Home} from './home/home';
+import {Login} from './login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: Home},
+  {path: 'login', component: Login}
+];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterLink, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
