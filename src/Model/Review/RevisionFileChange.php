6a31d2c7a529303b3e2c4951f45ceed55abb1183
Frank Dekker <fdekker@123inkt.nl>
2025-07-02 22:27:20 +0200

    Add coverage

--- src/Model/Review/RevisionFileChange.php
+++ src/Model/Review/RevisionFileChange.php
@@ -5,6 +5,9 @@ namespace DR\Review\Model\Review;
 
 readonly class RevisionFileChange
 {
+    /**
+     * @codeCoverageIgnore Simple DTO
+     */
     public function __construct(public int $revisionId, public int $fileCount, public int $linesAdded, public int $linesRemoved)
     {
     }
