6b1d3e3690d2364c441c9e1725dbd62edef084dd
Daniel Blok <dblok@123inkt.nl>
2025-07-04 17:06:25 +0200

    Fix review points

--- assets/ts/controllers/component/checkbox_select_all_controller.ts
+++ assets/ts/controllers/component/checkbox_select_all_controller.ts
@@ -6,26 +6,13 @@ export default class extends Controller {
     public toggleAll(event: Event): void {
         const target    = event.currentTarget as HTMLInputElement;
         const role      = target.dataset.forRole;
-        const search    = this.element.querySelector<HTMLInputElement>('[data-role~="search"]')?.value.toLowerCase() ?? '';
         const revisions = this.getRevisions();
 
-        if (search === '' || search === undefined) {
-            revisions.forEach(el => el.querySelector<HTMLInputElement>(`[data-role~="${role}"]`)!.checked = target.checked);
-        } else {
-            revisions.forEach(el => el.querySelector<HTMLInputElement>(`[data-role~="${role}"]`)!.checked = this.matchesSearch(el.closest('[data-role~="revision"]'), search) && target.checked);
-        }
+        revisions.forEach(el => el.querySelector<HTMLInputElement>(`[data-role~="${role}"]`)!.checked = target.checked);
     }
 
     private getRevisions(): HTMLElement[] {
-        return Array.from(this.element.querySelectorAll<HTMLElement>(`[data-role~="revision"]`));
-    }
-
-    private matchesSearch(el: HTMLElement | null, search: string | undefined): boolean {
-        if (el === null || search === undefined || search === '') {
-            return true;
-        }
-
-        return el.dataset.title?.toLowerCase().includes(search) === true
-            || el.dataset.revision?.toLowerCase().includes(search) === true;
+        return Array.from(this.element.querySelectorAll<HTMLElement>(`[data-role~="revision"]`))
+            .filter(el => el.checkVisibility());
     }
 }
