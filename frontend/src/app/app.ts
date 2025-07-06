e7b074b61bda6c0392ec5ba3be831f23eef5104b
Frank Dekker <fdekker@123inkt.nl>
2025-07-06 14:59:17 +0200

    Add loading indicator

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,14 +1,25 @@
 import {Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
+import {NavigationEnd, NavigationStart, ResolveEnd, ResolveStart, Router, RouterOutlet} from '@angular/router';
 import {Header} from '@component/header/header';
-import {environment} from '@environment/environment';
+import {Loader} from '@component/loader/loader';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Loader],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected apiPort = environment.apiPort;
+  public loading: boolean = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof ResolveStart || event instanceof NavigationStart) {
+        this.loading = true;
+      }
+      if (event instanceof ResolveEnd || event instanceof NavigationEnd) {
+        this.loading = false;
+      }
+    });
+  }
 }
---
+++ frontend/src/app/component/loader/loader.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loader',
+  imports: [],
+  templateUrl: './loader.html',
+  styleUrl: './loader.scss'
+})
+export class Loader {
+}
