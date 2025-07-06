e5ea9a0a0fc722411046cf1fedfdd61cf0f838e4
Frank Dekker <fdekker@123inkt.nl>
2025-07-06 20:46:53 +0200

    Add form handling

--- frontend/src/app/services/authentication-service.ts
+++ frontend/src/app/services/authentication-service.ts
@@ -1,6 +1,6 @@
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
-import {BehaviorSubject, catchError, Observable, pipe, throwError} from 'rxjs';
+import {BehaviorSubject, Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
