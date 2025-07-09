52cfddfa6af25840e05639e9e0d8b55d5948d524
Frank Dekker <fdekker@123inkt.nl>
2025-07-09 21:46:31 +0200

    Add project search filter

--- frontend/src/app/component/projects/section/projects-section.ts
+++ frontend/src/app/component/projects/section/projects-section.ts
@@ -1,27 +1,36 @@
 import {DecimalPipe} from '@angular/common';
-import {Component, input} from '@angular/core';
+import {Component, input, SimpleChanges} from '@angular/core';
+import {contains} from '@lib/Strings';
 import Repository from '@model/entities/Repository';
-import {ContainsPipe} from '../../../pipes/contains-pipe';
 
 @Component({
   selector: 'app-projects-section',
-  imports: [DecimalPipe, ContainsPipe],
+  imports: [DecimalPipe],
   templateUrl: './projects-section.html',
   styleUrl: './projects-section.scss'
 })
 export class ProjectsSection {
-  public repositories  = input.required<Repository[]>();
-  public revisionCount = input.required<{ [key: number]: number }>();
-  public searchQuery   = input.required<string>();
+  public repositories                      = input.required<Repository[]>();
+  public revisionCount                     = input.required<{ [key: number]: number }>();
+  public searchQuery                       = input.required<string>();
+  public repositoryColumns: Repository[][] = [];
 
-  public getBatches(): Repository[][] {
-    const columnSize = Math.ceil(this.repositories().length / 3);
+  public ngOnChanges(changes: SimpleChanges) {
+    if (changes['searchQuery'] || changes['repositories']) {
+      this.updateRepositoryColumns();
+    }
+  }
+
+  private updateRepositoryColumns(): void {
+    const searchQuery  = this.searchQuery().toLowerCase().trim();
+    const repositories = this.repositories().filter(repo => contains(repo.displayName, searchQuery));
+    const columnSize   = Math.ceil(repositories.length / 3);
 
     const batches: Repository[][] = [];
-    for (let i = 0; i < this.repositories().length; i += columnSize) {
-      batches.push(this.repositories().slice(i, i + columnSize));
+    for (let i = 0; i < repositories.length; i += columnSize) {
+      batches.push(repositories.slice(i, i + columnSize));
     }
 
-    return batches;
+    this.repositoryColumns = batches;
   }
 }
--- frontend/src/app/pipes/contains-pipe.ts
+++
@@ -1,9 +0,0 @@
-import {Pipe, PipeTransform} from '@angular/core';
-import {contains} from '@lib/Strings';
-
-@Pipe({name: 'contains'})
-export class ContainsPipe implements PipeTransform {
-  public transform(value: string, search: string): boolean {
-    return contains(value, search);
-  }
-}
