01b6c3cc4f7bf85b9e0b00390416b01c51d02ccf
Frank Dekker <fdekker@123inkt.nl>
2025-07-19 19:07:42 +0200

    Add reviews overview

--- frontend/src/app/component/project-reviews/project-reviews.ts
+++ frontend/src/app/component/project-reviews/project-reviews.ts
@@ -1,20 +1,25 @@
 import {Component, input, OnInit} from '@angular/core';
+import {Title} from '@angular/platform-browser';
 import {ActivatedRoute} from '@angular/router';
+import {environment} from '@environment/environment';
+import ProjectReviewsViewModel from '@model/viewmodels/ProjectReviewsViewModel';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
     selector: 'app-project-reviews',
-    imports: [],
+    imports: [TranslatePipe],
     templateUrl: './project-reviews.html',
     styleUrl: './project-reviews.scss'
 })
 export class ProjectReviews implements OnInit {
     public id = input.required<number>();
-    public reviewsViewModel: unknown;
+    public declare reviewsViewModel: ProjectReviewsViewModel;
 
-    constructor(private readonly route: ActivatedRoute) {
+    constructor(private readonly title: Title, private readonly route: ActivatedRoute) {
     }
 
     public ngOnInit(): void {
-        this.reviewsViewModel = this.route.snapshot.data['projectsViewModel'];
+        this.reviewsViewModel = this.route.snapshot.data['reviewsViewModel'];
+        this.title.setTitle(this.reviewsViewModel.repository.displayName + ' - ' + environment.appName);
     }
 }
