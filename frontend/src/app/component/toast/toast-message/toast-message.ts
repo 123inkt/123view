e65f43c63edb89e2617650a03f7cf89419272d00
Frank Dekker <fdekker@123inkt.nl>
2025-07-26 21:38:00 +0200

    Toast list

--- frontend/src/app/component/toast/toast-message/toast-message.ts
+++ frontend/src/app/component/toast/toast-message/toast-message.ts
@@ -5,8 +5,22 @@ import Toast from '@model/Toast';
     selector: 'app-toast-message',
     imports: [],
     templateUrl: './toast-message.html',
-    styleUrl: './toast-message.scss'
+    styleUrl: './toast-message.scss',
+    host: {
+        '(click)': 'onClick',
+        '(animationend)': 'onAnimationEnd()'
+    }
 })
 export class ToastMessage {
     public readonly toast = input.required<Toast>();
+
+    public onClick(): void {
+        this.toast().close();
+        console.log('toast clicked');
+    }
+
+    public onAnimationEnd(): void {
+        this.toast().close();
+        console.log('Animation ended');
+    }
 }
--- frontend/src/app/models/Toast.ts
+++ frontend/src/app/models/Toast.ts
@@ -2,11 +2,12 @@ export default class Toast {
     constructor(
         public readonly message: string,
         public readonly type: 'success' | 'error' | 'info' = 'info',
-        private readonly onClose?: (toast: Toast) => void
+        private onClose?: (toast: Toast) => void
     ) {
     }
 
     public close(): void {
         this.onClose?.(this);
+        this.onClose = undefined;
     }
 }
