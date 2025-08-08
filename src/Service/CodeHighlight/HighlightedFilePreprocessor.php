4ab7b7496499edd6014e2b907ca84ad67e1e34bd
Frank Dekker <fdekker@123inkt.nl>
2025-08-08 15:39:04 +0200

    Remove generics from typescript files before highlighting

---
+++ src/Service/CodeHighlight/HighlightedFilePreprocessor.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\Review\Service\CodeHighlight;
+
+class HighlightedFilePreprocessor
+{
+    public function process(string $language, string $content): string
+    {
+        if ($language === "typescript") {
+            // currently 2025-08-21 highlightjs does not support typescript generics completely.
+            $content = preg_replace('#<\w+\\[]>#', '', $content);
+        }
+        return $content;
+    }
+}
