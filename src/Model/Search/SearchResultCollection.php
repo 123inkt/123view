464017eafee22181c470f9eeeb31b6135aee0b5b
Frank Dekker <fdekker@123inkt.nl>
2025-08-24 10:06:04 +0200

    Add option to show more results after searching code

--- src/Model/Search/SearchResultCollection.php
+++ src/Model/Search/SearchResultCollection.php
@@ -7,9 +7,26 @@ readonly class SearchResultCollection
 {
     /**
      * @codeCoverageIgnore Simple DTO
+     *
      * @param SearchResult[] $results
      */
     public function __construct(public array $results, public bool $moreResultsAvailable)
     {
+    }
+
+    /**
+     * @return iterable<int, SearchResult[]>
+     */
+    public function iteratePerRepository(): iterable
+    {
+        $grouped = [];
+        foreach ($this->results as $result) {
+            $repoId             = $result->repository->getId();
+            $grouped[$repoId]   ??= [];
+            $grouped[$repoId][] = $result;
+        }
+        foreach ($grouped as $repoId => $results) {
+            yield $repoId => $results;
+        }
     }
 }
