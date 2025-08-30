828bdb349b8c7a8707ebf4d172b6b84db2cdcfcd
Frank Dekker <fdekker@123inkt.nl>
2025-08-30 11:28:15 +0200

    Add copy reply to clipboard

--- src/Twig/CommentUrlExtension.php
+++ src/Twig/CommentUrlExtension.php
@@ -5,6 +5,7 @@ namespace DR\Review\Twig;
 
 use DR\Review\Controller\App\Review\ReviewController;
 use DR\Review\Entity\Review\Comment;
+use DR\Review\Entity\Review\CommentReply;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
@@ -20,16 +21,33 @@ class CommentUrlExtension extends AbstractExtension
      */
     public function getFunctions(): array
     {
-        return [new TwigFunction('comment_url', [$this, 'getUrl'], ['is_safe' => ['all']])];
+        return [
+            new TwigFunction('comment_url', [$this, 'getCommentUrl'], ['is_safe' => ['all']]),
+            new TwigFunction('comment_reply_url', [$this, 'getCommentReplyUrl'], ['is_safe' => ['all']])
+        ];
     }
 
-    public function getUrl(Comment $comment, bool $absolute = false): string
+    public function getCommentUrl(Comment $comment, bool $absolute = false): string
     {
         return $this->urlGenerator->generate(
             ReviewController::class,
             [
                 'review'   => $comment->getReview(),
-                'filepath' => $comment->getFilePath()
+                'filePath' => $comment->getFilePath()
+            ],
+            $absolute ? UrlGeneratorInterface::ABSOLUTE_URL : UrlGeneratorInterface::ABSOLUTE_PATH
+        );
+    }
+
+    public function getCommentReplyUrl(CommentReply $reply, bool $absolute = false): string
+    {
+        $comment = $reply->getComment();
+
+        return $this->urlGenerator->generate(
+            ReviewController::class,
+            [
+                'review'   => $comment->getReview(),
+                'filePath' => $comment->getFilePath()
             ],
             $absolute ? UrlGeneratorInterface::ABSOLUTE_URL : UrlGeneratorInterface::ABSOLUTE_PATH
         );
