ce996dae277b6d19246d45cd609cdc47cdcf5e16
Frank Dekker <fdekker@123inkt.nl>
2025-09-19 16:23:11 +0200

    Add gitlab mandatory sync controller and subscriber

--- src/Controller/App/User/UserMandatoryGitlabSyncController.php
+++ src/Controller/App/User/UserMandatoryGitlabSyncController.php
@@ -4,13 +4,16 @@ declare(strict_types=1);
 namespace DR\Review\Controller\App\User;
 
 use DR\Review\Controller\AbstractController;
+use DR\Review\Security\Role\Roles;
 use Symfony\Bridge\Twig\Attribute\Template;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class UserMandatoryGitlabSyncController extends AbstractController
 {
     #[Route('/app/user-gitlab-sync-mandatory', self::class, methods: ['GET'])]
     #[Template('app/user/user.gitlab.sync.mandatory.html.twig')]
+    #[IsGranted(Roles::ROLE_USER)]
     public function __invoke(): array
     {
         return [];
