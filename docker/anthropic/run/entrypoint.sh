5e6ff0ec691aa1d9202fc07126671ba608baf67c
Frank Dekker <fdekker@123inkt.nl>
2025-11-14 21:18:34 +0100

    .test

--- docker/anthropic/run/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-npm install --no-save
-
-# exec is needed to make supervisord pid 1 and able to receive SIGTERM signal
-exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
--- docker/nodejs/run/anthropic.js
+++ docker/nodejs/run/anthropic.js
@@ -1,16 +1,16 @@
 const http = require('http');
 const sdk = require("@anthropic-ai/claude-agent-sdk");
 
-const hostname = process.env.ANTHROPIC_HOST;
+const hostname = process.env.NODEJS_HOST;
 const port     = process.env.ANTHROPIC_PORT;
 
 const server = http.createServer(async (request, response) => {
     console.info(`Received request: ${request.method} ${request.url}`);
 
-    if (request.method !== 'POST') {
+    if (request.method !== 'GET') {
         response.statusCode = 400;
         response.setHeader('Content-Type', 'text/plain');
-        response.end('Only POST requests are supported');
+        response.end('Only GET requests are supported');
         return;
     }
     console.log('----------------starting-----------------');
@@ -23,9 +23,16 @@ const server = http.createServer(async (request, response) => {
             systemPrompt: 'You are a helpful coding assistant that reviews code snippets for potential issues and suggests improvements. Use AGENTS.md if present for context',
         }
     });
+
+    let msg = '';
     for await (const message of result) {
-        console.log(JSON.stringify(message, null, 2));
+        msg += result.result;
+        console.log(result.result ?? null);
     }
+
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    response.end(msg);
 });
 
 server.listen(port, hostname, () => {
