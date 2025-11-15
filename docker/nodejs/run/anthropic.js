94ee72322c166071d46b4893227f6a2406aec87d
Frank Dekker <fdekker@123inkt.nl>
2025-11-15 11:09:47 +0100

    Setup anthropic nodejs server

--- docker/nodejs/run/anthropic.js
+++ docker/nodejs/run/anthropic.js
@@ -1,48 +1,68 @@
 const http = require('http');
-const sdk = require("@anthropic-ai/claude-agent-sdk");
+const sdk  = require("@anthropic-ai/claude-agent-sdk");
 
-const hostname = process.env.NODEJS_HOST;
-const port     = process.env.ANTHROPIC_PORT;
+const hostname     = process.env.NODEJS_HOST ?? '127.0.0.1';
+const port         = process.env.ANTHROPIC_PORT ?? 3010;
+const systemPrompt = "You are an expert software developer.\n" +
+        "You're aware of programming paradigms like DRY, YAGNI and SOLID.\n" +
+        "Only give comments when there's a high likelihood that there is an actual issue.\n" +
+        "Focus on code quality, readability, maintainability, performance, security, and adherence to best practices.\n" +
+        "Ensure comments are concise and relevant to the code provided.\n" +
+        "Prioritize coding errors, potential bugs, and best practices.\n" +
+        "Skip code review comments with low confidence.\n" +
+        "Skip code review comments with code errors that could be picked up by a linter, static analysis tool, or unit test.\n" +
+        "Some reviews may not contain any issues for you to comment on.\n" +
+        "Use AGENTS.md if present for context.\n";
 
 const server = http.createServer(async (request, response) => {
     console.info(`Received request: ${request.method} ${request.url}`);
 
-    if (request.method !== 'GET' || request.url !== '/') {
+    if (request.method !== 'POST' || request.url !== '/query') {
         response.statusCode = 400;
         response.setHeader('Content-Type', 'text/plain');
-        response.end('Only GET requests are supported');
+        response.end('Only POST requests are supported for /query endpoint');
         return;
     }
-    console.log('----------------starting-----------------');
 
-    const resultQuery = sdk.query({
-        prompt: 'Review the following code for potential issues and suggest improvements:\n\n```javascript\nfunction add(a, b) {\n  return a + b;\n}\n\nconsole.log(add(2, 3));\n```',
-        options: {
-            model: 'claude-sonnet-4-20250514',
-            allowedTools: ['ls', 'find', 'grep'],
-            systemPrompt: 'You are a helpful coding assistant that reviews code snippets for potential issues and suggests improvements. Use AGENTS.md if present for context',
+    let body = '';
+    request.on('data', chunk => body += chunk);
+    request.on('end', async () => {
+        const data = JSON.parse(body);
+        console.log('Received review request:', data);
+
+        const resultQuery = sdk.query({
+            prompt: data.review,
+            options: {
+                model: 'claude-sonnet-4-5',
+                allowedTools: ['ls', 'find', 'grep'],
+                cwd: data.projectDir,
+                systemPrompt: systemPrompt,
+            }
+        });
+
+        let result = null;
+        for await (const message of resultQuery) {
+            if (message.type === 'result') {
+                result = message.result;
+            } else {
+                console.log("\n----------------- message -----------------");
+                console.log(message);
+            }
         }
+
+        if (result === null) {
+            response.statusCode = 404;
+            response.setHeader('Content-Type', 'text/plain');
+            response.end('');
+        }
+
+        response.statusCode = 200;
+        response.setHeader('Content-Type', 'text/markdown; charset=utf-8');
+        response.end(result);
     });
-
-    let result = null;
-    for await (const message of resultQuery) {
-        if (message.type === 'result') {
-            console.log('response', message.result);
-            result = message.result;
-        } else {
-            console.log('other type', message.type);
-        }
-    }
-
-    console.log('----------------finished-----------------');
-
-
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/plain; charset=utf-8');
-    response.end(result);
 });
 
-server.timeout = 120000;
+server.timeout        = 120000;
 server.headersTimeout = 120000;
 server.requestTimeout = 120000;
 server.listen(port, hostname, () => {
