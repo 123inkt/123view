f878a263205b304b858f33ce8b68fd2f5a930a9d
Frank Dekker <fdekker@123inkt.nl>
2025-12-02 21:59:48 +0100

    Add parentHash to revision

--- migrations/Version20251202213700.php
+++ migrations/Version20251202213700.php
@@ -17,12 +17,16 @@ final class Version20251202213700 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('ALTER TABLE revision ADD parent_hash VARCHAR(50) DEFAULT NULL after commit_hash');
-        $this->addSql('CREATE INDEX repository_parent_hash ON revision (`repository_id` , `parent_hash`)');
+        $this->addSql('ALTER TABLE revision ADD `sort` BINARY(16) DEFAULT NULL AFTER `first_branch`');
+        $this->addSql('CREATE INDEX repository_parent_hash ON revision (`repository_id`, `parent_hash`)');
+        $this->addSql('CREATE INDEX sort_timestamp ON revision (`sort`, `create_timestamp`)');
     }
 
     public function down(Schema $schema): void
     {
         $this->addSql('DROP INDEX repository_parent_hash ON revision');
+        $this->addSql('DROP INDEX sort_timestamp ON revision');
         $this->addSql('ALTER TABLE revision DROP parent_hash');
+        $this->addSql('ALTER TABLE revision DROP sort');
     }
 }
--- src/MessageHandler/NewRevisionTopoOrderMessageHandler.php
+++ src/MessageHandler/NewRevisionTopoOrderMessageHandler.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace DR\Review\MessageHandler;
 
+use DR\Review\Entity\Revision\Revision;
 use DR\Review\Message\Revision\NewRevisionMessage;
 use DR\Review\Repository\Revision\RevisionRepository;
 use Psr\Log\LoggerAwareInterface;
@@ -32,7 +33,24 @@ class NewRevisionTopoOrderMessageHandler implements LoggerAwareInterface
         }
 
         // find revisions in topo order
-        $this->revisionRepository->findBy(['commitHash' => $revision->]);
+        if ($revision->getParentHash() !== null) {
+            $parents = $this->revisionRepository->findBy(['repository' => $revision->getRepository(), 'commitHash' => $revision->getParentHash()]);
+        }
+
+        $children = $this->revisionRepository->findBy(['repository' => $revision->getRepository(), 'parentHash' => $revision->getCommitHash()]);
+
+
+    }
+
+    private function updateRevisionOrder(Revision $parentRev, Revision $childRev): void
+    {
+        if ($parentRev->getCreateTimestamp() > $childRev->getCreateTimestamp()) {
+            $childTimestamp = $parentRev->getCreateTimestamp() + 1;
+            $parentTimestamp = $childRev->getCreateTimestamp() - 1;
+        } else {
+            $childTimestamp = $childRev->getCreateTimestamp();
+            $parentTimestamp = $parentRev->getCreateTimestamp();
+        }
 
     }
 }
