93d48ad24612f2135b6a38c35b02f375d3fdbcaa
Frank Dekker <fdekker@123inkt.nl>
2025-12-27 14:30:29 +0100

    Remove auto generate proxy classes (#1285)

--- config/packages/prod/doctrine.php
+++ config/packages/prod/doctrine.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 use Symfony\Config\DoctrineConfig;
 
 return static function (DoctrineConfig $doctrineConfig): void {
-    $doctrineConfig->orm()->autoGenerateProxyClasses(true);
-
     $em = $doctrineConfig->orm()->entityManager('default');
     $em->metadataCacheDriver()->type('pool')->pool('doctrine.system_cache_pool');
     $em->queryCacheDriver()->type('pool')->pool('doctrine.system_cache_pool');
