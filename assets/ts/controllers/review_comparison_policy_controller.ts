ba07262cbf009a575d2abd3eb2cb646bf74f3350
Frank Dekker <fdekker@123inkt.nl>
2026-01-25 16:30:15 +0100

    Fix frontend

--- assets/ts/controllers/review_comparison_policy_controller.ts
+++ assets/ts/controllers/review_comparison_policy_controller.ts
@@ -1,12 +1,7 @@
 import {Controller} from '@hotwired/stimulus';
-import DataSet from '../lib/DataSet';
 
-export default class extends Controller<HTMLElement> {
+export default class extends Controller<HTMLFormElement> {
     public connect(): void {
-        this.element.addEventListener('change', this.onSelect.bind(this));
-    }
-
-    private onSelect(event: Event): void {
-        window.location.href = DataSet.string((event.target as HTMLElement), 'url');
+        this.element.addEventListener('change', () => this.element.submit());
     }
 }
