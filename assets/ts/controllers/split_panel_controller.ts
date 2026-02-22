3c686e79c809d892275f768d4b5f3d6af247c338
Frank Dekker <fdekker@123inkt.nl>
2026-02-22 11:13:35 +0100

    Add moveable split panel to side by side view (#1349)
    * Add split panel to side-by-side view
    
    * Add split panel to side-by-side view

--- assets/ts/controllers/split_panel_controller.ts
+++ assets/ts/controllers/split_panel_controller.ts
@@ -4,16 +4,17 @@ import Draggable from '../lib/Draggable';
 export default class extends Controller<HTMLElement> {
     public static targets                            = ['left', 'right', 'divider'];
     private readonly declare leftTarget: HTMLElement;
-    private readonly declare rightTarget: HTMLElement;
     private readonly declare dividerTarget: HTMLElement;
     private initialLeftCellWidth: number | undefined = undefined;
     private drag?: Draggable                         = undefined;
 
     public connect(): void {
         this.drag = new Draggable(this.dividerTarget, {
+            onStart: this.onStart.bind(this),
             onDragStart: this.onDragStart.bind(this),
             onDragMove: this.onDragMove.bind(this),
-            onDragStop: this.onDragStop.bind(this)
+            onDragStop: this.onDragStop.bind(this),
+            onComplete: this.onComplete.bind(this),
         }).attach();
     }
 
@@ -22,28 +23,39 @@ export default class extends Controller<HTMLElement> {
         this.drag = undefined;
     }
 
+    private onStart(): void {
+        this.dividerTarget.classList.toggle('dragging', true);
+    }
+
     private onDragStart(): void {
         this.initialLeftCellWidth = this.leftTarget.clientWidth;
     }
 
     private onDragMove(event: {dx: number; dy: number}): void {
-        const newLeftWidth = (this.initialLeftCellWidth ?? 0) + event.dx;
-        // Prevent the left cell from being resized to less than 200px
-        if (newLeftWidth < 200) {
-            return;
-        }
+        const containerWidth = this.element.clientWidth;
+        const dividerWidth   = this.dividerTarget.clientWidth;
 
-        const leftWidth                        = this.leftTarget.clientWidth;
-        this.element.style.gridTemplateColumns = `${newLeftWidth}px 10px auto`;
+        // calculate new widths based on pixels
+        let newLeftWidth  = (this.initialLeftCellWidth ?? 0) + event.dx;
+        let newRightWidth = containerWidth - newLeftWidth - dividerWidth;
 
-        const newRightWidth = this.rightTarget.clientWidth;
-        // Prevent the right cell from being resized to less than 20px
-        if (newRightWidth < 200) {
-            this.element.style.gridTemplateColumns = `${leftWidth}px 10px auto`;
-        }
+        // bound width within the container with 100px minimum
+        newLeftWidth  = Math.max(100, Math.min(newLeftWidth, containerWidth - dividerWidth - 100));
+        newRightWidth = Math.max(100, Math.min(newRightWidth, containerWidth - dividerWidth - 100));
+
+        // calculate new widths based on percentage
+        const leftPercentage  = (newLeftWidth / containerWidth * 100).toFixed(10);
+        const rightPercentage = (newRightWidth / containerWidth * 100).toFixed(10);
+
+        // format grid template style
+        this.element.style.gridTemplateColumns = `${leftPercentage}% ${dividerWidth}px ${rightPercentage}%`;
     }
 
     private onDragStop(): void {
         this.initialLeftCellWidth = undefined;
+    }
+
+    private onComplete(): void {
+        this.dividerTarget.classList.toggle('dragging', false);
     }
 }
