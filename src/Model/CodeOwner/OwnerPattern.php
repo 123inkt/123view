45ae245572b4d1a3be9ca553e452b225d7f79fad
Frank Dekker <fdekker@123inkt.nl>
2026-04-18 13:58:11 +0200

    Add coverage

--- src/Model/CodeOwner/OwnerPattern.php
+++ src/Model/CodeOwner/OwnerPattern.php
@@ -6,6 +6,7 @@ namespace DR\Review\Model\CodeOwner;
 readonly class OwnerPattern
 {
     /**
+     * @codeCoverageIgnore Simple DTO
      * @param list<string> $owners
      */
     public function __construct(public string $pattern, public array $owners)
