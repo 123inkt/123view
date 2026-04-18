0a37779576ad187b6db81e45bab8f01d5739fc90
Frank Dekker <fdekker@123inkt.nl>
2026-04-18 11:38:22 +0200

    Add code owner file finder

---
+++ src/Service/CodeOwner/CodeOwnerFinder.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\Review\Service\CodeOwner;
+
+use DR\Review\Entity\Repository\Repository;
+use DR\Utils\Assert;
+
+readonly class CodeOwnerFinder
+{
+    public function __construct(
+        private CodeOwnerFileFinder $fileFinder,
+        private CodeOwnerFileParser $parser,
+        private CodeOwnerFilepathMatcher $matcher,
+    ) {
+    }
+
+    /**
+     * @return list<string>
+     */
+    public function find(Repository $repository, string $filepath): array
+    {
+        $files = $this->fileFinder->find($repository, $filepath);
+
+        foreach ($files as $file) {
+            $patterns = $this->parser->parse(Assert::notFalse(file_get_contents($file)));
+            $match    = $this->matcher->match($filepath, $patterns);
+            if ($match !== null) {
+                return $match->owners;
+            }
+        }
+
+        return [];
+    }
+}
