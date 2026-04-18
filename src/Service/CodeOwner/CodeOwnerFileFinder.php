158daba39704a586a584deafa4c87e051cc4a1ed
Frank Dekker <fdekker@123inkt.nl>
2026-04-18 11:24:31 +0200

    Add code owner file finder

---
+++ src/Service/CodeOwner/CodeOwnerFileFinder.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace DR\Review\Service\CodeOwner;
+
+use DR\Review\Entity\Repository\Repository;
+use DR\Review\Service\Git\GitRepositoryLocationService;
+use Symfony\Component\Filesystem\Path;
+
+class CodeOwnerFileFinder
+{
+    public function __construct(private GitRepositoryLocationService $locationService)
+    {
+    }
+
+    /**
+     * @return string[]
+     */
+    public function find(Repository $repository, string $filepath): array
+    {
+        $filepath  = Path::normalize($filepath);
+        $directory = rtrim(Path::normalize($this->locationService->getLocation($repository)), '/');
+
+        $results = [];
+        while (true) {
+            $filepath = dirname($filepath);
+            if ($filepath === '' || $filepath === '.') {
+                break;
+            }
+
+            $codeowners = $directory . '/' . $filepath . '/CODEOWNERS';
+            if (file_exists($codeowners)) {
+                $results = [$codeowners];
+            }
+        }
+
+        return $results;
+    }
+}
