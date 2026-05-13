92ed233fb5e9e1303c6b9434fb563d2331d7e291
Frank Dekker <fdekker@123inkt.nl>
2026-05-13 23:05:44 +0200

    Add concept comment

--- src/Doctrine/Type/CommentTypeType.php
+++ src/Doctrine/Type/CommentTypeType.php
@@ -8,8 +8,5 @@ use DR\Review\Entity\Review\CommentTypeEnum;
 class CommentTypeType extends AbstractEnumType
 {
     public const string TYPE   = 'enum_comment_type';
-    public const array VALUES = [
-        CommentTypeEnum::Draft->value,
-        CommentTypeEnum::Final->value,
-    ];
+    public const array  VALUES = [CommentTypeEnum::Draft->value, CommentTypeEnum::Final->value];
 }
