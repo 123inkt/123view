bcb6d62babe15ada6bb553e1226aa84decc6bf2d
root <root@localhost>
2026-06-22 12:47:22 +0000

    chore: upgrade webpack-encore v5 -> v7, Babel 7 -> 8, fix uuid audit vuln

---
+++ webpack.config.mjs
@@ -0,0 +1,27 @@
+import Encore from '@symfony/webpack-encore';
+
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+        .setOutputPath('public/build/')
+        .setPublicPath('/build')
+        .addEntry('app', './assets/app.ts')
+        .addEntry('app.dark', './assets/app.dark.ts')
+        .addEntry('app.light', './assets/app.light.ts')
+        .addEntry('bs.theme', './assets/bootstrap-theme.ts')
+        .enableStimulusBridge('./assets/controllers.json')
+        .splitEntryChunks()
+        .enableSingleRuntimeChunk()
+        .cleanupOutputBeforeBuild()
+        .enableSourceMaps(!Encore.isProduction())
+        .enableVersioning(Encore.isProduction())
+        .configureBabel((babelConfig) => {
+            babelConfig.plugins.push(['polyfill-corejs3', { method: 'usage-global', version: '3.26' }]);
+        })
+        .enableSassLoader()
+        .enableTypeScriptLoader()
+;
+
+export default await Encore.getWebpackConfig();
