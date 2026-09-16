c5a4f5a82fcff6d7e889d411f5faafe268241313
Frank Dekker <fdekker@123inkt.nl>
2026-09-16 11:11:12 +0200

    Revision id is null (#1564)

--- src/MessageHandler/CommitAddedMessageHandler.php
+++ src/MessageHandler/CommitAddedMessageHandler.php
@@ -46,10 +46,12 @@ class CommitAddedMessageHandler implements LoggerAwareInterface
         }
 
         $revisions = $this->revisionFactory->createFromCommit($commit);
-        $this->revisionRepository->saveAll($repository, $revisions);
+        $revisions = $this->revisionRepository->saveAll($repository, $revisions);
 
         foreach ($revisions as $revision) {
-            $this->bus->dispatch(new NewRevisionMessage($revision->getId()));
+            if ($revision->hasId()) {
+                $this->bus->dispatch(new NewRevisionMessage($revision->getId()));
+            }
         }
     }
 }
