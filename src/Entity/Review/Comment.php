3b133c019d648f3c73747ac5d6d18a478a5d2ac3
Frank Dekker <fdekker@123inkt.nl>
2026-09-21 15:26:34 +0200

    Make comment.replies lazy

--- src/Entity/Review/Comment.php
+++ src/Entity/Review/Comment.php
@@ -77,11 +77,11 @@ class Comment
     private User $user;
 
     /** @phpstan-var Collection<int, CommentReply> */
-    #[ORM\OneToMany(targetEntity: CommentReply::class, mappedBy: 'comment', cascade: ['persist', 'remove'], fetch: 'EAGER', orphanRemoval: false)]
+    #[ORM\OneToMany(targetEntity: CommentReply::class, mappedBy: 'comment', cascade: ['persist', 'remove'], orphanRemoval: false)]
     private Collection $replies;
 
     /** @phpstan-var Collection<int, UserMention> */
-    #[ORM\OneToMany(targetEntity: UserMention::class, mappedBy: 'comment', cascade: ['persist', 'remove'], fetch: 'LAZY', orphanRemoval: false)]
+    #[ORM\OneToMany(targetEntity: UserMention::class, mappedBy: 'comment', cascade: ['persist', 'remove'], orphanRemoval: false)]
     private Collection $mentions;
 
     public function __construct()
