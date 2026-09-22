77c6b4853347f4f175775b09fea86615cecdcb61
Frank Dekker <fdekker@123inkt.nl>
2026-09-22 09:59:21 +0200

    Cleanup functional test

--- tests/Unit/ApiPlatform/Provider/CommentCollectionProviderTest.php
+++ tests/Unit/ApiPlatform/Provider/CommentCollectionProviderTest.php
@@ -18,6 +18,7 @@ use DR\Review\Tests\AbstractTestCase;
 use InvalidArgumentException;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\MockObject\MockObject;
+use RuntimeException;
 
 #[CoversClass(CommentCollectionProvider::class)]
 class CommentCollectionProviderTest extends AbstractTestCase
@@ -40,41 +41,61 @@ class CommentCollectionProviderTest extends AbstractTestCase
         $this->collectionProvider->expects($this->never())->method('provide');
         $this->outputFactory->expects($this->never())->method('create');
 
-        $this->expectException(InvalidArgumentException::class);
+        $this->expectException(RuntimeException::class);
         $this->expectExceptionMessage('Only GetCollection operation is supported');
         $this->provider->provide(new Get());
     }
 
     public function testMapsIterableWithoutSecondCall(): void
     {
-        $operation = new GetCollection();
-        $comment   = new Comment();
-        $output    = static::createStub(CommentOutput::class);
+        $operation     = new GetCollection();
+        $uriVariables  = ['review.id' => '123'];
+        $context       = ['filters' => ['state' => 'open']];
+        $comment       = new Comment();
+        $output        = static::createStub(CommentOutput::class);
 
-        $this->collectionProvider->expects($this->once())->method('provide')->with($operation)->willReturn(new ArrayIterator([$comment]));
+        $this->collectionProvider
+            ->expects($this->once())
+            ->method('provide')
+            ->with($operation, $uriVariables, $context)
+            ->willReturn(new ArrayIterator([$comment]));
         $this->outputFactory->expects($this->once())->method('create')->with($comment)->willReturn($output);
 
-        $result = $this->provider->provide($operation);
+        $result = $this->provider->provide($operation, $uriVariables, $context);
 
         static::assertSame([$output], iterator_to_array($result));
     }
 
     public function testMapsPaginatorAndMetadata(): void
     {
-        $operation = new GetCollection();
-        $comment   = new Comment();
-        $output    = static::createStub(CommentOutput::class);
-        $paginator  = new TraversablePaginator(new ArrayIterator([$comment]), 2, 10, 25);
+        $operation    = new GetCollection();
+        $uriVariables = ['review.id' => '123'];
+        $context      = ['filters' => ['state' => 'open']];
+        $comment      = new Comment();
+        $output       = static::createStub(CommentOutput::class);
+        $paginator     = new TraversablePaginator(new ArrayIterator([$comment]), 2, 10, 25);
 
-        $this->collectionProvider->expects($this->once())->method('provide')->with($operation)->willReturn($paginator);
+        $this->collectionProvider->expects($this->once())->method('provide')->with($operation, $uriVariables, $context)->willReturn($paginator);
         $this->outputFactory->expects($this->once())->method('create')->with($comment)->willReturn($output);
 
-        $result = $this->provider->provide($operation);
+        $result = $this->provider->provide($operation, $uriVariables, $context);
 
         static::assertInstanceOf(PaginatorInterface::class, $result);
         static::assertSame(2.0, $result->getCurrentPage());
         static::assertSame(10.0, $result->getItemsPerPage());
         static::assertSame(25.0, $result->getTotalItems());
         static::assertSame([$output], iterator_to_array($result));
+    }
+
+    public function testRejectsNonIterableCollectionProviderResult(): void
+    {
+        $operation = new GetCollection();
+
+        $this->collectionProvider->expects($this->once())->method('provide')->with($operation, [], [])->willReturn(null);
+        $this->outputFactory->expects($this->never())->method('create');
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Collection provider must return an iterable.');
+        $this->provider->provide($operation);
     }
 }
