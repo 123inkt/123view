78aa68c7af7b27571432d4c47304c0a1555b6451
Frank Dekker <fdekker@123inkt.nl>
2026-09-22 09:25:54 +0200

    Update services.php

--- config/services.php
+++ config/services.php
@@ -117,7 +117,7 @@ return static function (ContainerConfigurator $container): void {
     $services->load('DR\Review\ApiPlatform\Factory\\', __DIR__ . '/../src/ApiPlatform/Factory');
     $services->load('DR\Review\ApiPlatform\Provider\\', __DIR__ . '/../src/ApiPlatform/Provider');
     $services->load('DR\Review\ApiPlatform\StateProcessor\\', __DIR__ . '/../src/ApiPlatform/StateProcessor');
-    $services->set(CommentVisibilityExtension::class);
+    $services->load('DR\Review\ApiPlatform\Extension\\', __DIR__ . '/../src/ApiPlatform/Extension');
     $services->load('DR\Review\Command\\', __DIR__ . '/../src/Command');
     $services->load('DR\Review\EventSubscriber\\', __DIR__ . '/../src/EventSubscriber');
     $services->load('DR\Review\Form\\', __DIR__ . '/../src/Form');
